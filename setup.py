65fdc7d68131e210506899063c14551fc212dce8
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-02-09 18:35:49 +0000

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+from stonesoup import __version__ as version
+
+setup(name='stonesoup',
+      version=version,
+      description='A target tracking development/testing framework',
+      url='https://github.com/dstl/Stone-Soup',
+      packages=find_packages(exclude=('docs', '*.tests')),
+      install_requires=['numpy', 'ruamel.yaml'],
+      extras_require={
+          'dev': ['pytest', 'Sphinx', 'flake8', 'coverage'],
+      },
+      )
