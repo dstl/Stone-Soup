6804f08298d419ba6be42a106bb235942b012905
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-04-11 09:09:41 +0100

    Minor fixes for Likelihood Ratio Hypothesiser

--- stonesoup/hypothesiser/likelihood.py
+++ stonesoup/hypothesiser/likelihood.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+
 import numpy as np
 
 from .base import Hypothesiser
@@ -13,16 +14,17 @@ class LikelihoodRatioMatrixHypothesiser(Hypothesiser):
         hypotheses = defaultdict(list)
         for track in tracks:
             for detection in detections:
-                state_vector, innov = self.update(track, detection)
-                diff = np.linalg.lstsq(innov.covar, innov.state)
+                state_vector, innov = self.updater.update(track, detection)
+                diff = np.linalg.lstsq(innov.covar, innov.state)[0]
                 mahabDist = np.sum(diff * diff)
                 if mahabDist <= gammaVal:
-                    hypothesis = Hypothesis(*state_vector)
+                    hypothesis = Hypothesis(
+                        state_vector.state, state_vector.covar)
                     hypothesis.detection = detection
                     hypothesis.ratio = track.det_prob/clutter_param*(1/((2*np.pi)**(len(innov.covar)/2)*np.abs(np.linalg.det(innov.covar))))*np.exp(-0.5*mahabDist)
                     hypotheses[track].append(hypothesis)
             # Missed detection ratio
-            hypothesis = Hypothesis(*track.state_vector)
+            hypothesis = Hypothesis(track.state, track.covar)
             hypothesis.ratio = 1 - track.det_prob
             hypotheses[track].append(hypothesis)
         return hypotheses
