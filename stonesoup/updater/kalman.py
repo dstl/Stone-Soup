eeb4c529524657aea6abf1038d86ceedfa19eb48
sglvladi <sglvladi@gmail.com>
2018-04-23 04:36:13 +0100

    Updated ExtendedKalmanUpdater class

--- stonesoup/updater/kalman.py
+++ stonesoup/updater/kalman.py
@@ -3,7 +3,7 @@
 import numpy as np
 
 from .base import Updater
-from ..functions import tria
+from ..functions import tria, jacobian
 from ..measurementmodel import MeasurementModel
 from ..types.base import GaussianState, StateVector
 from ..base import Property
@@ -57,7 +57,7 @@ class KalmanUpdater(Updater):
         """
 
         if(cross_covar is None):
-            cross_covar = state_pred.covar@self.meas_model.eval().The
+            cross_covar = state_pred.covar@self.meas_model.eval().T
 
         state_post = GaussianState()
 
@@ -100,7 +100,69 @@ class KalmanUpdater(Updater):
         x_post = x_pred + K@(y-y_pred)
         P_post = P_pred - K@S@K.T
 
-        return (x_post, P_post, K)
+        return x_post, P_post, K
+
+
+class ExtendedKalmanUpdater(KalmanUpdater):
+    """Extended Kalman Filter
+
+    Perform measurement update step in the Extended Kalman Filter.
+
+    Parameters
+    ----------
+    meas_model : :class:`MeasurementModel`
+        The measurement model
+    """
+
+    meas_model = Property(MeasurementModel, doc="measurement model")
+
+    def __init__(self, meas_model, *args, **kwargs):
+        """Constructor method
+
+        Parameters
+        ----------
+        meas_model : :class:`MeasurementModel`
+            The measurement model
+        """
+
+        super().__init__(meas_model, *args, **kwargs)
+
+    def update(self, state_pred, meas_pred, meas, cross_covar=None):
+        """ExtendedKalman Filter update step
+
+        Parameters
+        ----------
+        state_pred : :class:`GaussianState`
+            The state prediction
+        meas_pred : :class:`GaussianState`
+            The measurement prediction
+        meas : 1-D numpy.ndarray of shape (Nm,1)
+            The measurement vector
+        cross_covar: 2-D numpy.ndarray of shape (Nm,Nm), optional
+            The state-to-measurement cross covariance (the default is None, in
+            which case ``cross_covar`` will be computed internally)
+
+        Returns
+        -------
+        state_post : :class:`GaussianState`
+            The state posterior
+        kalman_gain : 2-D numpy.ndarray of shape (Ns,Nm)
+            The computed Kalman gain
+        """
+
+        if(cross_covar is None):
+            def h(x):
+                return self.meas_model.eval(x)
+            H = jacobian(h, state_pred.mean)
+            cross_covar = state_pred.covar@H.T
+
+        state_post = GaussianState()
+
+        state_post.mean, state_post.covar, kalman_gain = \
+            super()._update(state_pred.mean, state_pred.covar, meas,
+                            meas_pred.mean, meas_pred.covar, cross_covar)
+
+        return state_post, kalman_gain
 
 
 class SqrtKalmanUpdater(Updater):
