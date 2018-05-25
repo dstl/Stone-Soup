a8fa07718d1fe0611cf61cece37b649be33fb521
josh coates <joshcoates@outlook.com>
2018-05-25 13:35:28 +0100

    Remove leftover incomplete linear transition model class.

--- stonesoup/models/transitionmodel/linear.py
+++ stonesoup/models/transitionmodel/linear.py
@@ -5,49 +5,8 @@ from scipy.stats import multivariate_normal
 
 from ...base import Property
 from ...types import CovarianceMatrix
-from ..base import LinearModel, GaussianModel, TimeVariantModel
+from ..base import LinearModel, GaussianModel, TimeVariantModel, TimeInvariantModel
 from .base import TransitionModel
-
-
-class LinearTransitionModel(TransitionModel):
-    """Linear Transition Model
-
-    Transitions state using a transition matrix, and optionally accepts a
-    control matrix and vector to apply to the new state.
-
-    .. math::
-
-        \hat{\mathbf{x}}_{k\mid k-1} &=
-            \mathbf{F}_k \hat{\mathbf{x}}_{k-1\mid k-1} +
-            \mathbf{B}_k \mathbf{u}_k
-
-    """
-
-    transition_matrix = Property(
-        sp.ndarray, doc="Linear transition matrix :math:`\mathbf{F}_k`.")
-    control_matrix = Property(
-        sp.ndarray, default=None, doc="Control matrix :math:`\mathbf{B}_k`.")
-
-    def transition(self, state_vector, control_vector=None):
-        """Transition state
-
-        Parameters
-        ----------
-        state_vector : StateVector
-            State vector :math:`\hat{\mathbf{x}}_{k-1|k-1}`.
-        control_vector : StateVector, optional
-            Control vector :math:`\mathbf{u}_k`. Default is None in which case
-            no control vector is applied.
-
-        Returns
-        -------
-        StateVector
-            New state vector :math:`\hat{\mathbf{x}}_{k\mid k-1}`.
-        """
-        new_state_vector = self.transition_matrix @ state_vector
-        if control_vector is not None:
-            new_state_vector += self.control_matrix @ control_vector
-        return new_state_vector
 
 
 class ConstantVelocity1D(TransitionModel, LinearModel,
