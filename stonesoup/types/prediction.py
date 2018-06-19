58a1a75f2ab9fd2e96dc83ffee1cea258fdfc613
sglvladi <sglvladi@gmail.com>
2018-06-19 20:51:26 +0100

    Fix typos and GaussianMeasurementPrediciton initiation

--- stonesoup/types/prediction.py
+++ stonesoup/types/prediction.py
@@ -19,7 +19,7 @@ class StatePrediction(Prediction):
 class MeasurementPrediction(Prediction):
     """ MeasurementPrediction type
 
-    Most simple mesurement prediction type, which only has time and a state
+    Most simple measurement prediction type, which only has time and a state
     vector.
     """
 
@@ -33,7 +33,7 @@ class GaussianStatePrediction(StatePrediction, GaussianState):
 
 
 class GaussianMeasurementPrediction(MeasurementPrediction, GaussianState):
-    """ GausssianMeasurementPrediction type
+    """ GaussianMeasurementPrediction type
 
     This is a simple Gaussian measurement prediction object, which, as the name
     suggests, is described by a Gaussian distribution.
@@ -45,9 +45,9 @@ class GaussianMeasurementPrediction(MeasurementPrediction, GaussianState):
 
     def __init__(self, state_vector, covar, timestamp=None,
                  cross_covar=None, *args, **kwargs):
-        super().__init__(state_vector, covar, timestamp, *args, **kwargs)
         if(cross_covar is not None
            and cross_covar.shape[1] != state_vector.shape[0]):
             raise ValueError("cross_covar should have the same number of \
                              columns as the number of rows in state_vector")
-        self.cross_covar = cross_covar
+        super().__init__(state_vector, covar, timestamp,
+                         cross_covar, *args, **kwargs)
