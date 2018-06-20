192d3d4804e5332c8b8ab6cbbe43fed6d1963550
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-06-20 14:16:43 +0100

    Update hypothesis type test for new Prediction classes

--- stonesoup/types/tests/test_hypothesis.py
+++ stonesoup/types/tests/test_hypothesis.py
@@ -2,13 +2,14 @@
 import numpy as np
 import pytest
 
-from ..state import State
+from ..prediction import Prediction, MeasurementPrediction
 from ..detection import Detection
 from ..track import Track
-from ..hypothesis import Hypothesis, DistanceHypothesis, JointHypothesis, DistanceJointHypothesis
+from ..hypothesis import (
+    Hypothesis, DistanceHypothesis, JointHypothesis, DistanceJointHypothesis)
 
-prediction = State(np.array([[1], [0]]))
-innovation = State(np.array([[1], [0]]))
+prediction = Prediction(np.array([[1], [0]]))
+measurement_prediction = MeasurementPrediction(np.array([[1], [0]]))
 detection = Detection(np.array([[1], [0]]))
 distance = float(1)
 
@@ -17,16 +18,17 @@ def test_hypothesis():
     """Hypothesis type test"""
 
     with pytest.raises(TypeError):
-        Hypothesis(prediction, innovation, detection)
+        Hypothesis(prediction, measurement_prediction, detection)
 
 
 def test_distance_hypothesis():
     """Distance Hypothesis type test"""
 
-    hypothesis = DistanceHypothesis(prediction, innovation, detection, distance)
+    hypothesis = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
 
     assert hypothesis.prediction is prediction
-    assert hypothesis.innovation is innovation
+    assert hypothesis.measurement_prediction is measurement_prediction
     assert hypothesis.detection is detection
     assert hypothesis.distance is distance
 
@@ -34,8 +36,10 @@ def test_distance_hypothesis():
 def test_distance_hypothesis_comparison():
     """Distance Hypothesis comparison test"""
 
-    h1 = DistanceHypothesis(prediction, innovation, detection, distance)
-    h2 = DistanceHypothesis(prediction, innovation, detection, distance + 1)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance + 1)
 
     assert h1 > h2
     assert h2 < h1
@@ -49,8 +53,10 @@ def test_distance_joint_hypothesis():
 
     t1 = Track()
     t2 = Track()
-    h1 = DistanceHypothesis(prediction, innovation, detection, distance)
-    h2 = DistanceHypothesis(prediction, innovation, detection, distance)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
 
     hypotheses = {t1: h1, t2: h2}
     joint_hypothesis = JointHypothesis(hypotheses)
@@ -59,22 +65,25 @@ def test_distance_joint_hypothesis():
     assert joint_hypothesis[t1] is h1
     assert joint_hypothesis[t2] is h2
     assert joint_hypothesis.distance == distance * 2
-    
-    
+
+
 def test_distance_joint_hypothesis_comparison():
     """Distance Joint Hypothesis comparison test"""
 
     t1 = Track()
     t2 = Track()
-    h1 = DistanceHypothesis(prediction, innovation, detection, distance)
-    h2 = DistanceHypothesis(prediction, innovation, detection, distance)
-    h3 = DistanceHypothesis(prediction, innovation, detection, distance + 1)
+    h1 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h2 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance)
+    h3 = DistanceHypothesis(
+        prediction, measurement_prediction, detection, distance + 1)
 
     hypotheses1 = {t1: h1, t2: h2}
     hypotheses2 = {t1: h1, t2: h3}
     j1 = JointHypothesis(hypotheses1)
     j2 = JointHypothesis(hypotheses2)
-    
+
     assert j1 > j2
     assert j2 < j1
     assert j1 <= j1
