1a45c7455c1b9820b781b1765a82b94786c2ab03
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-06-20 09:36:08 +0100

    Add simple MultiTargetTracker class

--- stonesoup/tracker/simple.py
+++ stonesoup/tracker/simple.py
@@ -80,3 +80,67 @@ class SingleTargetTracker(Tracker):
                     self.track = None
 
             yield time, self.tracks
+
+
+class MultiTargetTracker(Tracker):
+    """A simple multi target tracker.
+
+    Track multiple objects using Stone Soup components. The tracker works by
+    first calling the :attr:`data_associator` with the active tracks, and then
+    either updating the track state with the result of the :attr:`updater` if
+    a detection is associated, or with the prediction if no detection is
+    associated to the track. Tracks are then checked for deletion by the
+    :attr:`deletor`, and remaining unassociated detections are passed to the
+    :attr:`initiator` to generate new tracks.
+
+    Parameters
+    ----------
+    """
+    initiator = Property(
+        Initiator,
+        doc="Initiator used to initialise the track.")
+    deletor = Property(
+        Deletor,
+        doc="Initiator used to initialise the track.")
+    detector = Property(
+        DetectionReader,
+        doc="Detector used to generate detection objects.")
+    data_associator = Property(
+        DataAssociator,
+        doc="Association algorithm to pair predictions to detections")
+    updater = Property(
+        Updater,
+        doc="Updater used to update the track object to the new state.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._tracks = set()
+
+    @property
+    def tracks(self):
+        return self._tracks.copy()
+
+    def tracks_gen(self):
+        self._tracks = set()
+
+        for time, detections in self.detector.detections_gen():
+
+            associations = self.data_associator.associate(
+                self._tracks, detections, time)
+            associated_detections = set()
+            for track, hypothesis in associations.items():
+                if hypothesis.detection is not None:
+                    state_post = self.updater.update(
+                        hypothesis.prediction,
+                        hypothesis.detection,
+                        hypothesis.innovation)
+                    track.states.append(state_post)
+                    associated_detections.add(hypothesis.detection)
+                else:
+                    track.states.append(hypothesis.prediction)
+
+            self._tracks -= self.deletor.delete_tracks(self._tracks)
+            self._tracks |= self.initiator.initiate(
+                detections - associated_detections)
+
+            yield time, self.tracks
