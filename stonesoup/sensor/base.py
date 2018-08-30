824a6243b9690127e1db0627038fa659d3078ae1
sglvladi <sglvladi@gmail.com>
2018-08-30 04:21:06 +0100

    Add Sensor class and implementation of a SimpleRadar

---
+++ stonesoup/sensor/base.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from abc import abstractmethod
+
+from ..base import Base, Property
+from ..types.array import StateVector
+from ..models import MeasurementModel
+
+
+class Sensor(Base):
+    """Sensor base class
+
+    A sensor object that opperates according to a given
+    :class:`~.MeasurementModel`.
+    """
+
+    measurement_model = Property(
+        MeasurementModel, default=None, doc="Measurement model")
+
+    @abstractmethod
+    def gen_measurement(**kwargs):
+        """Generate a measurement"""
+        raise NotImplemented
+
+
+class MountableSensor(Sensor):
+    """MountableSensor base class
+
+    A sensor that can be mounted on a platform.
+    """
+
+    platform_offset = Property(
+        StateVector, default=None,
+        doc="A state vector describing the mounting offset of the sensor,\
+            relative to the platform on which it is mounted")
---
+++ stonesoup/sensor/radar.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+from .base import Sensor
+from ..base import Property
+from ..types.state import StateVector
+from ..types.detection import Detection
+from ..types.array import CovarianceMatrix
+from ..models.measurement.nonlinear\
+    import RangeBearingGaussianToCartesian
+
+
+class SimpleRadar(Sensor):
+    """A simple radar sensor that generates measurements of targets, using a
+    :class:`~.RangeBearingGaussianToCartesian` model, relative to its position.
+    """
+
+    position = Property(StateVector,
+                        doc="The radar position on a 2D Cartesian plane")
+    noise_covar = Property(CovarianceMatrix,
+                           doc="The sensor noise covariance matrix. This is utilised\
+                                by (and follow in format) the underlying\
+                                :class:`~.RangeBearingGaussianToCartesian`\
+                                model")
+
+    def __init__(self, position, noise_covar, *args, **kwargs):
+
+        measurement_model = RangeBearingGaussianToCartesian(
+            ndim_state=2, mapping=np.array([0, 1]),
+            noise_covar=noise_covar,
+            origin_offset=position)
+        super().__init__(position, noise_covar, measurement_model,
+                         *args, **kwargs)
+
+    def set_position(self, position):
+        self.position = position
+        self.measurement_model.origin_offset = position
+
+    def gen_measurement(self, ground_truth, **kwargs):
+        """Generate a measurement for a given state
+
+        Parameters
+        ----------
+        ground_truth : :class:`~.State`
+            A ground-truth state
+
+        Returns
+        -------
+        :class:`~.Detection`
+            A measurement generated from the given state. The timestamp of the\
+            measurement is set equal to that of the provided state.
+        """
+
+        measurement_vector = self.measurement_model.function(
+            ground_truth.state_vector, **kwargs)
+
+        return Detection(measurement_vector, timestamp=ground_truth.timestamp)
