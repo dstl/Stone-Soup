2bf6cadb5581e538b69030ad834b38a2c7b7a17e
David Last <dlast@dstl.gov.uk>
2018-10-22 16:34:33 +0100

    Added 'metadata' attribute to Detection class.

--- stonesoup/types/detection.py
+++ stonesoup/types/detection.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
+from ..base import Property
 from .state import State, GaussianState
 
 
 class Detection(State):
     """Detection type"""
+
+    metadata = Property(dict, default=None,
+                        doc='Non-standard metadata items for Detections. '
+                            'Default to empty dictionary.')
+
+    def __init__(self, state_vector, *args, **kwargs):
+        super().__init__(state_vector, *args, **kwargs)
+        if self.metadata is None:
+            self.metadata = {}
 
 
 class GaussianDetection(Detection, GaussianState):
