359f50ae67a9d44cb931cd86a090823f24c17623
Richard Green <rjgreen@dstl.gov.uk>
2018-10-26 12:10:27 +0100

    Platform modified to operate within a 3D environment, with sensor
    sensor positions being applied relative to the moving sensor. Additional
    modification need to make the offsets relative to the heading of the
    platform

--- stonesoup/platform/simple.py
+++ stonesoup/platform/simple.py
@@ -10,40 +10,68 @@ from .base import Platform
 class SensorPlatform(Platform):
     """A simple Platform that can carry a number of different sensors
 
-    Note
-    ----
-    The current implementation of this class assumes a 2D Cartesian plane.
+    Notes
+    -----
+    The current implementation of this class assumes an 2D Cartesian plane.
 
-    TODO: Extend to nD state space
     """
+
+    # TODO: Offset sensors relative to platform orientation
+
+    # TODO: Determine where a platform coordinate frame should be maintained
 
     sensors = Property([Sensor], doc="A list of mounted sensors")
     mounting_offsets = Property(
         [np.array], doc="A list of sensor offsets (For now expressed\
-                            as a 2xN array of 2D Cartesian coordinates)")
+                            as a nxN array of nD Cartesian coordinates)")
+    # TODO: determine if mappings are per-sensor or per-platform
     mounting_mappings = Property(
-        [np.array], doc="Mappings between the platform state vector and the\
-                         respective sensor positions")
+        [np.array], doc="Mappings between the platform state vector and each\
+                            individuals sensors mouting offset")
+    coordinate_reference_system = Property(str, default="NEU",
+                                           doc="Coordinate reference system,\
+                                           default is North, East Up")
+
+    def __init__(self, *args, **kwargs):
+        """
+        Ensure that the platform location and the sensor locations are
+        consistent at initialisation.
+        """
+        super().__init__(*args, **kwargs)
+        self._move_sensors()
+
+    # TODO: create add_sensor method
 
     def move(self, timestamp=None, **kwargs):
         """Propagate the platform position using the :attr:`transition_model`,\
-        while updating the positions of all mounted sensors.
+        and use _move_sensors method to update sensor positions
 
         Parameters
         ----------
         timestamp: :class:`datetime.datetime`, optional
-            A timestamp signifying when the end of the maneuver \
+            A timestamp signifying when the maneuver completes \
             (the default is `None`)
 
         """
 
         # Call superclass method to update platform state
         super().move(timestamp=timestamp, **kwargs)
+        # Move the platforms sensors
+        self._move_sensors()
 
-        # Update the positions of all sensors
-        for i in range(0, len(self.sensors)):
-            self.sensors[i].set_position(StateVector(
-                [[self.state.state_vector[self.mounting_mappings[0][i]][0]
-                  + self.mounting_offsets[0][i]],
-                 [self.state.state_vector[self.mounting_mappings[1][i]][0]
-                  + self.mounting_offsets[1][i]]]))
+    def _move_sensors(self):
+        """  Propogste the Sensor positions based upon the mounting
+        offsets and the platform position and heading post manoeuvre.
+        TODO -  handle heading information
+        x' = x.cos[theta] + y.sin[theta]
+        y' = y.cos[theta] + x.sin[theta]
+        z' =
+        """
+        # Update the positions of all sensors relative to the platform
+        for i in range(len(self.sensors)):
+            new_sensor_pos = np.zeros([self.mounting_offsets.shape[1], 1])
+            for j in range(self.mounting_offsets.shape[1]):
+                new_sensor_pos[j, 0] = (self.state.state_vector
+                                        [self.mounting_mappings[i, j]]
+                                        + self.mounting_offsets[i, j])
+            self.sensors[i].set_position(StateVector(new_sensor_pos))
