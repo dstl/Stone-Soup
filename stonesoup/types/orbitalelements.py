bcffe1a3b4ec6e9120bd5b784415c807908db46c
jmbarr <jmbarr@dstl.gov.uk>
2018-11-15 15:41:50 +0000

    Augmented orbital elements state vector

--- stonesoup/types/orbitalelements.py
+++ stonesoup/types/orbitalelements.py
@@ -38,38 +38,111 @@ class OrbitalElements(State):
 
     # Could replace with getters and setters
     def semimajor_axis(self):
+        """
+
+        :return: Orbit semimajor axis, :math:`a` (m)
+
+        Note that positive number indicates an elliptical (or circular) orbit, negative a hyperbolic orbit and
+        0 is parabolic
+
+        """
         return self.state_vector[1][0]
 
     def eccentricity(self):
+        """
+
+        :return: Orbit eccentricity, :math:`e` (unitless)
+
+        :math:`e = 0` is a circle
+        :math:`0 < e < 1`, an elliptical orbit
+        :math:`e = 1` is a parabola, and
+        :math:`e > 1' denotes a hyperbolic orbit
+
+        """
         return self.state_vector[0][0]
 
     def inclination(self):
-        return self.state_vector[2][0]
+        """
+
+        :return: Orbit inclination (radians)
+
+        Usually defined as the angle between the orbital plane and that defined by the rotational axis of the parent
+        body
+
+
+        """
+        # Ensure that what's returned is between 0 and PI/2 (required by some os the astro libraries)
+        return np.remainder(self.state_vector[2][0], np.pi/2)
+
 
     def long_asc_node(self):
+        """
+
+        :return: Longitude of the ascending node :math:`\Omega` (radians)
+
+        For geocentric orbits, often known as the right ascension of the ascending node
+
+        The longitude at which the orbit crosses the parent's rotational plane from negative to positive
+
+        """
         return self.state_vector[3][0]
 
     def arg_periapsis(self):
+        """
+
+        :return: Argument of periapsis, :math:`\omega` (radians)
+
+        Or the argument of perigee for geocentric orbits, argument of perihelion for heliocentric orbits.
+
+        The angle between the orbital semi-major axis and the longitude of the ascending node (measured anticlockwise
+        from the ascending node to the semi-major axis). Note that this is undefined for circular orbits, as circles
+        don't have a unique semi-major axis.
+
+
+
+        """
         return self.state_vector[4][0]
 
     def true_anomaly(self):
+        """
+
+        :return: True anomaly (radians)
+
+        The current angle of the orbiting body measured anticlockwise from the argument of periapsis.
+
+        """
         return self.state_vector[5][0]
 
     def period(self):
+        """
+
+        :return: Orbital period, :math:`T`
+
+        """
         return ((2*np.pi)/np.sqrt(self.grav_parameter))*np.power(self.semimajor_axis(), 3/2)
 
     def spec_angular_mom(self):
+        """
+
+        :return: Magnitude of the specific angular momentum, :math:`h`
+
+        """
         return np.sqrt(self.grav_parameter * self.semimajor_axis() * (1 - self.eccentricity()**2))
 
     def spec_orb_ener(self):
+        """
+
+        :return: Specific orbital energy (:math:`\frac{-GM}{2a}`)
+
+        """
         return -self.grav_parameter/(2 * self.semimajor_axis())
 
     def mean_anomaly(self):
         """
 
-        :return: M, the mean anomaly
+        :return: Mean anomaly, :math:`M` (radians)
 
-        Use eccentric anomaly and Kepler's equation to get mean anomaly from true anomaly and eccentricity
+        Uses the eccentric anomaly and Kepler's equation to get mean anomaly from true anomaly and eccentricity
 
         """
 
@@ -80,7 +153,7 @@ class OrbitalElements(State):
     def position_vector(self):
         """
 
-        :return: r (sometimes confusingly called the state vector)
+        :return: Position vector in the parent-fixed inertial frame, r (sometimes confusingly called the state vector)
 
         """
 
@@ -97,7 +170,7 @@ class OrbitalElements(State):
     def velocity_vector(self):
 
         """
-        :return: v or :math:`r^/dot`
+        :return: Velocity vector in the parent-centred inertial frame, :math:`v` or :math:`r^/dot`
 
         """
 
@@ -115,7 +188,7 @@ class OrbitalElements(State):
     def matrix_perifocal_to_geocentric(self):
 
         """
-        :return: Matrix to Rotate vectors from perifocal plane to the Geocentric plane
+        :return: Matrix to Rotate vectors from perifocal plane to the plane defined by the parent body
 
         """
 
