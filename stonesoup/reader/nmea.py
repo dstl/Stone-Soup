565460eebebc769f27588da728188f7797bc165b
Mamoon Rashid <mamoon.rashid@drdc-rddc.gc.ca>
2018-11-26 10:51:17 -0500

    Minor changes to nmea.py.

--- stonesoup/reader/nmea.py
+++ stonesoup/reader/nmea.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 import numpy as np
@@ -9,3 +9,18 @@ from aisutils.parser import NMEAParser, MSSISParser
 
 from stonesoup.reader import DetectionReader, TextFileReader
 from stonesoup.types import Detection
+
+
+class NMEAReader(DetectionReader):
+	bbox = Property(
+        (float, float, float, float),
+        default=None,
+        doc="left, bottom, right, top")
+    start_time = Property(
+        datetime,
+        doc="Time to begin extracting NMEA messages.")
+    end_time = Property(
+        datetime,
+        doc="Time to finish extracting NMEA messages.")
+    number_steps = Property(
+        int, default=10, doc="Number of time steps to run for")
