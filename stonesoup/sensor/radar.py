a28d10b6bb642efbb3c2e3fc3036f7b5f1e171ba
sglvladi <sglvladi@gmail.com>
2018-12-04 02:54:24 +0000

    Add orientation property to SimpleRadar

--- stonesoup/sensor/radar.py
+++ stonesoup/sensor/radar.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from .base import Sensor
 from ..base import Property
-from ..types.state import StateVector
+from ..types.state import State, StateVector
 from ..types.detection import Detection
 from ..types.array import CovarianceMatrix
 from ..models.measurement.nonlinear\
@@ -24,6 +24,15 @@ class SimpleRadar(Sensor):
 
     position = Property(StateVector,
                         doc="The radar position on a 2D Cartesian plane")
+    orientation = Property(StateVector,
+                           doc="A 3x1 array of angles (rad), specifying the\
+                                radar orientation in terms of the\
+                                clockwise rotation around each\
+                                Cartesian axis in the order :math:`z,y,x`.\
+                                The rotation angles are positive if the\
+                                rotation is in the clockwise direction\
+                                when viewed by an observer looking along the\
+                                respective rotation axis, towards the origin")
     ndim_state = Property(
         int,
         doc="Number of state dimensions. This is utilised by (and follows in\
@@ -39,20 +48,25 @@ class SimpleRadar(Sensor):
                                 :class:`~.RangeBearingGaussianToCartesian`\
                                 model")
 
-    def __init__(self, position, ndim_state, mapping, noise_covar,
+    def __init__(self, position, orientation, ndim_state, mapping, noise_covar,
                  *args, **kwargs):
         measurement_model = RangeBearingGaussianToCartesian(
             ndim_state=ndim_state,
             mapping=mapping,
             noise_covar=noise_covar,
-            origin_offset=position)
+            translation_offset=position,
+            rotation_offset=orientation)
 
-        super().__init__(position, ndim_state, mapping, noise_covar,
-                         measurement_model, *args, **kwargs)
+        super().__init__(position, orientation, ndim_state, mapping,
+                         noise_covar, measurement_model, *args, **kwargs)
 
     def set_position(self, position):
         self.position = position
-        self.measurement_model.origin_offset = position
+        self.measurement_model.translation_offset = position
+
+    def set_orientation(self, orientation):
+        self.orientation = orientation
+        self.measurement_model.axis_roation = orientation
 
     def gen_measurement(self, ground_truth, **kwargs):
         """Generate a measurement for a given state
