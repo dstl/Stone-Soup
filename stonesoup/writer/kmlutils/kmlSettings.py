27c58520673df8b1395264505fd3f92469ee6ddf
Mamoon Rashid <m3rashid@gmail.com>
2018-12-20 17:40:47 -0500

    Incorporated simplekml but not yet tested.

--- stonesoup/writer/kmlutils/kmlSettings.py
+++ stonesoup/writer/kmlutils/kmlSettings.py
@@ -1,113 +1,40 @@
 
-KML_SCHEMA_FILE = "ogckml22.xsd"
-
-GOOGLE_KML_SCHEMA_FILE = "kml22gx.xsd"
-
-
-NS_MAP = {
-        None: 'http://www.opengis.net/kml/2.2',
-        'gx': 'http://www.google.com/kml/ext/2.2',
-}
-
-
-# Style directory  name
-STYLE_DIR = "styles"
-
-# Document path in KML file.
-DOC_PATH = "/x:kml/x:Document"
-
 # Document Name
 DOC_NAME = "Stone Soup Output"
 
-
-
-# Folder name for AIS tracks
+# Folder name for tracks
 TRACKS_FOLDER_NAME = 'All Stone Soup Tracks'
 
 
-# KML track style nodes as string
-STONE_SOUP_TRACK_STYLE_NODES_AS_STR = '''
-	<SS_KML_STYLE_FORMAT>
-	<Style id="{KML_STYLE_ID}_n">
-      <IconStyle>
-        <color>{KML_STYLE_COLOR}</color>
-        <scale>0.5</scale>
-        <Icon>
-          <href>http://earth.google.com/images/kml-icons/track-directional/track-none.png</href>
-        </Icon>
-      </IconStyle>
-    </Style>
-    <Style id="{KML_STYLE_ID}_h">
-      <IconStyle>
-        <color>{KML_STYLE_COLOR}</color>
-        <scale>1.2</scale>
-        <Icon>
-          <href>http://earth.google.com/images/kml-icons/track-directional/track-none.png</href>
-        </Icon>
-      </IconStyle>
-    </Style>
-    <StyleMap id="{KML_STYLE_ID}">
-      <Pair>
-        <key>normal</key>
-        <styleUrl>#{KML_STYLE_ID}_n</styleUrl>
-      </Pair>
-      <Pair>
-        <key>highlight</key>
-        <styleUrl>#{KML_STYLE_ID}_h</styleUrl>
-      </Pair>
-    </StyleMap>
-    <Style id="{KML_STYLE_ID}_L">
-      <LineStyle>
-        <color>{KML_STYLE_COLOR}</color>
-        <width>6</width>
-      </LineStyle>
-    </Style>
-	</SS_KML_STYLE_FORMAT>
-'''
-
-# KML detection style nodes as string
-STONE_SOUP_DETECTION_STYLE_NODES_AS_STR = '''
-	<SS_KML_STYLE_FORMAT>
-	<Style id="{KML_STYLE_ID}_n">
-      <IconStyle>
-        <color>{KML_STYLE_COLOR}</color>
-        <scale>0.5</scale>
-        <Icon>
-          <href>http://earth.google.com/images/kml-icons/track-directional/track-none.png</href>
-        </Icon>
-      </IconStyle>
-    </Style>
-    <Style id="{KML_STYLE_ID}_h">
-      <IconStyle>
-        <color>{KML_STYLE_COLOR}</color>
-        <scale>1.2</scale>
-        <Icon>
-          <href>http://earth.google.com/images/kml-icons/track-directional/track-none.png</href>
-        </Icon>
-      </IconStyle>
-    </Style>
-    <StyleMap id="{KML_STYLE_ID}">
-      <Pair>
-        <key>normal</key>
-        <styleUrl>#{KML_STYLE_ID}_n</styleUrl>
-      </Pair>
-      <Pair>
-        <key>highlight</key>
-        <styleUrl>#{KML_STYLE_ID}_h</styleUrl>
-      </Pair>
-    </StyleMap>
-	</SS_KML_STYLE_FORMAT>
-'''
+# Folder name for  detections
+DETECTIONS_FOLDER_NAME = 'All Stone Soup Detections'
 
 
-# KML track style id prefix 
-KML_TRACK_STYLE_ID_PREFIX = "SS_Style_Track"
+# Track Point Normal Icon Scale
+TRACK_POINT_NORMAL_ICON_SCALE = "0.5"
 
-# KML detection style id prefix 
-KML_DETECTION_STYLE_ID_PREFIX = "SS_Style_Detection"
+# Track Point Normal Icon HREF
+TRACK_POINT_NORMAL_ICON_HREF = "http://earth.google.com/images/kml-icons/track-directional/track-none.png"
 
-# KML style id place holder
-KML_STYLE_ID_PLACEHOLDER = "{KML_STYLE_ID}"
+# Track Point Highlight Icon Scale
+TRACK_POINT_HIGHLIGHT_ICON_SCALE = "1.2"
 
-# KML color place holder
-KML_COLOR_PLACEHOLDER = "{KML_STYLE_COLOR}"
+# Track Point Highlight Icon HREF
+TRACK_POINT_HIGHLIGHT_ICON_HREF = "http://earth.google.com/images/kml-icons/track-directional/track-none.png"
+
+# Track line width
+TRACK_LINE_WIDTH = "6"
+
+# Detection Normal Icon Scale
+DETECTION_NORMAL_ICON_SCALE = "0.5"
+
+# Detection Normal Icon HREF
+DETECTION_NORMAL_ICON_HREF = "http://earth.google.com/images/kml-icons/track-directional/track-none.png"
+
+# Detection Highlight Icon Scale
+DETECTION_HIGHLIGHT_ICON_SCALE = "1.2"
+
+# Detection Highlight Icon HREF
+DETECTION_HIGHLIGHT_ICON_HREF = "http://earth.google.com/images/kml-icons/track-directional/track-none.png"
+
+
--- stonesoup/writer/kmlutils/sskml.py
+++ stonesoup/writer/kmlutils/sskml.py
@@ -2,7 +2,7 @@ import matplotlib
 from matplotlib import cm
 from enum import Enum
 from lxml import etree
-
+from simplekml import Kml
 
 from . import kmlSettings as settings
 
@@ -11,257 +11,170 @@ class AltitudeModes(Enum):
     relativeToGround = 'relativeToGround'
     absolute = 'absolute'
 
+
 class SSKMLTrack(object):
-	def __init__(self, track_id, style_id, tks_position_array, tks_timestamps=None):
-		self.trackFolder = etree.Element('Folder')
-		nameNode = etree.SubElement(self.trackFolder, "name")
-		nameNode.text = track_id
-		
-		# Create TimeSpan node
+	def __init__(self, parent_folder, track_id, track_color, tks_position_array, tks_timestamps=None):
+		self.track_folder = parent_folder.newfolder(name=track_id)
+
+		# Ceate time stamp node
 		if tks_timestamps:
-			timeStampsSorted = sorted(tks_timestamps)
-			timeStampStart = timeStampsSorted[0]
-			timeStampEnd = timeStampsSorted[-1]
+			time_stamps_sorted = sorted(tks_timestamps)
+			time_stamps_start = time_stamps_sorted[0]
+			time_stamps_end = time_stamps_sorted[-1]
 
-			timeSpanNode = SSKMLTrack.getTimeSpanNode(timeStampStart, timeStampEnd)
-			self.trackFolder.append(timeSpanNode)
+			self.track_folder.timespan.begin = time_stamps_start.strftime('%Y-%m-%dT%H:%M:%SZ')
+			self.track_folder.timespan.end = time_stamps_end.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-		# Points folder.
-		self.pointsFolder = etree.SubElement(self.trackFolder, "Folder")
-		nameNode = etree.SubElement(self.pointsFolder, "name")
-		nameNode.text = "Points"
+
+		# Points folder
+		self.points_folder = self.track_folder.newfolder(name="Points")
 		if tks_timestamps and (len(tks_timestamps) == len(tks_position_array)):
 			for track_position, timestamp in zip(tks_position_array, tks_timestamps):
 				track_position = track_position.ravel()
-				self.pointsFolder.append(SSKMLTrack.getSSPointPlacemark(track_position[0],\
-					track_position[1], track_position[2], style_id, timestamp))
+				# Add point place mark
+				self.appendSSPointPlacemark(track_position[0], track_position[1],\
+					track_position[2],track_color, timestamp)
 		else:
 			for track_position in tks_position_array:
 				track_position = track_position.ravel()
-				self.pointsFolder.append(SSKMLTrack.getSSPointPlacemark(track_position[0],\
-					track_position[1], track_position[2], style_id))
+				# Add point placemark
+				self.appendSSPointPlacemark(track_position[0], track_position[1],\
+					track_position[2], style_id)
 
-		# Track line.
-		lineStyleID = "{}_L".format(style_id)
-		self.trackFolder.append(SSKMLTrack.getSSLinePlacemark(tks_position_array, lineStyleID))
+		# Track Line
+		self.appendSSLinePlacemark(tks_position_array, track_color)
 
-	def getTrackFolder(self):
-		return self.trackFolder
-
-	@staticmethod
-	def getTimeSpanNode(start_timestamp, end_timestamp):
-		tsNode = etree.Element("TimeStamp")
-		beginNode = etree.SubElement(tsNode, "begin")
-		beginNode.text = start_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
-		endNode = etree.SubElement(tsNode, "end")
-		endNode.text = end_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
-		return tsNode
-
-	@staticmethod
-	def getSSPointPlacemark(lat, lon, alt, style_id,\
-		timestamp=None, altitude_mode=AltitudeModes.relativeToGround):
-		pmarkNode = etree.Element("Placemark")
-		if (timestamp):
-			timestampNode = etree.SubElement(pmarkNode, "TimeStamp")
-			tsWhenNode = etree.SubElement(timestampNode, "when")
-			tsWhenNode.text = timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
-		styleURLNode = etree.SubElement(pmarkNode, "styleUrl")
-		styleURLNode.text = "#{}".format(style_id)
-		extrudeNode = etree.SubElement(pmarkNode, "extrude")
-		extrudeNode.text = "0"
-		altitudeMode = etree.SubElement(pmarkNode, "altitudeMode")
-		altitudeMode.text = altitude_mode.value
-		pointNode = etree.SubElement(pmarkNode, "Point")
-		extrudeNode = etree.SubElement(pointNode, "extrude")
-		extrudeNode.text = "0"
-		altitudeMode = etree.SubElement(pointNode, "altitudeMode")
-		altitudeMode.text = altitude_mode.value
-		coordinatesNode = etree.SubElement(pointNode, "coordinates")
-		coordinatesNode.text = "{},{},{}".format(lon, lat, alt)
-		return pmarkNode
-
-	@staticmethod
-	def getSSLinePlacemark(lla_ordered_list, style_id,\
+	def appendSSPointPlacemark(lat, lon, alt, track_color, timestamp=None,\
 		altitude_mode=AltitudeModes.relativeToGround):
-		pmarkNode = etree.Element("Placemark")
-		nameNode = etree.SubElement(pmarkNode, "name")
-		nameNode.text = "Path"
-		styleURLNode = etree.SubElement(pmarkNode, "styleUrl")
-		styleURLNode.text = "#{}".format(style_id)
-		extrudeNode = etree.SubElement(pmarkNode, "extrude")
-		extrudeNode.text = "0"
-		altitudeMode = etree.SubElement(pmarkNode, "altitudeMode")
-		altitudeMode.text = altitude_mode.value
-		lineStringNode = etree.SubElement(pmarkNode, "LineString")
-		# tessellateNode = etree.SubElement(lineStringNode, "tessellate")
-		# tessellateNode.text = "1"
-		extrudeNode = etree.SubElement(lineStringNode, "extrude")
-		extrudeNode.text = "0"
-		altitudeMode = etree.SubElement(lineStringNode, "altitudeMode")
-		altitudeMode.text = altitude_mode.value
-		coordinatesNode = etree.SubElement(lineStringNode, "coordinates")
-		coordStr = ''.join(["{},{},{} ".format(lla_pnt.ravel()[1], lla_pnt.ravel()[0],\
-		 lla_pnt.ravel()[2]) for lla_pnt in lla_ordered_list])
-		coordStr = coordStr.rstrip()
-		coordinatesNode.text = coordStr
-		return pmarkNode
+		point_placemark = self.points_folder.newpoint()
+		if (timestamp):
+			point_placemark.timestamp.when = timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
+		point_placemark.extrude = "0"
+		point_placemark.altitudemode = altitude_mode.value
+		# Add styles
+		point_placemark.stylemap.highlightstyle.iconstyle.scale = settings.TRACK_POINT_HIGHLIGHT_ICON_SCALE
+		point_placemark.stylemap.highlightstyle.iconstyle.icon.href = settings.TRACK_POINT_NORMAL_ICON_HREF
+		point_placemark.stylemap.highlightstyle.iconstyle.color  = track_color
+		point_placemark.stylemap.normalstyle.iconstyle.scale = settings.TRACK_POINT_NORMAL_ICON_SCALE
+		point_placemark.stylemap.normalstyle.iconstyle.icon.href = settings.TRACK_POINT_HIGHLIGHT_ICON_HREF
+		point_placemark.stylemap.normalstyle.iconstyle.color = track_color
+		point_placemark.coords = "{},{},{}".format(lon, lat, alt)
 
+	def appendSSLinePlacemark(lla_ordered_list, track_color, altitude_mode=AltitudeModes.relativeToGround):
+		line_placemark = self.track_folder.newlinestring()
+		# Add line style
+		line_placemark.style.linestyle.color = track_color
+		line_placemark.style.linestyle.width = settings.TRACK_LINE_WIDTH 
+
+		line_placemark.extrude = "0"
+		line_placemark.altitudemode = altitude_mode.value
+		# Add line coordinates
+		coord_str = ''.join(["{},{},{} ".format(lla_pnt.ravel()[1], lla_pnt.ravel()[0],\
+			lla_pnt.ravel()[2]) for lla_pnt in lla_ordered_list])
+		coord_str = coord_str.rstrip()
+		line_placemark.coords = coord_str
 
 class SSKML(object):
 
 	def __init__(self):
-		self._kmlRoot = etree.Element('kml', nsmap=settings.NS_MAP)
-		self._kmlDoc = etree.SubElement(self._kmlRoot, "Document")
-		docName = etree.SubElement(self._kmlDoc, "name")
-		docName.text = settings.DOC_NAME
-		self._tksStyleIndex = 0
-		self._detStyleIndex = 0
-		self._tksMaxColors = 128
-		self._detMaxColors = 128
-		self._tkscmapNorm = matplotlib.colors.Normalize(vmin=1, vmax=self._tksMaxColors)
-		self._detcmapNorm = matplotlib.colors.Normalize(vmin=1, vmax=self._detMaxColors)
+		self._kmlRoot = Kml()
+		self._doc = self._kmlRoot.document
+		self._doc.name = settings.DOC_NAME
+		self._tks_style_idx = 0
+		self._det_style_idx = 0
+		self._tks_max_colors = 128
+		self._det_max_colors = 128
+		self._tks_cmap_norm = matplotlib.colors.Normalize(vmin=1, vmax=self._tks_max_colors)
+		self._det_cmap_norm = matplotlib.colors.Normalize(vmin=1, vmax=self._det_max_colors)
 
 
-	def _appendSSTrackStyleNode(self):
-		if (self._tksStyleIndex >= self._tksMaxColors):
-			styleIDPre = format("{}_{}".format(settings.KML_TRACK_STYLE_ID_PREFIX, int(self._tksMaxColors*0.5)))
-			return styleIDPre
-
-		self._tksStyleIndex += 1
-		
-		styleIDPre = format("{}_{}".format(settings.KML_TRACK_STYLE_ID_PREFIX, self._tksStyleIndex))
-		styleColorRGBA = cm.jet(self._tkscmapNorm(self._tksStyleIndex))
-		styleColorHEX = matplotlib.colors.rgb2hex(styleColorRGBA)
+	def _getDetColor(self):
+		self._det_style_idx += 1
+		color_rgba = cm.jet(self._det_cmap_norm(self._tksStyleIndex))
+		color_hex = matplotlib.colors.rgb2hex(color_rgba)
 		# Matplotlib HEX -> #RGBA, KML HEX -> #ABGR
-		styleColorKMLHEX = "#ff{}".format(styleColorHEX[6:0:-1])
-		# Generate Style Nodes
-		ssKMLStylesStr = settings.STONE_SOUP_TRACK_STYLE_NODES_AS_STR
-		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_STYLE_ID_PLACEHOLDER, styleIDPre)
-		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_COLOR_PLACEHOLDER, styleColorKMLHEX)
-		ssStyleNodes = etree.fromstring(ssKMLStylesStr)
-		styleNodes = ssStyleNodes.getchildren()
-		[self._kmlDoc.append(sNode) for sNode in styleNodes]
-		return styleIDPre
+		color_hex_kml = "#ff{}".format(color_hex[6:0:-1])
+		return color_hex_kml
 
-	def _appendSSDetectionStyleNode(self):
-		if (self._detStyleIndex >= self._detMaxColors):
-			styleIDPre = format("{}_{}".format(settings.KML_TRACK_STYLE_ID_PREFIX, int(self._detMaxColors*0.5)))
-			return styleIDPre
-
-		self._detStyleIndex += 1
-		
-		styleIDPre = format("{}_{}".format(settings.KML_DETECTION_STYLE_ID_PREFIX, self._detStyleIndex))
-		styleColorRGBA = cm.jet(self._detcmapNorm(self._detStyleIndex))
-		styleColorHEX = matplotlib.colors.rgb2hex(styleColorRGBA)
+	def _getTrackColor(self):
+		self._tks_style_idx += 1
+		color_rgba = cm.jet(self._tks_cmap_norm(self._det_style_idx))
+		color_hex = matplotlib.colors.rgb2hex(color_rgba)
 		# Matplotlib HEX -> #RGBA, KML HEX -> #ABGR
-		styleColorKMLHEX = "#ff{}".format(styleColorHEX[6:0:-1])
-		# Generate Style Nodes
-		ssKMLStylesStr = settings.STONE_SOUP_DETECTION_STYLE_NODES_AS_STR
-		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_STYLE_ID_PLACEHOLDER, styleIDPre)
-		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_COLOR_PLACEHOLDER, styleColorKMLHEX)
-		ssStyleNodes = etree.fromstring(ssKMLStylesStr)
-		styleNodes = ssStyleNodes.getchildren()
-		[self._kmlDoc.append(sNode) for sNode in styleNodes]
-		return styleIDPre
+		color_hex_kml = "#ff{}".format(color_hex[6:0:-1])
+		return color_hex_kml
 
-	@staticmethod
-	def getKMLFolderWithName(name):
-		folderNode = etree.Element("Folder")
-		nameNode = etree.SubElement(folderNode, "name")
-		nameNode.text = name
-		return folderNode
-
-	# def _appendSSStyles(self, doc_node, color_count = 5):
-	# 	norm = matplotlib.colors.Normalize(vmin=1, vmax=colour_count)
-	# 	for k in range(1, colour_count):
-	# 		styleIDPre = format("{}_{}".format(settings.KML_STYLE_ID_PREFIX, k))
-	# 		styleColorRGBA = cm.jet(norm(k))
-	# 		styleColorHEX = matplotlib.colors.rgb2hex(styleColorRGBA)
-	# 		# Matplotlib HEX -> #RGBA, KML HEX -> #ABGR
-	# 		styleColorKMLHEX = "#ff{}".format(styleColorHEX[6:0:-1])
-	# 		# Generate Style Nodes
-	# 		ssKMLStylesStr = settings.STONE_SOUP_STYLE_NODES_AS_STR
-	# 		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_STYLE_ID_PLACEHOLDER, styleIDPre)
-	# 		ssKMLStylesStr = ssKMLStylesStr.replace(settings.KML_COLOR_PLACEHOLDER, styleColorKMLHEX)
-	# 		ssStyleNodes = etree.fromstring(ssKMLStylesStr)
-	# 		styleNodes = ssStyleNodes.getchildren()
-	# 		[doc_node.append(sNode) for sNode in styleNodes]
-
-
-
-	def _appendFolderToDocNode(self, folder_name="Stone Soup Tracks"):
-		tksFolder = etree.SubElement(self._kmlDoc, "Folder")
-		tksFolderName = etree.SubElement(tksFolder, "name")
-		tksFolderName.text = folder_name
-		return tksFolder
-
-
-	def _ssKMLDocFactory(self, num_tracks=5):
-		kmlRoot = etree.Element('kml', nsmap=settings.NS_MAP)
-		kmlDoc = etree.SubElement(kmlRoot, "Document")
-		docName = etree.SubElement(kmlDoc, "name")
-		docName.text = settings.DOC_NAME
-		self._numStyles = appendSSStyles(kmlDoc, num_tracks)
-		
 	def appendTracks(self, tks_position_matrix, tks_ids, tks_timestamp_matrix=None):
-		tksFolder = self._appendFolderToDocNode("Stone Soup Tracks")
-		numTracks = len(tks_position_matrix)
-		self._tksMaxColors = numTracks
-		self._tkscmapNorm = matplotlib.colors.Normalize(vmin=1, vmax=self._tksMaxColors)
+		tks_folder = self._doc.newfolder(name=settings.TRACKS_FOLDER_NAME)
+		num_tracks = len(tks_position_matrix)
+		self._tks_max_colors = num_tracks
+		self._tks_cmap_norm = matplotlib.colors.Normalize(vmin=1, vmax=self._tks_max_colors)
 		if (tks_timestamp_matrix):
 			for track_positions, track_id, track_timestamps in zip(tks_position_matrix, tks_ids, tks_timestamp_matrix):
-				style_id = self._appendSSTrackStyleNode()
-				ssKMLTrack = SSKMLTrack(track_id, style_id, track_positions, track_timestamps)
-				tksFolder.append(ssKMLTrack.getTrackFolder())
+				track_color_str = self._getTrackColor()
+				SSKMLTrack(tks_folder, track_id, track_color_str, track_positions, track_timestamps)
 		else:
 			for track_positions, track_id in zip(tks_position_matrix, tks_ids):
-				style_id = self._appendSSTrackStyleNode()
-				ssKMLTrack = SSKMLTrack(track_id, style_id, track_positions, track_timestamps)
-				tksFolder.append(ssKMLTrack.getTrackFolder())
-		self._kmlDoc.append(tksFolder)
+				track_color_str = self._getTrackColor()
+				SSKMLTrack(tks_folder, track_id, track_color_str, track_positions)
+				
+
 
 
 	def appendDetections(self, det_position_array, det_time_array=None):
-		ssDetFolder = self._appendFolderToDocNode("Stone Soup Detections")
-		numDetections = len(det_position_array)
-		self._detMaxColors = numDetections
-		self._detcmapNorm = matplotlib.colors.Normalize(vmin=1, vmax=self._detMaxColors)
+		detections_folder = self._doc.newfolder(name=settings.DETECTIONS_FOLDER_NAME)
+		num_detections = len(det_position_array)
+		self._det_max_colors = num_detections
+		self._det_cmap_norm = matplotlib.colors.Normalize(vmin=1, vmax=self._det_max_colors)
 
 		if (det_time_array is not None):
 			k = 1
 			for det_position, det_timestamp in zip(det_position_array, det_time_array):
 				det_position = det_position.ravel()
-				style_id = self._appendSSDetectionStyleNode()
-				detPntFolder = SSKML.getKMLFolderWithName("Detection_{}".format(k))
-				ssKMLDet = SSKMLTrack.getSSPointPlacemark(det_position[0], det_position[1],\
-					det_position[2], style_id, det_timestamp)
-				detPntFolder.append(ssKMLDet)
-				ssDetFolder.append(detPntFolder)
+				det_color_str = self._getDetColor()
+				det_folder = detections_folder.newfolder(name="Detection_{}".format(k))
+				det_placemark = det_folder.newpoint()
+				det_placemark.timestamp.when = det_timestamp.strftime('%Y-%m-%dT%H:%M:%SZ')
+				det_placemark.extrude = "0"
+				det_placemark.altitudemode = AltitudeModes.relativeToGround.value
+				# Add styles
+				det_placemark.stylemap.highlightstyle.iconstyle.scale = settings.DETECTION_HIGHLIGHT_ICON_SCALE
+				det_placemark.stylemap.highlightstyle.iconstyle.icon.href = settings.DETECTION_HIGHLIGHT_ICON_HREF
+				det_placemark.stylemap.highlightstyle.iconstyle.color  = det_color_str
+				det_placemark.stylemap.normalstyle.iconstyle.scale = settings.DETECTION_NORMAL_ICON_SCALE
+				det_placemark.stylemap.normalstyle.iconstyle.icon.href = settings.DETECTION_NORMAL_ICON_HREF
+				det_placemark.stylemap.normalstyle.iconstyle.color = det_color_str
+
+				# Update coordinates
+				det_placemark.coords = "{},{},{}".format(det_position[1], det_position[0], det_position[2])
 				k += 1
 		else:
 			k = 1
 			for det_position in det_position_array:
 				det_position = det_position.ravel()
-				style_id = self._appendSSDetectionStyleNode()
-				detPntFolder = SSKML.getKMLFolderWithName("Detection_{}".format(k))
-				ssKMLDet = SSKMLTrack.getSSPointPlacemark(det_position[0], det_position[1],\
-					det_position[2], style_id)
-				detPntFolder.append(ssKMLDet)
-				ssDetFolder.append(detPntFolder)
+				det_color_str = self._getDetColor()
+				det_folder = detections_folder.newfolder(name="Detection_{}".format(k))
+				det_placemark = det_folder.newpoint()
+				det_placemark.extrude = "0"
+				det_placemark.altitudemode = AltitudeModes.relativeToGround.value
+				# Add styles
+				det_placemark.stylemap.highlightstyle.iconstyle.scale = settings.DETECTION_HIGHLIGHT_ICON_SCALE
+				det_placemark.stylemap.highlightstyle.iconstyle.icon.href = settings.DETECTION_HIGHLIGHT_ICON_HREF
+				det_placemark.stylemap.highlightstyle.iconstyle.color  = det_color_str
+				det_placemark.stylemap.normalstyle.iconstyle.scale = settings.DETECTION_NORMAL_ICON_SCALE
+				det_placemark.stylemap.normalstyle.iconstyle.icon.href = settings.DETECTION_NORMAL_ICON_HREF
+				det_placemark.stylemap.normalstyle.iconstyle.color = det_color_str
+
+				# Update coordinates
+				det_placemark.coords = "{},{},{}".format(det_position[1], det_position[0], det_position[2])
 				k += 1
 
+
+
+
 	def write(self, output_path):
-		# Validate
+		self._kmlRoot.save(str(output_path)
 
-		# Write output
-		et = etree.ElementTree(self._kmlRoot)
-		et.write(str(output_path), pretty_print=True, xml_declaration=True, encoding='UTF-8')
-		# with open(str(output_path), 'w') as f:
-		# 	f.write(etree.tostring(self._kmlRoot, pretty_print = True,
-		# 		xml_declaration=True, encoding='UTF-8', method='xml'))
-	
 
-	def _validate(self):
+	def validate(self):
 		pass
-
-
