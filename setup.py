b2b82daab3303a7a38ec3928f4f60e719949c92e
Mamoon Rashid <m3rashid@gmail.com>
2019-01-08 16:44:33 -0500

    Added 'lxml' dependency for NMEA reader class.

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(name='stonesoup',
       install_requires=['ruamel.yaml>=0.15.45', 'scipy', 'matplotlib'],
       extras_require={
           'dev': [
-              'pytest-flake8', 'pytest-cov', 'Sphinx', 'sphinx_rtd_theme'],
+              'pytest-flake8', 'pytest-cov', 'Sphinx', 'sphinx_rtd_theme',
+              'setuptools>=30', 'pymap3d', 'lxml'],
       },
       )
