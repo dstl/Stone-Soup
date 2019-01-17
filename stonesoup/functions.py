63d0e3a39b666fea7b031f8c8a1e89948a40d097
YIFAN ZHOU <zhouyifan1990@gmail.com>
2019-01-17 12:02:57 +0000

    Add Sunday by Yifan
    Signed-off-by: YIFAN ZHOU <zhouyifan1990@gmail.com>

--- stonesoup/functions.py
+++ stonesoup/functions.py
@@ -483,5 +483,6 @@ def dayOfTheWeek(number):
         day = "Nope"
     # TODO:
     #   Add your code below, possibly starting with "else if....."
-
+    elif number == 7:
+        day = "Sunday"
     return day
