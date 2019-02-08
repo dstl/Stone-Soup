49970d0d766a2a2dee826d81f939b63919b6f076
Sam Thomas <sthomas@dstl.gov.uk>
2019-02-08 15:54:53 +0000

    Implimented metrics structure along with basic metrics, OSPA distance and most of the SIAP metrics

--- stonesoup/metricgenerator/__init__.py
+++ stonesoup/metricgenerator/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
 from .base import MetricGenerator
+from .basicmetrics import *  # noqa:F401,F403
+from .plotter import *  # noqa:F401,F403
+from .manager import *  # noqa:F401,F403
 
 __all__ = ['MetricGenerator']
