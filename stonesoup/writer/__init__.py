57c72bd603e28e0489a85c3e900db9d2860efab4
Mamoon Rashid <m3rashid@gmail.com>
2019-02-12 15:01:07 -0500

    Adding kml.py and kmlutils for writing SS tracks and detections to KML.

--- stonesoup/writer/__init__.py
+++ stonesoup/writer/__init__.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from .base import Writer, MetricsWriter, TrackWriter
 from .yaml import *  # noqa:F401,F403
+from .kml import KMLTrackWriter, KMLMetricsWriter, KMLWriter, CoordinateSystems
 
-__all__ = ['MetricsWriter', 'TrackWriter']
+__all__ = ['MetricsWriter', 'TrackWriter', 'KMLTrackWriter',
+           'CoordinateSystems']
 __all__.extend(subclass_.__name__ for subclass_ in Writer.subclasses)
