9bb7ca2e679f7bef505cdaaf291a555b0b14f474
Mamoon Rashid <m3rashid@gmail.com>
2019-02-14 11:01:44 -0500

    Added test for KMLTrackWriter class.

--- stonesoup/writer/tests/test_kml.py
+++ stonesoup/writer/tests/test_kml.py
@@ -1,154 +1,18 @@
-# -*- coding: utf-8 -*-
-import sys
-from textwrap import dedent
-
-import pytest
-
-from ..kml import KMLMetricsWriter, KMLTrackWriter, KMLWriter
+"""Tests for KML writer classes."""
+import os
 
 
-dict_order_skip = pytest.mark.skipif(
-    sys.version_info < (3, 6), reason="requires Python >= 3.6 for dict order")
+from ..kml import KMLTrackWriter, CoordinateSystems
 
 
-@dict_order_skip
-def test_detections_yaml(detection_reader, tmpdir):
-    filename = tmpdir.join("detections.yaml")
-
-    with YAMLWriter(filename.strpath,
-                    detections_source=detection_reader) as writer:
-        writer.write()
-
-    with filename.open('r') as yaml_file:
-        generated_yaml = yaml_file.read()
-
-    expected_yaml = dedent("""\
-         ---
-         time: 2018-01-01 14:00:00
-         detections: !!set {}
-         ...
-         ---
-         time: &id001 2018-01-01 14:01:00
-         detections: !!set
-           ? !stonesoup.types.detection.Detection
-           - state_vector: !numpy.ndarray
-             - [1]
-           - timestamp: *id001
-           - metadata: {}
-           :
-         ...
-         ---
-         time: &id001 2018-01-01 14:02:00
-         detections: !!set
-           ? !stonesoup.types.detection.Detection
-           - state_vector: !numpy.ndarray
-             - [2]
-           - timestamp: *id001
-           - metadata: {}
-           :
-           ? !stonesoup.types.detection.Detection
-           - state_vector: !numpy.ndarray
-             - [2]
-           - timestamp: *id001
-           - metadata: {}
-           :
-         ...
-         """)
-
-    assert generated_yaml == expected_yaml
-
-
-@dict_order_skip
-def test_groundtruth_paths_yaml(groundtruth_reader, tmpdir):
-    filename = tmpdir.join("groundtruth_paths.yaml")
-
-    with YAMLWriter(filename.strpath,
-                    groundtruth_source=groundtruth_reader) as writer:
-        writer.write()
-
-    with filename.open('r') as yaml_file:
-        generated_yaml = yaml_file.read()
-
-    expected_yaml = dedent("""\
-        ---
-        time: 2018-01-01 14:00:00
-        groundtruth_paths: !!set {}
-        ...
-        ---
-        time: &id001 2018-01-01 14:01:00
-        groundtruth_paths: !!set
-          ? !stonesoup.types.groundtruth.GroundTruthPath
-          - states:
-            - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !numpy.ndarray
-                - [1]
-              - timestamp: *id001
-          :
-        ...
-        ---
-        time: &id001 2018-01-01 14:02:00
-        groundtruth_paths: !!set
-          ? !stonesoup.types.groundtruth.GroundTruthPath
-          - states:
-            - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !numpy.ndarray
-                - [2]
-              - timestamp: *id001
-            - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !numpy.ndarray
-                - [12]
-              - timestamp: *id001
-          :
-          ? !stonesoup.types.groundtruth.GroundTruthPath
-          - states:
-            - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !numpy.ndarray
-                - [2]
-              - timestamp: *id001
-            - !stonesoup.types.groundtruth.GroundTruthState
-              - state_vector: !numpy.ndarray
-                - [12]
-              - timestamp: *id001
-          :
-        ...
-        """)
-
-    assert generated_yaml == expected_yaml
-
-
-@dict_order_skip
-def test_tracks_yaml(tracker, tmpdir):
-    filename = tmpdir.join("tracks.yaml")
-
-    with YAMLWriter(filename.strpath, tracks_source=tracker) as writer:
-        writer.write()
-
-    with filename.open('r') as yaml_file:
-        generated_yaml = yaml_file.read()
-
-    expected_yaml = dedent("""\
-        ---
-        time: 2018-01-01 14:00:00
-        tracks: !!set {}
-        ...
-        ---
-        time: &id001 2018-01-01 14:01:00
-        tracks: !!set
-          ? !stonesoup.types.track.Track
-          - states:
-            - !stonesoup.types.state.State
-              - state_vector: !numpy.ndarray
-                - [1]
-              - timestamp: *id001
-          - id: '0'
-          :
-        ...
-        """)
-
-    assert generated_yaml == expected_yaml
-
-
-def test_yaml_bad_init(tmpdir):
-    filename = tmpdir.join("bad_init.yaml")
-    with pytest.raises(ValueError, match="At least one source required"):
-        YAMLWriter(filename.strpath)
+def test_kml_trackwriter(ss_tracker_obj, tmpdir,
+                         tks_coord_system=CoordinateSystems.LLA,
+                         coord_ref_point=(0.0, 0.0, 0.0)):
+    """Test for KMLTrackWriter."""
+    output_filename = tmpdir.join("ss_output.kml")
+    kml_track_writer = KMLTrackWriter(ss_tracker_obj, output_filename,
+                                      coordinate_system=tks_coord_system,
+                                      reference_point=coord_ref_point)
+    kml_track_writer.write()
+    # Check if the file was created.
+    assert os.path.isfile(output_filename)
