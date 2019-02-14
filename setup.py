9eec7fc39b2d65600806cb608bb76c8e09b17148
Mamoon Rashid <m3rashid@gmail.com>
2019-02-14 12:52:43 -0500

    Added dependencies in setup.pr from KML writer classes.

--- setup.py
+++ setup.py
@@ -10,10 +10,11 @@ setup(name='stonesoup',
       description='A target tracking development/testing framework',
       url='https://github.com/dstl/Stone-Soup',
       packages=find_packages(exclude=('docs', '*.tests')),
-      install_requires=['ruamel.yaml>=0.15.45', 'scipy', 'matplotlib'],
+      install_requires=['ruamel.yaml>=0.15.45', 'scipy', 'matplotlib',
+                        'lxml', 'simplekml>=1.3.1'],
       extras_require={
           'dev': [
               'pytest-flake8', 'pytest-cov', 'Sphinx', 'sphinx_rtd_theme',
-              'setuptools>=30'],
+              'setuptools>=30', 'pymap3d'],
       },
       )
