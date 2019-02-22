67fc6235b7476eb12fd08fa0014783d267723d2d
Mamoon Rashid <m3rashid@gmail.com>
2019-02-22 17:10:59 -0500

    Removed unimplemented classes from __init__.py.

--- stonesoup/writer/__init__.py
+++ stonesoup/writer/__init__.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from .base import Writer, MetricsWriter, TrackWriter
 from .yaml import *  # noqa:F401,F403
-from .kml import KMLTrackWriter, KMLMetricsWriter, KMLWriter, CoordinateSystems
+from .kml import KMLTrackWriter, CoordinateSystems
 
-__all__ = ['MetricsWriter', 'TrackWriter', 'KMLTrackWriter',
-           'CoordinateSystems']
+__all__ = ['KMLTrackWriter', 'CoordinateSystems']
 __all__.extend(subclass_.__name__ for subclass_ in Writer.subclasses)
