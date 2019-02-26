941d96a29819a50a98702d54d96d79c160f05972
jmbarr <jmbarr@dstl.gov.uk>
2019-02-26 10:43:49 +0000

    bugfix in predictor treatment of time intervals

--- stonesoup/predictor/kalman.py
+++ stonesoup/predictor/kalman.py
@@ -52,12 +52,12 @@ class AbstractKalmanPredictor(Predictor):
                                                     np.zeros(prior.covar.shape))
 
         # TODO time interval in the control model?
-        x_pred = self.transition_function(prior.state_vector, timestamp=timestamp) + \
+        x_pred = self.transition_function(prior.state_vector, time_interval=self.timestamp-prior.timestamp) + \
                  self.control_function(self.control_model.control_vector)
 
         # As this is Kalman-like, the control model must be capable of returning a control matrix (B)
-        P_pred = self.transition_matrix(time_interval=timestamp-prior.timestamp) @ prior.covariance @ \
-                 self.transition_matrix(time_interval=timestamp-prior.timestamp).T + \
+        P_pred = self.transition_matrix(time_interval=self.timestamp-prior.timestamp) @ prior.covariance @ \
+                 self.transition_matrix(time_interval=self.timestamp-prior.timestamp).T + \
                  self.transition_model.covar() + \
                  self.control_matrix() @ self.control_model.control_noise @ self.control_matrix().T
 
