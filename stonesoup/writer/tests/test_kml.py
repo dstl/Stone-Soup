6fa83362f612b35438c6ede45ffe30efc79b8a8d
Mamoon Rashid <m3rashid@gmail.com>
2019-02-27 11:55:27 -0500

    Fixed test_kml.py.

--- stonesoup/writer/tests/test_kml.py
+++ stonesoup/writer/tests/test_kml.py
@@ -5,12 +5,12 @@ import os
 from ..kml import KMLTrackWriter, CoordinateSystems
 
 
-def test_kml_trackwriter(ss_tracker_obj, tmpdir,
+def test_kml_trackwriter(tracker, tmpdir,
                          tracker_coord_system=CoordinateSystems.LLA,
                          coord_ref_point=(0.0, 0.0, 0.0)):
     """Test for KMLTrackWriter."""
     output_filename = tmpdir.join("ss_output.kml")
-    kml_track_writer = KMLTrackWriter(ss_tracker_obj, str(output_filename),
+    kml_track_writer = KMLTrackWriter(tracker, str(output_filename),
                                       coordinate_system=tracker_coord_system,
                                       reference_point=coord_ref_point)
     kml_track_writer.write()
