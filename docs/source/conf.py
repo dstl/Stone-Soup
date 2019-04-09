4ab829ed569b248c92915f653bac9df86ac02e89
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2019-04-09 13:13:04 +0100

    Update LICENSE and documentation copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Stone Soup'
-copyright = '2017, Dstl and Stone Soup contributors'
+copyright = '2017-2019 Stone Soup contributors'
 author = 'Dstl'
 
 # The version info for the project you're documenting, acts as replacement for
--- docs/source/copyright.rst
+++ docs/source/copyright.rst
@@ -1,5 +1,5 @@
-Copyright
-=====================
+Copyright & License
+===================
 
 The Stone Soup software is released under the following license by copyright
 holders listed:
