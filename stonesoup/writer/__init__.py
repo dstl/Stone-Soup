89aab1350c487424e0fd4d69e7ee24b8fedc238c
Mamoon Rashid <m3rashid@gmail.com>
2019-05-16 13:08:09 -0400

    Fixed flake8 issues.

--- stonesoup/writer/__init__.py
+++ stonesoup/writer/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .base import Writer
 from .yaml import *  # noqa:F401,F403
-from .kml import *  # noqa:F401,F403
+from .kml import CoordinateSystems, KMLTrackWriter  # noqa:F401,F403
 
 __all__ = ['Writer', 'CoordinateSystems']
 __all__.extend(subclass_.__name__ for subclass_ in Writer.subclasses)
