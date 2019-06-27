c48873857df9aab95729dc365956bfd6362a284c
declan <dcrew@dstl.gov.uk>
2019-06-27 11:23:25 +0100

    Add documentation, remove unneeded arg passing on __iter__

--- stonesoup/buffered_generator.py
+++ stonesoup/buffered_generator.py
@@ -1,4 +1,35 @@
 class BufferedGenerator:
+    """
+    Converts a class with a generator method into a buffered generator.
+    The generator method to be used is decorated using
+    BufferedGenerator.generator_method. This can then be used as expected
+
+    .. code-block:: python
+
+        class Foo(BufferedGenerator):
+            '''
+            Example Foo generator
+            '''
+
+            @BufferedGenerator.generator_method
+            def count_to_ten(self):
+                "Counts to ten"
+                for i in range(10):
+                    yield i + 1
+
+        foo = Foo()
+        for i in foo:
+            print(i)
+
+    The current state of the generator is available using the 'current'
+    attribute.
+
+    .. code-block:: python
+        foo = Foo()
+        for i in foo:
+            print(i)
+            print(foo.current)
+    """
     @staticmethod
     def generator_method(method):
         method.is_generator = True
@@ -9,10 +40,10 @@ class BufferedGenerator:
             if callable(item):
                 yield item
 
-    def __iter__(self, *args, **kwargs):
+    def __iter__(self):
         for method in self._get_methods():
             if hasattr(method, 'is_generator'):
-                self._gen = method(*args, **kwargs)
+                self._gen = method()
                 return self
 
     def __next__(self):
