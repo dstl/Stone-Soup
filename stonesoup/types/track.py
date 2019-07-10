3fa01ee82d80ab9707d7c9b7b384baad076c7af0
sglvladi <sglvladi@gmail.com>
2019-07-10 23:54:35 +0100

    Modify Track class to maintain an explicit copy of its metadata

--- stonesoup/types/track.py
+++ stonesoup/types/track.py
@@ -2,6 +2,7 @@
 import uuid
 
 from ..base import Property
+from .multihypothesis import MultipleHypothesis
 from .state import State, StateMutableSequence
 from .update import Update
 
@@ -25,8 +26,22 @@ class Track(StateMutableSequence):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # Initialise metadata
+        self._metadata = {}
+        for state in self.states:
+            self._update_metadata_from_state(state)
         if self.id is None:
             self.id = str(uuid.uuid4())
+
+    def __setitem__(self, index, value):
+        # Update metadata
+        self._update_metadata_from_state(value)
+        return super().__setitem__(index, value)
+
+    def insert(self, index, value):
+        # Update metadata
+        self._update_metadata_from_state(value)
+        return super().insert(index, value)
 
     @property
     def metadata(self):
@@ -42,11 +57,33 @@ class Track(StateMutableSequence):
             All metadata associate with this track.
         """
 
-        metadata = {}
+        return self._metadata
 
-        for state in self:
-            if isinstance(state, Update) \
-                    and state.hypothesis.measurement.metadata is not None:
-                metadata.update(state.hypothesis.measurement.metadata)
+    def _update_metadata_from_state(self, state):
+        """ Extract and update track metadata, given a state
 
-        return metadata
+        Parameters
+        ----------
+        state: State
+            A state object from which to extract metadata. Metadata can only
+            be extracted from Update (or subclassed) objects. Calling this
+            method with a non-Update (subclass) object will NOT return an
+            error, but will have no effect on the metadata.
+
+        """
+
+        if isinstance(state, Update):
+            if isinstance(state.hypothesis, MultipleHypothesis):
+                # Sort and iterate through multiple hypotheses such that most
+                # likely hypothesis comes last. This ensures that metadata
+                # from all hypotheses are retained, but more likely
+                # hypotheses will over-write the metadata set by less likely
+                # ones.
+                for hypothesis in sorted(state.hypothesis, reverse=True):
+                    if hypothesis \
+                            and hypothesis.measurement.metadata is not None:
+                        self._metadata.update(hypothesis.measurement.metadata)
+            else:
+                hypothesis = state.hypothesis
+                if hypothesis and hypothesis.measurement.metadata is not None:
+                    self._metadata.update(hypothesis.measurement.metadata)
