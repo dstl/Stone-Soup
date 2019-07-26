48ad1f4770925359ca85533ab0d17e19917e5b4e
lflaherty-dstl <lflaherty@dstl.gov.uk>
2019-07-26 09:11:35 +0100

    Add 2d assignment alg + new gnn

--- stonesoup/dataassociator/neighbour.py
+++ stonesoup/dataassociator/neighbour.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import numpy as np
 
 from .base import DataAssociator
 from ..base import Property
 from ..hypothesiser import Hypothesiser
-
+from ..assignment import assignAlgs
+from ..types.detection import MissedDetection
+from ..types.hypothesis import SingleDistanceHypothesis
 
 class NearestNeighbour(DataAssociator):
     """Nearest Neighbour Associator
@@ -104,5 +107,74 @@ class GlobalNearestNeighbour(DataAssociator):
         # Link hypotheses into a set of joint_hypotheses and evaluate
         joint_hypotheses = self.enumerate_joint_hypotheses(hypotheses)
         associations = max(joint_hypotheses)
+        
+        return associations
 
+class GNNwith2DAssignment(DataAssociator):
+    """Global Nearest Neighbour Associator
+
+    Associates detections to a predicted state using the Global Nearest Neighbour method, 
+    utilising a 2D matrix of distances and a "shortest path" assignment algorithm.
+    """
+
+    hypothesiser = Property(
+        Hypothesiser,
+        doc="Generate a set of hypotheses for each prediction-detection pair")
+
+    def associate(self, tracks, detections, time):
+        """Associate a set of detections with predicted states.
+
+        Parameters
+        ----------
+        tracks : set of :class:`Track`
+            Current tracked objects
+        detections : set of :class:`Detection`
+            Retrieved measurements
+        time : datetime
+            Detection time to predict to
+
+        Returns
+        -------
+        dict
+            Key value pair of tracks with associated detection
+        """
+        
+        # Convert sets to indexable lists
+        tracks = list(tracks)
+        detections = list(detections)
+        
+        # Generate 2d array "matrix" of hypotheses mapping track to detection
+        hypothesis_matrix = np.empty([len(tracks), len(detections) + len(tracks)], SingleDistanceHypothesis)
+        for i in range(len(tracks)):
+            row = np.empty([len(detections) + len(tracks)], SingleDistanceHypothesis)
+            track_hypotheses = self.hypothesiser.hypothesise(tracks[i], detections, time)
+            for hypothesis in track_hypotheses:
+                if isinstance(hypothesis.measurement, MissedDetection):
+                    row[len(detections) + i] = hypothesis
+                else:
+                    row[int(detections.index(hypothesis.measurement))] = hypothesis
+            hypothesis_matrix[i] = row
+        
+        # Generate 2d array "matrix" of distances
+        distance_matrix = np.empty([len(tracks), len(detections) + len(tracks)])
+        for x in range(hypothesis_matrix.shape[0]):
+            for y in range(hypothesis_matrix.shape[1]):
+                if hypothesis_matrix[x][y] is None:
+                    distance_matrix[x][y] = np.inf
+                else:
+                    distance_matrix[x][y] = hypothesis_matrix[x][y].distance
+        
+        # Use "shortest path" assignment algorithm on distance matrix to assign tracks to nearest detection
+        gain, col4row, row4col = assignAlgs.assign2D(distance_matrix)
+        
+        # Ensure the problem was feasible
+        if gain == np.empty(0):
+            print("IMPOSSIBLE ASSIGNMENT")
+            return
+        
+        # Generate dict of key/value pairs
+        associations = {}
+        for j in range(len(col4row)):
+            associations[tracks[j]] = hypothesis_matrix[j][int(col4row[j])]
+        
         return associations
