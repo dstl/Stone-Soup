Fixed bug in calculation of Eccentric Anomaly

stonesoup/types/tests/test_orbits.py
@@ -38,13 +38,16 @@ def test_tle_orbit():
                                         [-4],
                                         [-4]])
         tle_orbit = TLEOrbitalState(orbital_state_vector, metadata={})
-    orbital_state_vector = np.array([[51.6416*(np.pi/180)],
-                                    [90*(np.pi/180)],
-                                    [0.7],
-                                    [60*(np.pi/180)],
+    orbital_state_vector = np.array([[170.6416*(np.pi/180)],
+                                    [270*(np.pi/180)],
+                                    [0.17],
+                                    [0.0*(np.pi/180)],
                                     [325*(np.pi/180)],
-                                    [0.157*(np.pi/180)]])
+                                    [0.00157*(np.pi/180)]])
+    #orbital_state_vector = np.array([[2.674], [4.456], [0.1712], [0.0], [5.3504],
+     #                                [0.0007662]])
     tle_orbit = TLEOrbitalState(orbital_state_vector, metadata={})
+    print(tle_orbit.argument_periapsis)
     tle_orbit = tle_orbit
 
 