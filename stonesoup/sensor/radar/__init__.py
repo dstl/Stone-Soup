26a6dc63a8d0ae270a59897993581c19fbebe75e
Jonathan J Osborne <jjosborne@dstl.gov.uk>
2020-01-16 14:26:26 +0000

    fixed errors

--- stonesoup/sensor/radar/__init__.py
+++ stonesoup/sensor/radar/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from .radar import RadarRangeBearing,RadarRotatingRangeBearing,AESARadar
+from .radar import RadarRangeBearing, RadarRotatingRangeBearing, AESARadar
 from .beam_shape import Beam2DGaussian, BeamShape
-from .beam_pattern import BeamTransitionModel,BeamSweep,StationaryBeam
+from .beam_pattern import BeamTransitionModel, BeamSweep, StationaryBeam
 
 
-__all__ = ['RadarRangeBearing','RadarRotatingRangeBearing','AESARadar',
-           'Beam2DGaussian', 'BeamShape', 'BeamTransitionModel','BeamSweep',
+__all__ = ['RadarRangeBearing', 'RadarRotatingRangeBearing', 'AESARadar',
+           'Beam2DGaussian', 'BeamShape', 'BeamTransitionModel', 'BeamSweep',
            'StationaryBeam']
