3a1ba00c941e444d7caf30f63cbb729986212fe2
Jack H <jharris3@dstl.gov.uk>
2020-03-16 16:27:24 +0000

    Added sensor management branch and files

---
+++ stonesoup/sensorman/base.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from abc import abstractmethod
+from ..base import Base
+import scipy as sp
+
+from stonesoup.models.base import Model
+from stonesoup.base import Property
+
+
+class SensorManModel(Base):
+    """Sensor management Model base class"""
+
+    ndim_state = Property(int, doc="Number of state dimensions")
+    mapping = Property(
+        sp.ndarray, doc="Mapping between measurement and state dims")
+
+    @property
+    def ndim(self):
+        return self.ndim_meas
+
+    @property
+    @abstractmethod
+    def ndim_meas(self):
+        """Number of measurement dimensions"""
+        pass
