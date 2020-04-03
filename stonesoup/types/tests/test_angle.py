fd5708260d2117a88286c79cc76f68a511f935e2
sglvladi <sglvladi@gmail.com>
2020-04-03 22:30:41 +0100

    Replace Class with class_ in test_angle.py

--- stonesoup/types/tests/test_angle.py
+++ stonesoup/types/tests/test_angle.py
@@ -21,30 +21,30 @@ def pytest_generate_tests(metafunc):
     metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
 
 
-scenario1 = ('bearing', {'Class': Bearing, 'func': mod_bearing})
-scenario2 = ('elevation', {'Class': Elevation, 'func': mod_elevation})
-scenario3 = ('longitude', {'Class': Longitude, 'func': mod_bearing})
-scenario4 = ('latitude', {'Class': Latitude, 'func': mod_elevation})
+scenario1 = ('bearing', {'class_': Bearing, 'func': mod_bearing})
+scenario2 = ('elevation', {'class_': Elevation, 'func': mod_elevation})
+scenario3 = ('longitude', {'class_': Longitude, 'func': mod_bearing})
+scenario4 = ('latitude', {'class_': Latitude, 'func': mod_elevation})
 
 
 class TestAngle:
     scenarios = [scenario1, scenario2, scenario3, scenario4]
     # In the following functions:
-    # Class - this is the class type we use for casting e.g. Bearing
+    # class_ - this is the class type we use for casting e.g. Bearing
     # func - this is the modulo function
 
-    def test_bearing_init(self, Class, func):
-        b1 = Class(3.14)
-        b2 = Class(deg2rad(700))
+    def test_bearing_init(self, class_, func):
+        b1 = class_(3.14)
+        b2 = class_(deg2rad(700))
         assert approx(b1) == func(3.14)
         assert approx(b2) == func(deg2rad(-20))
         assert b1 == b1
         assert b1 == b1
         assert b1 != b2
 
-    def test_multiply(self, Class, func):
-        b1 = Class(3.14)
-        b2 = Class(2)
+    def test_multiply(self, class_, func):
+        b1 = class_(3.14)
+        b2 = class_(2)
         bmul = float(b1)*float(b2)
         bmul2 = 2*float(b1)
         assert b1*b2 == approx(bmul)
@@ -52,43 +52,43 @@ class TestAngle:
         assert 2*b1 == approx(bmul2)
         assert b1*2 == approx(bmul2)
 
-    def test_addition(self, Class, func):
-        b1 = Class(3.14)
-        b2 = Class(2)
+    def test_addition(self, class_, func):
+        b1 = class_(3.14)
+        b2 = class_(2)
         b_sum = float(b1) + float(b2)
         assert approx(func(b_sum)) == b1+b2
         assert approx(func(b_sum)) == float(b1)+b2
         assert approx(func(b_sum)) == b1+float(b2)
         assert approx(func(b_sum)) == b2+b1
 
-    def test_subtraction(self, Class, func):
-        b1 = Class(3.14)
-        b2 = Class(2)
+    def test_subtraction(self, class_, func):
+        b1 = class_(3.14)
+        b2 = class_(2)
         b_diff = float(b1) - float(b2)
         assert approx(func(b_diff)) == b1-b2
         assert approx(func(b_diff)) == float(b1)-b2
         assert approx(func(b_diff)) == b1-float(b2)
         assert approx(-func(b_diff)) == b2-b1
 
-    def test_division(self, Class, func):
-        b1 = Class(3.14)
-        b2 = Class(2)
+    def test_division(self, class_, func):
+        b1 = class_(3.14)
+        b2 = class_(2)
         b_div = float(b1)/float(b2)
         assert approx(b_div) == b1/b2
         assert approx(float(b1)/2) == b1/2
         assert approx(b_div) == float(b1)/b2
 
-    def test_comaparision(self, Class, func):
-        b1 = Class(deg2rad(30))
-        b2 = Class(deg2rad(10))
+    def test_comaparision(self, class_, func):
+        b1 = class_(deg2rad(30))
+        b2 = class_(deg2rad(10))
         assert b1 == b1
         assert b1 > b2
         assert b1 >= b1
         assert b1 <= b1
         assert b2 < b1
 
-    def test_trig(self, Class, func):
-        b1 = Class(deg2rad(22.0))
+    def test_trig(self, class_, func):
+        b1 = class_(deg2rad(22.0))
         b2 = float(b1)
 
         assert np.sin(b1) == np.sin(b2)
@@ -99,8 +99,8 @@ class TestAngle:
         assert np.tanh(b1) == np.tanh(b2)
         assert np.rad2deg(b1) == np.rad2deg(b2)
 
-    def test_misc(self, Class, func):
-        b1 = Class(deg2rad(22.0))
+    def test_misc(self, class_, func):
+        b1 = class_(deg2rad(22.0))
         b2 = float(b1)
 
         assert ceil(b1) == ceil(b2)
@@ -111,6 +111,6 @@ class TestAngle:
         assert +b1 == +b2
         assert approx(-b1) == -b2
 
-    def test_degrees(self, Class, func):
-        b1 = Class(np.pi/4)  # pi/4 radians = 45 degrees
+    def test_degrees(self, class_, func):
+        b1 = class_(np.pi/4)  # pi/4 radians = 45 degrees
         assert b1.degrees == 45.0
