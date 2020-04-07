c27d186ec65754858cc8265e4633766786f69c56
Edward TF Rogers <erogers@dstl.gov.uk>
2020-04-07 14:27:34 +0100

    Add base platform movement and orientation tests

--- stonesoup/platform/tests/test_platform_base.py
+++ stonesoup/platform/tests/test_platform_base.py
@@ -2,7 +2,9 @@
 import datetime
 
 import numpy as np
+import pytest
 
+from stonesoup.types.array import StateVector
 from ...types.state import State
 from ...models.transition.linear import (
     ConstantVelocity, CombinedLinearGaussianTransitionModel)
@@ -27,6 +29,10 @@ def test_base():
     assert (np.array_equal(platform.state.state_vector, new_statevector))
     # Test to ensure platform time has updated
     assert (platform.state.timestamp == new_timestamp)
+    assert np.array_equal(platform.velocity, StateVector([0, 0]))
+    assert np.array_equal(platform.acceleration, StateVector([0, 0]))
+    assert platform.ndim == 2
+    assert not platform.is_moving
 
     # Define a static 3d platform and check it does not move
     platform_state3d = State(np.array([[2],
@@ -39,7 +45,11 @@ def test_base():
                                 [2],
                                 [2]])
     # Ensure 2d platform has not moved
-    assert (np.array_equal(platform.state.state_vector, new_statevector))
+    assert np.array_equal(platform.state.state_vector, new_statevector)
+    assert np.array_equal(platform.velocity, StateVector([0, 0, 0]))
+    assert np.array_equal(platform.acceleration, StateVector([0, 0, 0]))
+    assert platform.ndim == 3
+    assert not platform.is_moving
 
     # Define zero noise 2d constant velocity transition model
     model_1d = ConstantVelocity(0.0)
@@ -61,6 +71,11 @@ def test_base():
                                 [4],
                                 [1]])
     assert (np.array_equal(platform.state.state_vector, new_statevector))
+    assert np.array_equal(platform.velocity, StateVector([1, 1]))
+    with pytest.raises(AttributeError):
+        _ = platform.acceleration
+    assert platform.ndim == 2
+    assert platform.is_moving
 
     # Define zero noise 3d constant velocity transition model
     model_3d = CombinedLinearGaussianTransitionModel(
@@ -85,5 +100,165 @@ def test_base():
                                 [2],
                                 [1]])
     assert (np.array_equal(platform.state.state_vector, new_statevector))
+    assert np.array_equal(platform.velocity, StateVector([1, 1, 1]))
+    with pytest.raises(AttributeError):
+        _ = platform.acceleration
+    assert platform.ndim == 3
+    assert platform.is_moving
 
-    # TODO: More assertions
+
+def test_velocity_properties():
+    model_1d = ConstantVelocity(0.0)
+    model_3d = CombinedLinearGaussianTransitionModel(
+        [model_1d, model_1d, model_1d])
+    timestamp = datetime.datetime.now()
+    timediff = 2  # 2sec
+    new_timestamp = timestamp + datetime.timedelta(seconds=timediff)
+
+    platform_state = State(np.array([[2],
+                                     [0],
+                                     [2],
+                                     [0],
+                                     [0],
+                                     [0]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=model_3d, mapping=[0, 2, 4])
+    old_position = platform.position
+    assert not platform.is_moving
+    assert np.array_equal(platform.velocity, StateVector([0, 0, 0]))
+    # check it doesn't move with timestamp = None
+    platform.move(timestamp=None)
+    assert np.array_equal(platform.position, old_position)
+    # check it doesn't move (as it has zero velocity)
+    platform.move(timestamp)
+    assert np.array_equal(platform.position, old_position)
+    platform.move(new_timestamp)
+    assert np.array_equal(platform.position, old_position)
+
+    platform_state = State(np.array([[2],
+                                     [1],
+                                     [2],
+                                     [1],
+                                     [0],
+                                     [1]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=None, mapping=[0, 2, 4])
+    assert platform.is_moving
+    assert np.array_equal(platform.velocity, StateVector([1, 1, 1]))
+    old_position = platform.position
+    # check it doesn't move with timestamp = None
+    platform.move(None)
+    assert np.array_equal(platform.position, old_position)
+
+    with pytest.raises(AttributeError):
+        platform.move(timestamp)
+
+    # moving platform without velocity defined
+    platform_state = State(np.array([[2],
+                                     [2],
+                                     [0]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=None, mapping=[0, 2, 4])
+    with pytest.raises(AttributeError):
+        _ = platform.velocity
+    with pytest.raises(AttributeError):
+        _ = platform.acceleration
+
+
+orientation_tests_3d = [(StateVector([0, 1, 0, 0, 0, 0]), StateVector([0, 0, 0])),
+                        (StateVector([0, 0, 0, 0, 0, 1]), StateVector([0, 0, np.pi/2])),
+                        (StateVector([0, 0, 0, 1, 0, 0]), StateVector([0, np.pi/2, 0])),
+                        (StateVector([0, 2, 0, 0, 0, 0]), StateVector([0, 0, 0])),
+                        (StateVector([0, 0, 0, 0, 0, 2]), StateVector([0, 0, np.pi/2])),
+                        (StateVector([0, 0, 0, 2, 0, 0]), StateVector([0, np.pi/2, 0])),
+                        (StateVector([0, 1, 0, 0, 0, 1]), StateVector([0, 0, np.pi/4])),
+                        (StateVector([0, 0, 0, 1, 0, 1]), StateVector([0, np.pi/2, np.pi/4])),
+                        (StateVector([0, 1, 0, 1, 0, 1]),
+                            StateVector([0, np.pi/4, np.arctan(1/np.sqrt(2))])),
+                        (StateVector([0, -1, 0, 0, 0, 0]), StateVector([0, np.pi, 0])),
+                        (StateVector([0, 0, 0, -1, 0, 0]), StateVector([0, -np.pi/2, 0])),
+                        (StateVector([0, 0, 0, 0, 0, -1]), StateVector([0, 0, -np.pi/2])),
+                        (StateVector([0, -2, 0, 0, 0, 0]), StateVector([0, np.pi, 0])),
+                        (StateVector([0, 0, 0, -2, 0, 0]), StateVector([0, -np.pi/2, 0])),
+                        (StateVector([0, 0, 0, 0, 0, -2]), StateVector([0, 0, -np.pi/2])),
+                        (StateVector([0, -1, 0, 0, 0, -1]), StateVector([0, np.pi, -np.pi/4])),
+                        (StateVector([0, 0, 0, -1, 0, -1]), StateVector([0, -np.pi/2, -np.pi/4])),
+                        ]
+
+
+@pytest.mark.parametrize('state, orientation', orientation_tests_3d)
+def test_platform_orientation_3d(state, orientation):
+    model_1d = ConstantVelocity(0.0)
+    model_3d = CombinedLinearGaussianTransitionModel(
+        [model_1d, model_1d, model_1d])
+    timestamp = datetime.datetime.now()
+    timediff = 2  # 2sec
+    new_timestamp = timestamp + datetime.timedelta(seconds=timediff)
+
+    platform_state = State(state, timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=model_3d, mapping=[0, 2, 4])
+    assert np.allclose(platform.orientation, orientation)
+    # moving with a constant velocity model should not change the orientation
+    platform.move(new_timestamp)
+    assert np.allclose(platform.orientation, orientation)
+
+
+orientation_tests_2d = [(StateVector([0, 1, 0, 0]), StateVector([0, 0])),
+                        (StateVector([0, 0, 0, 1]), StateVector([0, np.pi/2])),
+                        (StateVector([0, 2, 0, 0]), StateVector([0, 0])),
+                        (StateVector([0, 0, 0, 2]), StateVector([0, np.pi/2])),
+                        (StateVector([0, 1, 0, 1]), StateVector([0, np.pi/4])),
+                        (StateVector([0, -1, 0, 0]), StateVector([0, np.pi])),
+                        (StateVector([0, 0, 0, -1]), StateVector([0, -np.pi/2])),
+                        (StateVector([0, -2, 0, 0]), StateVector([0, np.pi])),
+                        (StateVector([0, 0, 0, -2]), StateVector([0, -np.pi/2])),
+                        (StateVector([0, -1, 0, -1]), StateVector([0, -3*np.pi/4])),
+                        ]
+
+
+@pytest.mark.parametrize('state, orientation', orientation_tests_2d)
+def test_platform_orientation_2d(state, orientation):
+    model_1d = ConstantVelocity(0.0)
+    model_2d = CombinedLinearGaussianTransitionModel(
+        [model_1d, model_1d])
+    timestamp = datetime.datetime.now()
+    timediff = 2  # 2sec
+    new_timestamp = timestamp + datetime.timedelta(seconds=timediff)
+
+    platform_state = State(state, timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=model_2d, mapping=[0, 2])
+    assert np.allclose(platform.orientation, orientation)
+    # moving with a constant velocity model should not change the orientation
+    platform.move(new_timestamp)
+    assert np.allclose(platform.orientation, orientation)
+
+
+def test_orientation_error():
+    # moving platform without velocity defined
+    timestamp = datetime.datetime.now()
+    platform_state = State(np.array([[2],
+                                     [2],
+                                     [0]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=None, mapping=[0, 1, 2])
+    with pytest.raises(AttributeError):
+        _ = platform.orientation
+    platform_state = State(np.array([[2],
+                                     [0],
+                                     [2],
+                                     [0],
+                                     [2],
+                                     [0]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=None, mapping=[0, 2, 4])
+    with pytest.raises(AttributeError):
+        _ = platform.orientation
+
+    platform_state = State(np.array([[2],
+                                     [0],
+                                     [2],
+                                     [0]]),
+                           timestamp)
+    platform = MovingPlatform(state=platform_state, transition_model=None, mapping=[0, 2])
+    with pytest.raises(AttributeError):
+        _ = platform.orientation
