0c2b05c632fd67c3097ed2a24f3d1c963d0c365b
jjosborne <jjosborne@dstl.gov.uk>
2020-06-23 16:09:55 +0100

    work on doc strings

--- stonesoup/models/measurement/nonlinear.py
+++ stonesoup/models/measurement/nonlinear.py
@@ -966,22 +966,117 @@ class CartesianToElevationBearingRangeRate(NonLinearGaussianMeasurement, Reversi
         return out
 
 
-class RangeVelocityBinning(CartesianToElevationBearingRangeRate):
+class RangeRangeRateBinning(CartesianToElevationBearingRangeRate):
+    r"""This is a class implementation of a time-invariant measurement model, \
+    where measurements are assumed to be in the form of elevation \
+    (:math:`\theta`),  bearing (:math:`\phi`), range (:math:`r`) and
+    range-rate (:math:'\dot{r}'), with Gaussian noise in each dimension and the
+    range and range-rate are binned based on the
+    range resolution and range-rate resolution respectively.
+
+    The model is described by the following equations:
+
+    .. math::
+
+      \vec{y}_t = h(\vec{x}_t, \vec{v}_t)
+
+    where:
+
+    * :math:`\vec{y}_t` is a measurement vector of the form:
+
+    .. math::
+
+      \vec{y}_t = \begin{bmatrix}
+                \theta \\
+                \phi \\
+                r \\
+                \dot{r}
+            \end{bmatrix}
+
+    * :math:`h` is a non-linear model function of the form:
+
+    .. math::
+
+      h(\vec{x}_t,\vec{v}_t) = \begin{bmatrix}
+                asin(\mathcal{z}/\sqrt{\mathcal{x}^2 + \mathcal{y}^2 +\mathcal{z}^2}) \\
+                atan2(\mathcal{y},\mathcal{x}) \\
+                \sqrt{\mathcal{x}^2 + \mathcal{y}^2 + \mathcal{z}^2} \\
+                (x\dot{x} + y\dot{y} + z\dot{z})/\sqrt{x^2 + y^2 + z^2}
+                \end{bmatrix} + \vec{v}_t
+
+    * :math:`\vec{v}_t` is Gaussian distributed with covariance :math:`R`, i.e.:
+
+    .. math::
+
+      \vec{v}_t \sim \mathcal{N}(0,R)
+
+    .. math::
+
+      R = \begin{bmatrix}
+            \sigma_{\theta}^2 & 0 & 0 & 0\\
+            0 & \sigma_{\phi}^2 & 0 & 0\\
+            0 & 0 & \sigma_{r}^2 & 0\\
+            0 & 0 & 0 & \sigma_{\dot{r}}^2
+            \end{bmatrix}
+
+    The range and range rate are binned using
+
+    .. math::
+
+        x = floor(x/\Delta x)*\Delta x
+
+    The :py:attr:`mapping` property of the model is a 3 element vector, \
+    whose first (i.e. :py:attr:`mapping[0]`), second (i.e. \
+    :py:attr:`mapping[1]`) and third (i.e. :py:attr:`mapping[2]`) elements \
+    contain the state index of the :math:`x`, :math:`y` and :math:`z`  \
+    coordinates, respectively.
+
+    The :py:attr:`velocity_mapping` property of the model is a 3 element vector, \
+    whose first (i.e. :py:attr:`velocity_mapping[0]`), second (i.e. \
+    :py:attr:`velocity_mapping[1]`) and third (i.e. :py:attr:`velocity_mapping[2]`) elements \
+    contain the state index of the :math:`\dot{x}`, :math:`\dot{y}` and :math:`\dot{z}`  \
+    coordinates, respectively.
+
+    Note
+    ----
+    This class implementation assuming at 3D cartesian space, it therefore \
+    expects a 6D state space.
+    """
+
     range_res = Property(float, doc="Size of the range bins")
-    velocity_res = Property(float, doc="Size of the velocity bins")
+    range_rate_res = Property(float, doc="Size of the velocity bins")
 
     @property
     def ndim_meas(self):
         return 4
 
     def function(self, state, noise=False, **kwargs):
+        r"""Model function :math:`h(\vec{x}_t,\vec{v}_t)`
+
+        Parameters
+        ----------
+        state: :class:`~.StateVector`
+            An input state vector for the target
+
+        noise: :class:`numpy.ndarray` or bool
+            An externally generated random process noise sample (the default is
+            `False`, in which case no noise will be added and no binning takes place
+            if 'True', the output of :meth:`~.Model.rvs` is added and the
+            range and range rate are binned)
+
+        Returns
+        -------
+        :class:`numpy.ndarray` of shape (:py:attr:`~ndim_state`, 1)
+            The model function evaluated given the provided time interval.
+        """
+
         out = super().function(state, noise, **kwargs)
 
         if isinstance(noise, bool) or noise is None:
             if noise:
                 out[0] = np.floor(out[0] / self.range_res) * self.range_res
                 out[1] = np.floor(
-                    out[1] / self.velocity_res) * self.velocity_res
+                    out[1] / self.range_rate_res) * self.range_rate_res
 
         return out
 
@@ -1000,11 +1095,45 @@ class RangeVelocityBinning(CartesianToElevationBearingRangeRate):
         return self._gaussian_integral(a, b, mean, cov)
 
     def pdf(self, state1, state2, **kwargs):
+        r"""Model pdf/likelihood evaluation function
+
+        Evaluates the pdf/likelihood of ``state1``, given the state
+        ``state2`` which is passed to :meth:`function()`.
+
+        For the first 2 dimensions, this can be written as:
+
+        .. math::
+
+            p = p(y_t | x_t) = \mathcal{N}(y_t; x_t, Q)
+
+        where :math:`y_t` = ``state_vector1``, :math:`x_t` = ``state_vector2``,
+         :math:`Q` = :attr:`covar` and :math:`\mathcal{N}` is a normal distribution
+
+        The probability for the binned dimensions, the last 2, can be written as:
+
+        ..math::
+
+            p = P(a $\leq$ \mathcal{N} $\leq$ b)
+
+
+        In this equation a and b are the edges of the bin.
+
+        Parameters
+        ----------
+        state1 : :class:`~.State`
+        state2 : :class:`~.State`
+
+        Returns
+        -------
+        : :class:`~.Probability`
+            The likelihood of ``state1``, given ``state2``
+        """
+
         # state_vector1 is in measurement space
         # state_vector2 is in state_space
         if ((state1.state_vector[0, 0] / self.range_res).is_integer()
                 and (state1.state_vector[
-                         1, 0] / self.velocity_res).is_integer()):
+                         1, 0] / self.range_rate_res).is_integer()):
             mean_vector = self.function(state2, noise=False, **kwargs)
             # pdf for the binned range and velocity
             range_pdf = self._binned_pdf(
@@ -1015,7 +1144,7 @@ class RangeVelocityBinning(CartesianToElevationBearingRangeRate):
             velocity_pdf = self._binned_pdf(
                 state1.state_vector[1, 0],
                 mean_vector[1, 0],
-                self.velocity_res,
+                self.range_rate_res,
                 self.covar()[1])
             # pdf for the angles
             az_el_pdf = multivariate_normal.pdf(
