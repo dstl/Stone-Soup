791e6b68c2a05010b00a7990db8e423e115c857d
Edward TF Rogers <erogers@dstl.gov.uk>
2020-07-03 09:57:00 +0100

    Fix PyCharm and Flake8 warnings in qt.py

--- stonesoup/gui/qt.py
+++ stonesoup/gui/qt.py
@@ -1,10 +1,15 @@
 import sys
+# flake8: noqa F401
 
 if 'PySide2' in sys.modules:
     # PySide2
+    # noinspection PyUnresolvedReferences
     from PySide2 import QtGui, QtWidgets, QtCore, Qt
+    # noinspection PyUnresolvedReferences
     from PySide2.QtCore import Signal, Slot
 else:
     # PyQt5
+    # noinspection PyUnresolvedReferences
     from PyQt5 import QtGui, QtWidgets, QtCore, Qt
+    # noinspection PyUnresolvedReferences
     from PyQt5.QtCore import pyqtSignal as Signal, pyqtSlot as Slot
