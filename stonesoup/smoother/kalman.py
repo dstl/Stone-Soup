f63c286038ef363fcfef32cca349da7b83a8fd00
jordi <jmbarr@dstl.gov.uk>
2021-02-08 22:41:04 +0000

    Added some documentation

--- stonesoup/smoother/kalman.py
+++ stonesoup/smoother/kalman.py
@@ -12,11 +12,33 @@ from ..models.transition.linear import LinearGaussianTransitionModel
 
 class KalmanSmoother(Smoother):
     """
-    The linear-Gaussian or Rauch-Tung-Striebel smoother, colloquially the Kalman smoother.
+    The linear-Gaussian or Rauch-Tung-Striebel smoother, colloquially the Kalman smoother [1]_. The
+    transition model is therefore linear-Gaussian. No control model is currently implemented.
 
-    The transition model is linear Gaussian
+    TODO: Include a control model
 
-    No control model here.
+    The smooth function undertakes the backward algorithm on a :class:`~.Track()` object. This is
+    done by starting at the final index in the track, :math:`K` and proceed from
+    :math:`K \rightarrow 1` via:
+
+    .. math::
+
+        \mathbf{x}_{k+1|k} = F_{k+1} \mathbf{x}_k
+
+        P_{k+1|k} = F_{k+1} P_k F_{k+1}^T + Q_{k+1}
+
+        G_k = P_k F_{k+1}^T P_{k+1|k}^{-1}
+
+        \mathbf{x}_{k}^s = \mathbf{x}_k + G_k (\mathbf{x}_{k+1}^s - \mathbf{x}_{k+1|k})
+
+        P_k^s = P_k + G_k (P_{k+1}^s - P_{k+1|k}) G_k^T
+
+    where :math:`\mathbf{x}_{K}^s = \mathbf{x}_{K}` and :math:`P_K^s = P_K`.
+
+
+    References
+
+    .. [1] S\"ark\"a S. 2013, Bayesian filtering and smoothing, Cambridge University Press
 
     """
 
@@ -103,7 +125,7 @@ class KalmanSmoother(Smoother):
                 smooth_covar = state.covar + \
                     ksmooth_gain @ (prev_state.covar - prediction.covar) @ ksmooth_gain.T
 
-                # Create a new state called SmoothedState?
+                # Create a new type called SmoothedState?
                 if isinstance(state, Update):
                     prev_state = Update.from_state(state, smooth_mean, smooth_covar,
                                                    timestamp=state.timestamp, hypothesis=state.hypothesis)
@@ -118,9 +140,6 @@ class KalmanSmoother(Smoother):
 
         smoothed_track.reverse()
         return smoothed_track
-
-    def track_smooth(self, *args, **kwargs):
-        pass
 
 
 class ExtendedKalmanSmoother(KalmanSmoother):
