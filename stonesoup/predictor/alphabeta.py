2639c0ddc1cf27f608138db1fc3541505ecd791e
jordi <jmbarr@dstl.gov.uk>
2021-02-25 23:07:10 +0000

    Rewrote alpha-beta predictor (just executes constant velocity model)

--- stonesoup/predictor/alphabeta.py
+++ stonesoup/predictor/alphabeta.py
@@ -1,28 +1,40 @@
 # -*- coding: utf-8 -*-
 """alpha-beta for Stone Soup Predictor interface"""
+import numpy as np
+
 from .base import Predictor
-from ..types.prediction import StatePrediction
+from ..base import Property
+from ..types.prediction import Prediction, StatePrediction
+from ..models.transition.linear import ConstantVelocity
 
 
 class AlphaBetaPredictor(Predictor):
-    r"""Alpha-beta predictor which inherits from base.
+    r"""Alpha-beta predictor
 
-    A predictor is used to predict a new :class:`~.State` given a prior
-    :class:`~.State` and a transition model
+    The \alpha-\beta predictor assumes that the state vector is composed of 'position' and
+    'velocity' components.
 
     .. math::
 
-        f_k( \mathbf{x}_{k-1}) = \mathbf{x}_{k-1} + \Delta T \mathbf{v}_{k-1}, \
-        b_k(\mathbf{x}_{k}) = 0 \ \mathrm{and} \ \mathbf{\nu}_k \sim \mathcal{N}(0,Q_k)
+        \mathbf{x}_{k} = \mathbf{x}_{k-1} + \Delta T \mathbf{v}_{k-1}, \
 
     where :math:`\mathbf{x}_{k-1}` is the prior state, :math:`\mathbf{v}_{k-1}` is the prior
     first derivative of :math:`\mathbf{x}_{k-1}` with respect to time and :math:`\Delta T` is the
-    time interval between measurements.
+    time interval. The placement of the position and velocity within the state vector is made
+    explicit by the `position_map` and `velocity_map` binary vectors which must be of the same
+    size and non-overlapping.
+
+    No control model is assumed.
+
+    This class assumes the velocity is in units of the length per unit second. If different units
+    are required, scale the prior appropriately,
 
     """
 
-    transition_model = None
-    control_model = None
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if type(self.transition_model) is not ConstantVelocity:
+            raise TypeError("Transition model must be constant velocity")
 
     def predict(self, prior, timestamp=None, **kwargs):
         """The prediction function itself
@@ -37,18 +49,17 @@ class AlphaBetaPredictor(Predictor):
 
         Returns
         -------
-        : :class:`~.StatePrediction`
+        : :class:`~.Prediction`
             State prediction
         """
         try:
             time_interval = timestamp - prior.timestamp
         except TypeError as error:
             # TypeError: (timestamp or prior.timestamp) is None
-            raise ValueError('Time stamps are required') from error
+            raise ValueError('A time stamp is required for the alpha-beta predictor') from error
 
-        new_state_vector = prior.state_vector.copy()
-        for n in range(0, prior.ndim, 2):
-            new_state_vector[n, 0] += new_state_vector[n+1, 0] * (
-                time_interval.total_seconds())
+        new_state_vector = self.transition_model.function(prior, noise=False,
+                                                          time_interval=time_interval)
 
-        return StatePrediction(new_state_vector, timestamp=timestamp)
+        return Prediction.from_state(prior, new_state_vector, timestamp=timestamp,
+                                     transition_model=self.transition_model)
---
+++ stonesoup/predictor/tests/test_alphabeta.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+import datetime
+import pytest
+import numpy as np
+
+from stonesoup.types.state import State
+from stonesoup.types.array import StateVector
+from stonesoup.models.transition.linear import ConstantVelocity, ConstantAcceleration
+from stonesoup.predictor.alphabeta import AlphaBetaPredictor
+
+
+def test_alphabeta():
+
+    # Test wrong model correctly rejected
+    transition_model = ConstantAcceleration(noise_diff_coeff=0.01)
+    with pytest.raises(TypeError):
+        predictor = AlphaBetaPredictor(transition_model)
+
+    # Define time related variables
+    timestamp = datetime.datetime.now()
+    timediff = 2  # 2sec
+    new_timestamp = timestamp + datetime.timedelta(seconds=timediff)
+
+    # Define correct predictor this time
+    transition_model = ConstantVelocity(noise_diff_coeff=0.1)
+    predictor = AlphaBetaPredictor(transition_model)
+
+    # Define prior state
+    prior = State(StateVector([-6.45, 0.7]), timestamp=timestamp)
+    # Manually get the answer
+    outputstate = StateVector([-6.45 + timediff*0.7, 0.7])
+
+    prediction = predictor.predict(prior, timestamp=new_timestamp)
+
+    assert np.all(prediction.state_vector == outputstate)
