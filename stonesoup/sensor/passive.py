06d47aa51fe5b0851c0681a75c490af38c8097b8
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2021-03-15 12:59:43 +0000

    Document measure method on sensor base class

--- stonesoup/sensor/passive.py
+++ stonesoup/sensor/passive.py
@@ -35,25 +35,6 @@ class PassiveElevationBearing(Sensor):
 
     def measure(self, ground_truths: Set[GroundTruthState], noise: Union[np.ndarray, bool] = True,
                 **kwargs) -> Set[TrueDetection]:
-        """Generate a measurement for a given state
-
-        Parameters
-        ----------
-        ground_truths : Set[:class:`~.GroundTruthState`]
-            A set of :class:`~.GroundTruthState`
-        noise: :class:`numpy.ndarray` or bool
-            An externally generated random process noise sample (the default is
-            `True`, in which case :meth:`~.Model.rvs` is used
-            if 'False', no noise will be added)
-
-        Returns
-        -------
-        Set[:class:`~.TrueDetection`]
-            A set of measurements generated from the given states. The timestamps of the
-            measurements are set equal to that of the corresponding states that they were
-            calculated from. Each measurement stores the ground truth path that it was produced
-            from.
-        """
 
         measurement_model = CartesianToElevationBearing(
             ndim_state=self.ndim_state,
