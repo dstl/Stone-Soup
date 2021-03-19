58722724f4c7da42876780f3483f178539309b48
jordi <jmbarr@dstl.gov.uk>
2021-03-19 21:27:53 +0000

    Added tests for TLEKeplerianTransition

--- stonesoup/models/transition/orbital/tests/test_orbit.py
+++ stonesoup/models/transition/orbital/tests/test_orbit.py
@@ -1,23 +1,25 @@
 # coding: utf-8
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import numpy as np
 
 from .....types.orbitalstate import OrbitalState
-from ..orbit import CartesianKeplerianTransitionModel
-
+from .....types.array import StateVector, CovarianceMatrix
+from ..orbit import CartesianKeplerianTransitionModel, TLEKeplerianTransitionModel
 
 # Define some orbital states to test
 ini_cart = np.array([[7000], [-12124], [0], [2.6679], [4.621], [0]])
+out_tle = StateVector([2.674, 4.456, 0.1712, 0.3503, 0.3504, 0.0007662])
 
 # Set the times
 time1 = datetime(2011, 11, 12, 13, 45, 31)  # Just a time
 time2 = datetime(2011, 11, 12, 14, 45, 31)  # An hour later
 deltat = time2-time1
 
+# Different types of propagator
 propagator_c = CartesianKeplerianTransitionModel()
-
+propagator_sm = TLEKeplerianTransitionModel()
 
 def test_cartesiantransitionmodel():
     """Tests the CartesianKeplerianTransitionModel():
@@ -45,3 +47,85 @@ def test_cartesiantransitionmodel():
 
     # Check the elements match the book. But beware rounding
     assert np.allclose(final_state.cartesian_state_vector, fin_cart, rtol=1e-3)
+
+# Test the class TLEKeplerianTransitionModel class
+def test_meanmotion_transition():
+    """Tests SimpleMeanMotionTransitionModel()"""
+
+    initial_state = OrbitalState(out_tle, timestamp=time1, coordinates='TLE')
+    final_state = OrbitalState(
+        propagator_sm.transition(initial_state, noise=False, time_interval=deltat),
+        timestamp=time1 + deltat, coordinates='TLE')
+
+    # Check something's happened
+    assert not np.all(initial_state.two_line_element == final_state.two_line_element)
+
+    # Check the final state is correct
+    final_meananomaly = np.remainder(out_tle[4] + out_tle[5]*deltat.total_seconds(), 2*np.pi)
+
+    assert np.isclose(float(initial_state.mean_anomaly), float(out_tle[4]), rtol=1e-8)
+    assert np.isclose(float(final_state.mean_anomaly), float(final_meananomaly), rtol=1e-8)
+
+
+def test_meanm_cart_transition():
+    """Test two Keplerian transition models against each other"""
+
+    # Set the times up
+    time = time1
+    dt = timedelta(minutes=3)
+
+    # Initialise the state vector
+    initial_state = OrbitalState(ini_cart, coordinates="Cartesian",
+                                 timestamp=time, grav_parameter=398600)
+
+    # Make state copies to recurse
+    state1 = initial_state
+    state2 = deepcopy(state1)
+
+    assert (np.allclose(state1.cartesian_state_vector,
+                        state2.cartesian_state_vector, rtol=1e-8))
+
+    # Dumb way to do things
+    while time < datetime(2011, 11, 12, 13, 45, 31) + timedelta(minutes=6):
+        state1 = OrbitalState(propagator_c.transition(state1, time_interval=dt),
+                              timestamp=time, grav_parameter=398600)
+        state2 = OrbitalState(propagator_sm.transition(state2, noise=False, time_interval=dt),
+                              timestamp=time, coordinates='TLE', grav_parameter=398600)
+        assert (np.allclose(state1.state_vector, state2.state_vector,
+                            rtol=1e-8))
+        time = time + dt
+
+
+def test_sampling():
+    """test the sampling functions work"""
+
+    # Initialise the state vector
+    initial_state = OrbitalState(ini_cart, coordinates="Cartesian", timestamp=time1)
+    initial_state.grav_parameter = 398600
+
+    # noise it up
+    propagator_sm.process_noise = \
+        CovarianceMatrix(np.diag([1e-10, 1e-10, 1e-10, 1e-10, 1e-2, 1e-10]))
+    # take some samples
+    samp_states_sm = propagator_sm.rvs(num_samples=10, time_interval=deltat)
+
+    # Do it in Cartesian
+    propagator_c.process_noise = CovarianceMatrix(np.diag([10, 10, 10, 10e-3, 10e-3, 10e-3]))
+    samp_states_c = propagator_c.rvs(num_samples=100, time_interval=deltat)
+
+    for state in samp_states_c.T:
+        assert(propagator_c.pdf(OrbitalState(state,
+                                             timestamp=initial_state.timestamp
+                                             + deltat,
+                                             grav_parameter=initial_state.
+                                             grav_parameter),
+                                initial_state, time_interval=deltat) >= 0)
+        # PDF must be positive
+
+    for state in samp_states_sm.T:
+        assert(propagator_sm.pdf(OrbitalState(state,
+                                              timestamp=initial_state.timestamp
+                                              + deltat,
+                                              grav_parameter=initial_state.
+                                              grav_parameter),
+                                 initial_state, time_interval=deltat) >= 0)
