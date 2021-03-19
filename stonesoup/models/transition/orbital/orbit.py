fe89bc7f1514eb2d49a2d47c6318a17499b5a39e
jordi <jmbarr@dstl.gov.uk>
2021-03-19 21:05:23 +0000

    Streamlined the TLEKeplerianTransitionModel

--- stonesoup/models/transition/orbital/orbit.py
+++ stonesoup/models/transition/orbital/orbit.py
@@ -5,9 +5,10 @@ from datetime import timedelta
 import numpy as np
 
 from ....base import Property
-from ....types.array import StateVector
+from ....types.array import StateVector, Matrix
+from ....types.angle import Inclination, EclipticLongitude
 from .base import OrbitalGaussianTransitionModel
-from ...base import NonLinearModel
+from ...base import LinearModel, NonLinearModel
 from ....functions.orbital import lagrange_coefficients_from_universal_anomaly
 
 
@@ -18,10 +19,12 @@ class CartesianKeplerianTransitionModel(OrbitalGaussianTransitionModel, NonLinea
 
     Follows algorithm 3.4 in [1].
 
+    Process noise is additive and sampled from a multivariate-normal distribution in Cartesian
+    coordinates.
+
     References
     ----------
-    1. Curtis, H.D 2010, Orbital Mechanics for Engineering Students (3rd
-    Ed.), Elsevier Publishing
+    1. Curtis, H.D 2010, Orbital Mechanics for Engineering Students (3rd Ed.), Elsevier Publishing
 
     """
     _uanom_precision = Property(
@@ -32,14 +35,7 @@ class CartesianKeplerianTransitionModel(OrbitalGaussianTransitionModel, NonLinea
 
     @property
     def ndim_state(self):
-        """Dimension of the state vector is 6
-
-        Returns
-        -------
-        : int
-            The dimension of the state vector, i.e. 6
-
-        """
+        """Dimension of the state vector is 6"""
         return 6
 
     def transition(self, orbital_state, noise=False, time_interval=timedelta(seconds=0)):
@@ -107,3 +103,115 @@ class CartesianKeplerianTransitionModel(OrbitalGaussianTransitionModel, NonLinea
 
         # And put them together
         return StateVector(np.concatenate((bold_r, bold_v), axis=0)) + noise
+
+
+class TLEKeplerianTransitionModel(OrbitalGaussianTransitionModel, LinearModel):
+    r"""This transition model uses the mean motion to update the mean anomaly in simple Keplerian
+    fashion. It returns a state vector in TLE format. The function is linear (hence it's a linear
+    model) and the uncertainty is multivariate normal in the elements of the TLE. It therefore
+    differs from the :class:`~.CartesianKeplerianTransitionModel` only in the addition of noise.
+    It should be clear therefore that the :attr:`process_noise` covariance matrix is in units of
+    the TLE elements.
+
+    The transition proceeds as,
+
+        .. math::
+
+            M_{t_1} = M_{t_0} + n(t_1 - t_0), \; (\mathrm{modulo} \, 2\pi)
+
+
+    for the interval :math:`t_0 \rightarrow t_1`, where :math:`n` is the mean motion, computed as:
+
+        .. math::
+
+           n = \sqrt{ \frac{\mu}{a^3} }
+
+    which is in units of :math:`\mathrm{rad} \, s^{-1}`. The state vector is then recreated from
+    the TLE parameterisation of the orbital state vector:
+
+        .. math::
+
+            X = [i, \Omega, e, \omega, M_0, n]^{T} \\
+
+    where :math:`i` the inclination (radian), :math:`\Omega` is the longitude of the ascending
+    node (radian), :math:`e` is the orbital eccentricity (unitless), :math:`\omega` the argument
+    of perigee (radian), :math:`M_0` the mean anomaly (radian) and :math:`n` the mean motion
+    (radian/[time])
+
+    """
+
+    def matrix(self, time_interval, **kwargs):
+        """Return the transition matrix
+        """
+        matrix_out = Matrix(np.diag(np.ones(6)))
+        matrix_out[4, 5] = time_interval.total_seconds()
+        return matrix_out
+
+    @property
+    def ndim_state(self):
+        """The dimension of the state vector, 6"""
+        return 6
+
+    def transition(self, orbital_state, noise=False, time_interval=timedelta(seconds=0)):
+        r"""Execute the transition function
+
+        Parameters
+        ----------
+        orbital_state : :class:`~.OrbitalState`
+            The prior orbital state
+        noise : bool or StateVector, optional
+            A boolean parameter or a state vector. If the latter this represents an additive noise
+            (or bias) term added to the transited state. If True the noise vector is sampled via
+            the :meth:`rvs()` function. If False, noise is not added.
+        time_interval: :math:`dt` :attr:`datetime.timedelta`, optional
+            The time interval over which to test the new state, (default is 0
+            seconds)
+
+        Returns
+        -------
+        : StateVector
+            The orbital state vector returned by the transition function. It is in 'TLE' form:
+            :math:`X = [i, \Omega, e, \omega, M_0, n]^{T}`
+
+        Note
+        ----
+            This merely passes the parameters to the :attr:`.transition()`
+            function.  Units of mean motion must be :math:`\mathrm{rad} \, s^{-1}`
+
+        """
+        return self.function(orbital_state, noise=noise, time_interval=time_interval)
+
+    def function(self, orbital_state, noise=False, time_interval=timedelta(seconds=0)):
+        r"""Execute the transition function
+
+        Parameters
+        ----------
+        orbital_state: :class:`~.OrbitalState`
+            The prior orbital state
+        noise : bool or StateVector, optional
+            A boolean parameter or a state vector. If the latter this represents an additive noise
+            (or bias) term added to the transited state. If True the noise vector is sampled via
+            the :meth:`rvs()` function. If False, noise is not added.
+        time_interval: :attr:`datetime.timedelta`, optional
+            The time interval over which to calculate the new state
+            (default: 0 seconds)
+
+        Returns
+        -------
+        : :class:`~.StateVector`
+            The orbital state vector returned by the transition
+            function
+
+        Note
+        ----
+        Units of mean motion must be :math:`\mathrm{rad} \, s^{-1}`
+
+        """
+        noise = self._noiseinrightform(noise, time_interval=time_interval)
+
+        out_statev = self.matrix(time_interval) @ orbital_state.two_line_element + noise
+
+        # preserve type - must be a better way
+        return StateVector([Inclination(out_statev[0]), EclipticLongitude(out_statev[1]),
+                            out_statev[2], EclipticLongitude(out_statev[3]),
+                            EclipticLongitude(out_statev[4]), out_statev[5]])
