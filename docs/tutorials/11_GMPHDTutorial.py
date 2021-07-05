357ae4c30b255bda718a5a4f8c441b66117f218d
Emily Hunter <59794482+ekhunter123@users.noreply.github.com>
2021-07-05 13:45:57 -0400

    Update docs/tutorials/11_GMPHDTutorial.py
    Co-authored-by: nperree-dstl <71126372+nperree-dstl@users.noreply.github.com>

--- docs/tutorials/11_GMPHDTutorial.py
+++ docs/tutorials/11_GMPHDTutorial.py
@@ -90,7 +90,7 @@
 # :math:`\eqalignno{v _{ k\vert k-1} (x) =&\, \int p_{S,k}(\zeta)f_{k\vert k-1} (x\vert \zeta)v_{k-1}(\zeta)d\zeta\cr & +\int \beta_{k\vert k-1} (x\vert \zeta)v_{k-1}(\zeta)d\zeta+\gamma _{k}(x) & \cr v_{k} (x) =&\, \left[ 1-p_{D,k}(x)\right]v_{k\vert k-1}(x)\cr & +\!\!\sum\limits _{z\in Z_{k}} \!{{ p_{D,k}(x)g_{k}(z\vert x)}v_{k\vert k-1}(x) \over { \kappa _{k}(z)\!+\!\int p_{D,k}(\xi)g_{k}(z\vert \xi)v_{k\vert k-1}(\xi)}} . \cr &&}`
 #
 # For more information about the specific formulas for linear and non-linear Gaussian models,
-# please see the full text in [1].
+# please see the full text in [#]_.
 
 # %%
 # A Ground-Based Multi-Target Simulation
