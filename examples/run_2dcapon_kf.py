dac03866904d7d4032b6116a1f32f5099790def9
PhilClemson <p.clemson@liverpool.ac.uk>
2021-08-09 19:05:10 +0100

    Tidying up and added improved handling of mirrored modes along elevation axis

--- examples/run_2dcapon_kf.py
+++ examples/run_2dcapon_kf.py
@@ -3,12 +3,17 @@ import numpy as np
 from datetime import datetime
 
 from stonesoup.detector import beamformers_2d
-from stonesoup.types import state
 from stonesoup.models.transition.linear import CombinedLinearGaussianTransitionModel, \
                                                ConstantVelocity
 from stonesoup.models.measurement.linear import LinearGaussian
 from stonesoup.types.array import CovarianceMatrix
 from stonesoup.plotter import Plotter
+from stonesoup.predictor.kalman import KalmanPredictor
+from stonesoup.updater.kalman import KalmanUpdater
+from stonesoup.types.state import GaussianState
+from stonesoup.types.hypothesis import SingleHypothesis
+from stonesoup.types.track import Track
+import matplotlib.pyplot as plt
 
 
 # Get the arguments specified on the command line when the function was called
@@ -22,50 +27,33 @@ from stonesoup.plotter import Plotter
 def get_arg(args, flag):
     found = False
     for arg in args:
-            if(arg.find(flag) >= 0):
-                    found = True
-                    return arg.split(flag)[1]
-    if(found == False):
-            raise Exception('Required argument {} was not found' .format(flag))
+        if(arg.find(flag) >= 0):
+            found = True
+            return arg.split(flag)[1]
+    if(found is False):
+        raise Exception('Required argument {} was not found' .format(flag))
+
 
 args = sys.argv
 data_file = get_arg(args, '--datafile=')
 
 transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.01),
                                                           ConstantVelocity(0.01)])
-                                                          
-covar = CovarianceMatrix(np.array([[1,0],[0,1]])) # [[AA, AF],[AF, FF]]
-measurement_model = LinearGaussian(ndim_state=4, mapping=[0, 2],
-                       noise_covar=covar)
 
-from stonesoup.predictor.kalman import KalmanPredictor
+covar = CovarianceMatrix(np.array([[1, 0], [0, 1]]))
+measurement_model = LinearGaussian(ndim_state=4, mapping=[0, 2], noise_covar=covar)
+
 predictor = KalmanPredictor(transition_model)
 
-from stonesoup.updater.kalman import KalmanUpdater
 updater = KalmanUpdater(measurement_model)
 
-from stonesoup.types.state import GaussianState
 prior = GaussianState([[0.5], [0], [0.5], [0]], np.diag([1, 0, 1, 0]), timestamp=datetime.now())
 
 detector1 = beamformers_2d.capon(data_file)
 detector2 = beamformers_2d.rjmcmc(data_file)
 
-from stonesoup.types.hypothesis import SingleHypothesis
-
-from stonesoup.types.track import Track
 track1 = Track()
 track2 = Track()
-
-print("Capon detections:")
-for timestep, detections in detector1:
-    for detection in detections:
-        print(detection)
-        prediction = predictor.predict(prior, timestamp=detection.timestamp)
-        hypothesis = SingleHypothesis(prediction, detection)  # Group a prediction and measurement
-        post = updater.update(hypothesis)
-        track1.append(post)
-        prior = track1[-1]
-        
 print("RJMCMC detections:")
 for timestep, detections in detector2:
     for detection in detections:
@@ -75,10 +63,19 @@ for timestep, detections in detector2:
         post = updater.update(hypothesis)
         track2.append(post)
         prior = track2[-1]
+print("Capon detections:")
+for timestep, detections in detector1:
+    for detection in detections:
+        print(detection)
+        prediction = predictor.predict(prior, timestamp=detection.timestamp)
+        hypothesis = SingleHypothesis(prediction, detection)  # Group a prediction and measurement
+        post = updater.update(hypothesis)
+        track1.append(post)
+        prior = track1[-1]
+
 
 plotter = Plotter()
 plotter.plot_tracks(set([track1, track2]), [0, 2], uncertainty=True)
 plotter.fig
 
-import matplotlib.pyplot as plt
 plt.show()
