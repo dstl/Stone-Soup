cc4bf011fd4a83d4ecc546ded2dcb7904d901edb
Nikki Perree <nperree@dstl.gov.uk>
2021-09-14 11:19:43 +0100

    Fixes in sensor management documentation

--- docs/source/stonesoup.sensormanager.rst
+++ docs/source/stonesoup.sensormanager.rst
@@ -2,3 +2,16 @@ Sensor Managers
 ===============
 
 .. automodule:: stonesoup.sensormanager
+    :no-members:
+
+.. automodule:: stonesoup.sensormanager.base
+    :show-inheritance:
+
+.. automodule:: stonesoup.sensormanager.optimise
+    :show-inheritance:
+
+
+Reward Functions
+----------------
+.. automodule:: stonesoup.sensormanager.reward
+    :show-inheritance:
--- stonesoup/sensormanager/__init__.py
+++ stonesoup/sensormanager/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from .base import SensorManager, RandomSensorManager, BruteForceSensorManager
+from .optimise import OptimizeBruteSensorManager, OptimizeBasinHoppingSensorManager
 
-__all__ = ['SensorManager', 'RandomSensorManager', 'BruteForceSensorManager']
+__all__ = ['SensorManager', 'RandomSensorManager', 'BruteForceSensorManager',
+           'OptimizeBruteSensorManager', 'OptimizeBasinHoppingSensorManager']
