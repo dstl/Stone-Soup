a5cf850fe7afb2ab7e7bc43f793f638ce5d47bf5
Boardman, David <D.Boardman@liverpool.ac.uk>
2021-09-28 09:12:29 +0100

    added combinations generation

--- stonesoup/runmanager/runmanagercore1.py
+++ stonesoup/runmanager/runmanagercore1.py
@@ -1,18 +1,15 @@
-
 import itertools
 import json
 from pathlib import Path
 from itertools import product, starmap
 from collections import namedtuple
 
-#from stonesoup.runmanager.runmanager import index
-
 def read_json(json_input):
     with open(json_input) as json_file:
         json_data = json.load(json_file)
         return json_data
 
-
+# Calculate the steps for each item in a list
 def iterations(min_value, max_value, num_samples):
     temp = []
     difference = max_value - min_value
@@ -21,7 +18,8 @@ def iterations(min_value, max_value, num_samples):
         temp.append(min_value + (x * factor))
     return temp
 
-
+# gets the combinations for one tracker and stores in list
+# Once you have steps created from iterations, generate step combinations for one parameter
 def get_trackers_list(iterations_container_list, value_min):
     temp =[]
     for x in range(0, len(value_min)):
@@ -31,6 +29,8 @@ def get_trackers_list(iterations_container_list, value_min):
 
     return set_combinations
 
+# Returns the product of all combinations from the trackers list
+# Generates all of the combinations for n parameters
 def all_combos(**items):
     Combo = namedtuple('_params', items.keys())
     return starmap(Combo, product(*items.values()))
@@ -45,8 +45,10 @@ def run():
     trackers = []
     tracker = []
     iters = []
+
     # For each parameter in the json, calculate the increments of each step.
-    # Need to sort out bools will do in future
+    # Essentially, for each parameter calculate the steps and iterations, add to tracker list
+    # then combine all to generate combinations.
     for param in json_data["parameters"]:
         
         if param["type"] == "bool":
@@ -61,6 +63,8 @@ def run():
             tracker = []               
 
         elif param["type"] == "vector":
+            # If vector, requires extra loop for items within vector. 
+            # Need better solution for n_d arrays
             for x in range(0, len(param["value_min"])):
                 iters.append(iterations(param["value_min"][x], param["value_max"][x], param["n_samples"]))
             combo_list = get_trackers_list(iters, param["value_min"])
@@ -89,19 +93,20 @@ def run():
         else:
             print("nothing")
 
+
+    # Need to refactor into a tuple or something with named tracker list
+    # Make remove hardcoded names for combinations (should read from json)
+    # maybe change starmap to map and set key prior to this.
     combis = all_combos(velocity=combo_list, num_particles=int_list, total_weight=float_list, normalise=bool_list)
     
-    print(combis)
+    # Not required, can use print(combis) However was testing to format names for files with parameters
+    # for future implementation of outputting .json files for each parameter results
+    # Something like date/time of run + params used in each tracker experiment.
     for idx, output in enumerate(combis):
         #_track = str(output).replace("(", "_").replace(" ", "_").replace(",", "").replace(")", "")
         _track = str(output)
         print('tracker{:0>8d}{}'.format(idx, _track))
-    
-    #print(type(combis))
-    # Construct all of the trackers with correct step increments
-    #combos = set(itertools.product(combo_list, int_list, float_list, bool_list))
-    #sim_dir.mkdir(exist_ok=True)
-    
+      
    
 if __name__ == "__main__":
     run()
