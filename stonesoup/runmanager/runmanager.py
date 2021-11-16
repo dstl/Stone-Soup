61765e04db8eb2ba39ad25f9491449b12656f47e
Boardman, David <D.Boardman@liverpool.ac.uk>
2021-11-16 14:27:14 +0000

    added arguments to runmanager.py template file

--- stonesoup/runmanager/runmanager.py
+++ stonesoup/runmanager/runmanager.py
@@ -1,37 +1,33 @@
-import logging
+from stonesoup.runmanager.runmanagercore import RunManagerCore
 import sys
-from .runmanagercore import RunManagerCore
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", "-c", help="A configuration .yaml file", type=str)
     parser.add_argument("--parameter", "-p", help="A Parameter .json file", type=str)
-    parser.add_argument("--groundtruth", "-p", help="Ground truth setting. 1 for ground truth, 0 for no ground_truth", type=int)
+    parser.add_argument("--groundtruth", "-g", help="Ground truth setting. 1 for ground truth, 0 for no ground_truth", type=int)
     parser.add_argument("--dir", "-d", help="A config & parameter directory", type=str)
     args = parser.parse_args()
-    print("args directory {0}", args.config_dir)
-
+    dir = args.dir
+    
     try:
         configInput = args.config
-
     except Exception as e:
-        configInput = ""
-        logging.error(e)
+        configInput = "C:\\Users\\Davidb1\\Documents\\Python\\data\\config.yaml"
+        print(e)
 
     try:
-        parametersInput = args.parameters
+        parametersInput = args.parameter
     except Exception as e:
-        parametersInput = ""
-        logging.error(e)
-        # parametersInput= "C:\\Users\\gbellant\\Documents\\Projects\\Serapis\\dummy3.json"
+        parametersInput = "C:\\Users\\Davidb1\\Documents\\Python\\data\\parameters.json"
+        print(e)
 
     try:
         groundtruthSettings = args.groundtruth
     except Exception as e:
         groundtruthSettings = 1
-        logging.error(e)
+        print(e)
 
     rmc = RunManagerCore()
-
-    rmc.run(configInput, parametersInput, groundtruthSettings)
+    rmc.run(configInput, parametersInput, groundtruthSettings, dir)
