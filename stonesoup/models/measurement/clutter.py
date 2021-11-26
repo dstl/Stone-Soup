099b74c1e926ed297286a36506088aebb96bc58d
Emily Hunter <59794482+ekhunter123@users.noreply.github.com>
2021-11-26 14:28:59 -0500

    Make distribution more generic
    Incorporate suggested changes to make the distribution a Callable function, giving the user more freedom.

--- stonesoup/models/measurement/clutter.py
+++ stonesoup/models/measurement/clutter.py
@@ -1,25 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-from typing import Set, Union, Tuple
-from enum import Enum
+from typing import Set, Union, Callable, Tuple
 
 from ..base import Property
 from .base import MeasurementModel
 from ...types.detection import Clutter
 from ...types.groundtruth import GroundTruthState
-from ...types.array import CovarianceMatrix, StateVector, StateVectors
+from ...types.array import StateVector, StateVectors
 from ...types.numeric import Probability
 from ...types.state import State
 
-class Distribution(Enum):
-    """
-    Explicitly define the distributions that are supported for the clutter model.
-    """
-    uniform = "uniform"
-    normal = "normal"
-
-class ClutterBearingRange(MeasurementModel):
+class ClutterModel(MeasurementModel):
     """A simulator that generates sensor clutter (false alarms) according to a specified 
     distribution in 2D space relative to the sensor's position.
 
@@ -32,36 +24,20 @@ class ClutterBearingRange(MeasurementModel):
         default=1.0,
         doc="The average number of clutter points per time step. The actual "
             "number is Poisson distributed")
-    distribution: Distribution = Property(
-        default=Distribution.uniform,
-        doc="The type of distribution that the clutter follows. Supported "
-            "types are described by the Enum Distribution class.")
-    state_space: Tuple[Tuple[float, float], Tuple[float, float]] = Property(
-        default=None,
-        doc="A 2-D list in the form of math:`[[x_min, dx], [y_min, dy]]`. These give the "
-            "dimension bounds in the Cartesian space. For example, if the sensor is "
-            "positioned at -100 in the x-axis and can 'see' for 200m, then "
-            "math:`[x_min, dx] = [-100, 200]`. This is only needed when the distribution "
-            "is uniform.")
-    mean: Tuple[float, float, float] = Property(
-        default=None,
-        doc="The mean of the distribution for normally-distributed clutter. The "
-        "mean is defined in Cartesian space and must have length 2."
-        "This parameter is only needed when using the normal distribution.")
-    covariance: CovarianceMatrix = Property(
-        default=None,
-        doc="Covariance matrix of the distribution for normally-distributed clutter. "
-        "It must be a 2x2 matrix. This parameter is only needed when using the normal "
-        "distribution.")
+    distribution: Callable = Property(
+        default=np.random.default_rng().uniform,
+        doc="A function which represents the distribution of the clutter over the "
+            "measurement space. The function should return a single value (ie, do "
+            "not use multivariate distributions).")
+    dist_params: Tuple = Property(
+        default=((-200, 200), (-200, 200)),
+        doc="The required parameters for the clutter distribution function. The "
+        "length of the list must be equal to the number of dimensions (`self.ndim_meas`) "
+        "and should be defined for use in Cartesian space."
+        "The default defines the space for a uniform distribution in 2D. The call "
+        "`np.array([self.distribution(*arg) for arg in self.dist_params])` "
+        "must return a numpy array of length equal to the number of dimensions.")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # Check that the chosen distribution matches the parameters given
-        if self.distribution == Distribution.uniform and self.state_space is None:
-            raise ValueError("To use the uniform distribution, the state_space parameter must be defined")
-        elif self.distribution == Distribution.normal and self.mean is None and self.covariance is None:
-            raise ValueError("To use the normal distribution, the mean and covariance parameters must be defined")
 
     def function(self, ground_truths: Set[GroundTruthState], **kwargs) -> Set[Clutter]:
         """
@@ -89,10 +65,8 @@ class ClutterBearingRange(MeasurementModel):
         timestamp = next(iter(ground_truths)).timestamp
         clutter = set()
         for _ in range(np.random.poisson(self.clutter_rate)):
-            if self.distribution == Distribution.uniform:
-                random_vector = self.generate_uniform_vector()
-            else:
-                random_vector = self.generate_normal_vector()
+            # Call the distribution function to generate a random vector in the space
+            random_vector = np.array([self.distribution(*arg) for arg in self.dist_params])
             
             # Make a State object with the random vector
             state = State([0.0] * self.measurement_model.ndim_state, timestamp=timestamp)
@@ -101,7 +75,7 @@ class ClutterBearingRange(MeasurementModel):
             # Use the sensor's measurement model to incorporate the
             # translation offset and sensor rotation. This will also
             # convert the vector to the proper measurement space
-            # (polar coordinates). 
+            # (polar or spherical coordinates)
             clutter_vector = self.measurement_model.function(state)
 
             # Create a clutter object.
@@ -111,33 +85,6 @@ class ClutterBearingRange(MeasurementModel):
 
         return clutter
     
-    def generate_uniform_vector(self):
-        """Generate a random vector in the uniform distribution according
-        to the model's defined state space.
-
-        Returns
-        -------
-        : :class:`numpy.array`
-            A uniform random vector with length equal to self.ndim_meas.
-        """
-        arr = np.array([np.random.default_rng().uniform(*space) 
-                for space in self.state_space])
-        return arr
-        
-
-    def generate_normal_vector(self):
-        """Generate a random vector in the normal distribution according
-        to the model's defined mean and covariance.
-
-        Returns
-        -------
-        : :class:`numpy.array`
-            A normal random vector. Has the same length as the mean.
-        """
-        # Generate a multivariate random normal vector according to our parameters
-        random_vector = np.random.default_rng().multivariate_normal(self.mean, self.covariance)
-        
-        return random_vector
     
     @property
     def ndim_meas(self) -> int:
@@ -162,30 +109,3 @@ class ClutterBearingRange(MeasurementModel):
         Must be implemented to properly inherit the parent Model.
         """
         return None
-
-
-class ClutterElevationBearingRange(ClutterBearingRange):
-    """A simulation that generates sensor clutter (false alarms) according to a specified 
-    distribution in 3D space relative to the sensor's position.
-
-    Note
-    ----
-    This implementation of this class assumes a 3D Cartesian space.
-    """
-
-    state_space: Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]] = Property(
-        default=None,
-        doc="A 2-D list in the form of math:`[[x_min, dx], [y_min, dy], [z_min, dz]]`. "
-            "These give the dimension bounds in the Cartesian space. For example, if the sensor "
-            "is positioned at -100 in the x-axis and can 'see' for 200m, then "
-            "math:`[x_min, dx] = [-100, 200]`. This is only needed when the distribution is uniform.")
-    mean: Tuple[float, float, float] = Property(
-        default=None,
-        doc="The mean of the distribution for normally-distributed clutter. The "
-        "mean is defined in Cartesian space and must have length 3."
-        "This parameter is only needed when using the normal distribution.")
-    covariance: CovarianceMatrix = Property(
-        default=None,
-        doc="Covariance matrix of the distribution for normally-distributed clutter. "
-        "It must be a 3x3 matrix. This parameter is only needed when using the normal "
-        "distribution.")
