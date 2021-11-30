3a7a03d77272b930567ba524371e177dbd2e53c1
Oliver Harrald <oharrald@dstl.gov.uk>
2021-11-30 16:34:14 +0000

    Add constant jerk model simulation for platforms
    Another method for calculating a series of smooth transitions between a series of target states.

--- stonesoup/simulator/transition.py
+++ stonesoup/simulator/transition.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from copy import deepcopy
 from datetime import timedelta
-from typing import Tuple
+from itertools import combinations
+from typing import Tuple, Sequence
 
 import numpy as np
 
@@ -306,3 +307,253 @@ class Point2PointStop(TransitionModel):
             return StateVector([x + dx, vx, y + dy, vy])
         else:
             return state.state_vector  # if already at destination, stay
+
+
+class ConstantJerkSimulator(TransitionModel):
+    r"""Constant, noiseless, jerk transition model for cartesian space.
+
+    The state space has no acceleration or jerk elements. I.E. the only kinematic components are
+    position and velocity.
+
+    Solution given by :math:`\vec{\ddddot{x}} = \vec{0}`
+
+    The user will provide an initial and final state, each of which containing initial cartesian
+    position and velocities. For example, :math:`(x, vx, y, vy)`.
+
+    Components of the state vector that are not position or velocity are kept constant.
+    Initial and final accelerations are uniquely defined by this input.
+
+    Notes
+    -----
+        * Acceleration instantaneously changes at each target state
+    """
+    position_mapping: Sequence[int] = Property(
+        doc="Mapping between platform position and state vector.")
+    velocity_mapping: Sequence[int] = Property(
+        default=None,
+        doc="Mapping between platform velocity and state vector. Defaults to `[m+1 for m in "
+            "position_mapping]`")
+    init_state: State = Property(
+        doc="Initial state to move from. Must be `ndim_state` dimensions.")
+    final_state: State = Property(
+        doc="Final state to move to. Must be `ndim_state` dimensions.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.init_state.state_vector.shape[0] != self.final_state.state_vector.shape[0]:
+            raise ValueError(
+                f"Initial and final states must share the same number of dimensions. Initial "
+                f"state has ndim = {self.init_state.state_vector.shape[0]} but final state has "
+                f"ndim = {self.final_state.state_vector.shape[0]}")
+
+        if self.velocity_mapping is None:
+            self.velocity_mapping = [p + 1 for p in self.position_mapping]
+
+        # Full duration of transition
+        self.duration = (self.final_state.timestamp - self.init_state.timestamp).total_seconds()
+
+        # Initial position
+        self.init_X = self.init_state.state_vector[self.position_mapping, :]
+        # Initial velocity
+        self.init_V = self.init_state.state_vector[self.velocity_mapping, :]
+
+        # Final position
+        self.final_X = self.final_state.state_vector[self.position_mapping, :]
+        # Final velocity
+        self.final_V = self.final_state.state_vector[self.velocity_mapping, :]
+
+        self.init_A, self.final_A, self.jerk = list(), list(), list()
+        for init_x, init_v, final_x, final_v in zip(self.init_X, self.init_V,
+                                                    self.final_X, self.final_V):
+            init_a = self.calculate_init_accel(init_x, final_x,
+                                               init_v, final_v,
+                                               self.duration)
+            self.init_A.append(init_a)
+
+            final_a = self.calculate_final_accel(init_v, final_v, init_a, self.duration)
+            self.final_A.append(final_a)
+            self.jerk.append(self.calculate_jerk(init_a, final_a, self.duration))
+
+    @property
+    def ndim_state(self):
+        """Number of state space dimensions."""
+        return self.init_state.state_vector.shape[0]
+
+    def covar(self, **kwargs):
+        """Must be added due to inheritance."""
+        raise NotImplementedError('Covariance not defined')
+
+    def pdf(self, state1, state2, **kwargs):
+        """Must be added due to inheritance."""
+        raise NotImplementedError('pdf not defined')
+
+    def rvs(self, num_samples=1, **kwargs):
+        """Must be added due to inheritance."""
+        raise NotImplementedError('rvs not defined')
+
+    def function(self, state, time_interval, **kwargs):
+        """Apply a constant jerk transition to `state`, for `time_interval` duration, keeping
+        elements of state vector that are not position or velocity constant."""
+
+        # Total time that will have passed since initial state up until transition is complete
+        delta_t = (state.timestamp + time_interval - self.init_state.timestamp).total_seconds()
+
+        # New position and velocity calculated only from `delta_t`
+        # Assumed that `state` lies on the constant jerk path connecting `initial_state` with
+        # `final_state`
+
+        new_position = list()
+        new_velocity = list()
+        for init_x, init_v, init_a, jerk in zip(self.init_X, self.init_V, self.init_A, self.jerk):
+            new_position.append(self.calculate_pos(init_x, init_v, init_a, jerk, delta_t))
+            new_velocity.append(self.calculate_vel(init_v, init_a, jerk, delta_t))
+
+        # Non-kinematic components remain constant
+        new_sv = np.copy(state.state_vector).astype(float)  # May initiate with integers
+        new_sv[self.position_mapping, 0] = new_position
+        new_sv[self.velocity_mapping, 0] = new_velocity
+
+        return StateVector(new_sv)
+
+    @staticmethod
+    def calculate_pos(init_x, init_v, init_a, jerk, T):
+        """Calculate position, along a particular axis.
+
+        Parameters
+        ----------
+        init_x: float
+            Initial position along axis
+        init_v: float
+            Initial velocity along axis
+        init_a: float
+            Initial acceleration along axis
+        jerk: float
+            Constant jerk value along axis
+        T: float
+            Number for seconds to carry-out jerk transition
+
+        Returns
+        -------
+        float
+            New position along axis, given by:
+            :math:`X' = \frac{J_0 T^3}{6} + \frac{A_0 T^2}{2} + V_0 T + X_0`
+        """
+        return (jerk * T ** 3) / 6 + (init_a * T ** 2) / 2 + init_v * T + init_x
+
+    @staticmethod
+    def calculate_vel(init_v, init_a, jerk, T):
+        """Calculate velocity, along a particular axis.
+
+        Parameters
+        ----------
+        init_v: float
+            Initial velocity along axis
+        init_a: float
+            Initial acceleration along axis
+        jerk: float
+            Constant jerk value along axis
+        T: float
+            Number for seconds to carry-out jerk transition
+
+        Returns
+        -------
+        float
+            New velocity along axis, given by:
+            :math:`V' = \frac{J_0 T^2}{2} + A_0 T + V_0`
+        """
+        return (jerk * T ** 2) / 2 + init_a * T + init_v
+
+    @staticmethod
+    def calculate_init_accel(init_x, final_x, init_v, final_v, T):
+        """Calculate initial acceleration, along a particular axis.
+
+        Parameters
+        ----------
+        init_x: float
+            Initial position along axis
+        final_x: float
+            Final position along axis
+        init_v: float
+            Initial velocity along axis
+        final_v: float
+            Final velocity along axis
+        T: float
+            Number for seconds to get from initial to final state
+
+        Returns
+        -------
+        float
+            Initial acceleration along axis, given by:
+            :math:`A_0 = \frac{6}{T^2}(X_1 - X_0 - \frac{2 V_0 T}{3} - \frac{V_1 T}{3})`
+        """
+        return (6 / T ** 2) * (final_x - init_x - (2 * init_v * T / 3) - (final_v * T / 3))
+
+    @staticmethod
+    def calculate_final_accel(init_v, final_v, init_a, T):
+        """Calculate final acceleration, along a particular axis.
+
+        Parameters
+        ----------
+        init_v: float
+            Initial velocity along axis
+        final_v: float
+            Final velocity along axis
+        init_a: float
+            Initial acceleration along axis
+        T: float
+            Number for seconds to get from initial to final state
+
+        Returns
+        -------
+        float
+            Final acceleration along axis, given by:
+            :math:`A_1 = \frac{2}{T} (V_1 - V_0) - A_0`
+        """
+        if T == 0:
+            return 0
+        return (2 / T) * (final_v - init_v) - init_a
+
+    @staticmethod
+    def calculate_jerk(init_a, final_a, T):
+        """Calculate constant jerk, along a particular axis.
+
+        Parameters
+        ----------
+        init_a: float
+            Initial acceleration along axis
+        final_a: float
+            Final acceleration along axis
+        T: float
+            Number for seconds to get from initial to final state
+
+        Returns
+        -------
+        float
+            Constant jerk along axis, given by:
+            :math:`J = \frac{A_1 - A_0}{T}`
+        """
+        return (final_a - init_a) / T
+
+    @classmethod
+    def create_models(cls, states: Sequence[State], position_mapping, velocity_mapping=None):
+        """Generate a list of :class:`~.ConstantJerkSimulator` and transition durations, given a
+        list of states."""
+
+        if not all(state.ndim == state2.ndim for state, state2 in combinations(states, 2)):
+            raise ValueError("All states must have the same ndim")
+
+        transition_models, transition_times = list(), list()
+
+        for current_state, next_state in zip(states[:-1], states[1:]):
+
+            transition_times.append(next_state.timestamp - current_state.timestamp)
+
+            transition_models.append(
+                cls(position_mapping=position_mapping,
+                    velocity_mapping=velocity_mapping,
+                    init_state=current_state,
+                    final_state=next_state)
+            )
+
+        return transition_models, transition_times
