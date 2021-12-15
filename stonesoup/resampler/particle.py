aaa2c92ce28f71bc99a201719804cb6848ae0b89
Oliver Rosoman <orosoman@dstl.gov.uk>
2021-12-15 14:48:12 +0000

    Convert ESSResampler to a wrapper class, and add documentation

--- stonesoup/resampler/particle.py
+++ stonesoup/resampler/particle.py
@@ -49,17 +49,45 @@ class SystematicResampler(Resampler):
         return new_particles
 
 
-class ESSResampler(SystematicResampler):
+class ESSResampler(Resampler):
+    """ESSResampler class
+        This wrapper uses a :class:`~.Resampler` to resample the particles inside
+        an instant of :class:`~.Particles`, but only after checking if this is necessary
+        by comparing Effective Sample Size (ESS) with a supplied threshold (numeric).
+        Kish's ESS is used, as in Section 3.5 of [1]_.   Doucet-Johansen, 2008.
+
+        References
+        ----------
+        .. [1] Doucet A., Johansen A.M., 2009, Tutorial on Particle Filtering \
+        and Smoothing: Fifteen years later, Handbook of Nonlinear Filtering, Vol. 12.
+
+        """
 
     threshold: float = Property(default=None,
-                                doc='Threshold compared with ESS to decide whether to resample')
+                                doc='Threshold compared with ESS to decide whether to resample. \
+                                    Default is number of particles divided by 2, \
+                                        set in resample method')
+    resampler: Resampler = Property(default=SystematicResampler,
+                                    doc='Resampler to wrap, which is called \
+                                        when ESS below threshold')
 
     def resample(self, particles):
+        """
+        Parameters
+        ----------
+        particles : list of :class:`~.Particle`
+            The particles to be resampled according to their weight
+
+        Returns
+        -------
+        particles : list of :class:`~.Particle`
+            The particles, either unchanged or resampled, depending on weight degeneracy
+        """
         if not isinstance(particles, Particles):
             particles = Particles(particle_list=particles)
         if self.threshold is None:
             self.threshold = len(particles) / 2
         if 1 / np.sum(np.square(particles.weight)) < self.threshold:  # If ESS too small, resample
-            return super().resample(particles)
+            return self.resampler.resample(self.resampler, particles)
         else:
             return particles
