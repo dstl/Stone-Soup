76fc37446d157de3300ce14c05e09ff3b05cb9c9
PACarniglia <43048394+PACarniglia@users.noreply.github.com>
2022-01-04 15:28:21 -0500

    Refactored to meet PEP8 standards
    Using flake8 python library

--- stonesoup/plotter3.py
+++ stonesoup/plotter3.py
@@ -7,24 +7,23 @@ Created on Thu Nov 18 11:33:27 2021
 import warnings
 from itertools import chain
 
-from mpl_toolkits import mplot3d # new as of plotter3
+from mpl_toolkits import mplot3d  # new as of plotter3
 
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.lines import Line2D
-from matplotlib.patches import Ellipse
-from matplotlib.legend_handler import HandlerPatch
 
 from .types import detection
 from .models.base import LinearModel, NonLinearModel
 
 
 class Plotter:
-    """Plotting class for building graphs of Stone Soup simulations
+    """3D plotting class for building 3D visualizations of Stone Soup simulations
 
     A plotting class which is used to simplify the process of plotting ground truths,
-    measurements, clutter and tracks. Tracks can be plotted with uncertainty ellipses or
-    particles if required. Legends are automatically generated with each plot.
+    measurements, clutter and tracks with information in three spatial dimensions.
+    Tracks can be plotted with uncertainty bars at specified intervals if required.
+    Legends are automatically generated with each plot.
 
     Attributes
     ----------
@@ -32,10 +31,9 @@ class Plotter:
         Generated figure for graphs to be plotted on
     ax: matplotlib.axes.Axes
         Generated axes for graphs to be plotted on
-    handles_list: list of :class:`matplotlib.legend_handler.HandlerBase`
-        A list of generated legend handles
-    labels_list: list of str
-        A list of generated legend labels
+    legend_dict: dict
+        Dictionary of legend handles as :class:`matplotlib.legend_handler.HandlerBase`
+        and labels as str
     """
 
     def __init__(self):
@@ -67,7 +65,8 @@ class Plotter:
             :class:`~.GroundTruthPath` type, the argument is modified to be a set to allow for
             iteration.
         mapping: list
-            List of 2 items specifying the mapping of the x and y components of the state space.
+            List of 3 items specifying the mapping of the x, y, and z position
+            components of the state space.
         \\*\\*kwargs: dict
             Additional arguments to be passed to plot function. Default is ``linestyle="--"``.
         """
@@ -79,9 +78,9 @@ class Plotter:
 
         for truth in truths:
             self.ax.plot3D([state.state_vector[mapping[0]] for state in truth],
-                         [state.state_vector[mapping[1]] for state in truth],
-                         [state.state_vector[mapping[2]] for state in truth],
-                         **truths_kwargs)
+                           [state.state_vector[mapping[1]] for state in truth],
+                           [state.state_vector[mapping[2]] for state in truth],
+                           **truths_kwargs)
 
         # Generate legend items
         truths_handle = Line2D([], [], linestyle=truths_kwargs['linestyle'], color='black')
@@ -105,7 +104,8 @@ class Plotter:
         measurements : list of :class:`~.Detection`
             Detections which will be plotted. If measurements is a set of lists it is flattened.
         mapping: list
-            List of 2 items specifying the mapping of the x and y components of the state space.
+            List of 3 items specifying the mapping of the x, y, and z position
+            components of the state space.
         measurement_model : :class:`~.Model`, optional
             User-defined measurement model to be used in finding measurement state inverses if
             they cannot be found from the measurements themselves.
@@ -130,7 +130,7 @@ class Plotter:
             if meas_model is None:
                 meas_model = measurement_model  # measurement_model from input
 
-            if isinstance(meas_model, LinearModel):  # check to see if meas_model is a LinearModel type
+            if isinstance(meas_model, LinearModel):  # meas_model is a LinearModel type
                 model_matrix = meas_model.matrix()
                 inv_model_matrix = np.linalg.pinv(model_matrix)
                 state_vec = inv_model_matrix @ state.state_vector
@@ -159,7 +159,9 @@ class Plotter:
 
         if plot_detections:
             detection_array = np.array(plot_detections)
-            self.ax.scatter(detection_array[:, 0], detection_array[:, 1], detection_array[:, 2], **measurement_kwargs)
+            self.ax.scatter(detection_array[:, 0],
+                            detection_array[:, 1],
+                            detection_array[:, 2], **measurement_kwargs)
             measurements_handle = Line2D([], [], linestyle='', **measurement_kwargs)
 
             # Generate legend items for measurements
@@ -167,7 +169,9 @@ class Plotter:
 
         if plot_clutter:
             clutter_array = np.array(plot_clutter)
-            self.ax.scatter(clutter_array[:, 0], clutter_array[:, 1], clutter_array[:, 2], color='y', marker='2')
+            self.ax.scatter(clutter_array[:, 0],
+                            clutter_array[:, 1],
+                            clutter_array[:, 2], color='y', marker='2')
             clutter_handle = Line2D([], [], linestyle='', marker='2', color='y')
             clutter_label = "Clutter"
 
@@ -177,16 +181,17 @@ class Plotter:
         # Generate legend
         self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
 
-    def plot_tracks(self, tracks, mapping, uncertainty=False, particle=False, track_label="Track", err_freq=1, 
+    def plot_tracks(self, tracks, mapping, uncertainty=False, track_label="Track", err_freq=1,
                     **kwargs):
         """Plots track(s)
 
         Plots each track generated, generating a legend automatically. If ``uncertainty=True``,
-        uncertainty ellipses are plotted. If ``particle=True``, particles are plotted.
-        Tracks are plotted as solid lines with point markers and default colors.
-        Uncertainty ellipses are plotted with a default color which is the same for all tracks.
+        uncertainty bars are plotted every :attr:`err_freq` measurement, default
+        plots unceratinty bars at every track step. Tracks are plotted as solid
+        lines with point markers and default colors. Uncertainty bars are plotted
+        with a default color which is the same for all tracks.
 
-        Users can change linestyle, color and marker using keyword arguments. Uncertainty ellipses
+        Users can change linestyle, color and marker using keyword arguments. Uncertainty bars
         will also be plotted with the user defined colour and any changes will apply to all tracks.
 
         Parameters
@@ -195,13 +200,17 @@ class Plotter:
             Set of tracks which will be plotted. If not a set, and instead a single
             :class:`~.Track` type, the argument is modified to be a set to allow for iteration.
         mapping: list
-            List of 2 items specifying the mapping of the x and y components of the state space.
+            List of 3 items specifying the mapping of the x, y, and z position
+            components of the state space.
         uncertainty : bool
-            If True, function plots uncertainty ellipses.
+            If True, function plots uncertainty bars in x, y, and z.
         particle : bool
             If True, function plots particles.
         track_label: str
             Label to apply to all tracks for legend.
+        err_freq: int
+            Frequency of error bar plotting on tracks. Default value is 1, meaning
+            error bars are plotted at every track step.
         \\*\\*kwargs: dict
             Additional arguments to be passed to plot function. Defaults are ``linestyle="-"``,
             ``marker='.'`` and ``color=None``.
@@ -238,81 +247,23 @@ class Plotter:
                 for state in track:
                     if not check % err_freq:
                         w, v = np.linalg.eig(HH @ state.covar @ HH.T)
-                        
+
                         xl = state.state_vector[mapping[0]]
                         yl = state.state_vector[mapping[1]]
                         zl = state.state_vector[mapping[2]]
-                                                
+
                         x_err = w[0]
                         y_err = w[1]
                         z_err = w[2]
-                        
-                        self.ax.plot3D([xl+x_err, xl-x_err], [yl, yl], [zl, zl], marker="_", color=tracks_kwargs['color'])
-                        self.ax.plot3D([xl, xl], [yl+y_err, yl-y_err], [zl, zl], marker="_", color=tracks_kwargs['color'])
-                        self.ax.plot3D([xl, xl], [yl, yl], [zl+z_err, zl-z_err], marker="_", color=tracks_kwargs['color'])
+
+                        self.ax.plot3D([xl+x_err, xl-x_err], [yl, yl], [zl, zl],
+                                       marker="_", color=tracks_kwargs['color'])
+                        self.ax.plot3D([xl, xl], [yl+y_err, yl-y_err], [zl, zl],
+                                       marker="_", color=tracks_kwargs['color'])
+                        self.ax.plot3D([xl, xl], [yl, yl], [zl+z_err, zl-z_err],
+                                       marker="_", color=tracks_kwargs['color'])
                     check += 1
-            self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
-
-        elif particle:
-            # Plot particles
-            for track in tracks:
-                for state in track:
-                    data = state.particles.state_vector[mapping[:2], :]
-                    self.ax.plot(data[0], data[1], linestyle='', marker=".",
-                                 markersize=1, alpha=0.5)
-
-            # Generate legend items for particles
-            particle_handle = Line2D([], [], linestyle='', color="black", marker='.', markersize=1)
-            particle_label = "Particles"
-            self.legend_dict[particle_label] = particle_handle
-
-            # Generate legend
             self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
 
         else:
             self.ax.legend(handles=self.legend_dict.values(), labels=self.legend_dict.keys())
-
-    # Ellipse legend patch (used in Tutorial 3)
-    @staticmethod
-    def ellipse_legend(ax, label_list, color_list, **kwargs):
-        """Adds an ellipse patch to the legend on the axes. One patch added for each item in
-        `label_list` with the corresponding color from `color_list`.
-
-        Parameters
-        ----------
-        ax : matplotlib.axes.Axes
-            Looks at the plot axes defined
-        label_list : list of str
-            Takes in list of strings intended to label ellipses in legend
-        color_list : list of str
-            Takes in list of colors corresponding to string/label
-            Must be the same length as label_list
-        \\*\\*kwargs: dict
-                Additional arguments to be passed to plot function. Default is ``alpha=0.2``.
-        """
-
-        ellipse_kwargs = dict(alpha=0.2)
-        ellipse_kwargs.update(kwargs)
-
-        legend = ax.legend(handler_map={Ellipse: _HandlerEllipse()})
-        handles, labels = ax.get_legend_handles_labels()
-        for color in color_list:
-            handle = Ellipse((0.5, 0.5), 0.5, 0.5, color=color, **ellipse_kwargs)
-            handles.append(handle)
-        for label in label_list:
-            labels.append(label)
-        legend._legend_box = None
-        legend._init_legend_box(handles, labels)
-        legend._set_loc(legend._loc)
-        legend.set_title(legend.get_title().get_text())
-
-
-class _HandlerEllipse(HandlerPatch):
-    def create_artists(self, legend, orig_handle,
-                       xdescent, ydescent, width, height, fontsize, trans):
-        center = 0.5*width - 0.5*xdescent, 0.5*height - 0.5*ydescent
-        p = Ellipse(xy=center, width=width + xdescent,
-                    height=height + ydescent)
-        self.update_prop(p, orig_handle, legend)
-        p.set_transform(trans)
-        return [p]
