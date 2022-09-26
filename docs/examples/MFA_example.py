5408fcc4836112f0f2d91df30ace17b172a50c84
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2022-09-26 15:24:00 +0100

    Update MFA example

--- docs/examples/MFA_example.py
+++ docs/examples/MFA_example.py
@@ -1,42 +1,22 @@
 #!/usr/bin/env python
 
 """
-Multi Frame Assignment example
+Multi-Frame Assignment example
 ==============================
-This notebook includes an example of the Multi Frame Assignment (MFA) algorithm [#]_.
+This notebook includes an example of the Multi-Frame Assignment (MFA) algorithm [#]_.
+
+The multi-frame assignment algorithm maintains and prunes hypotheses over *N*-Scans. This enables
+the global optimum assignment choice to be selected over *N* multiple frames/timesteps/scans.
 """
-import matplotlib
-# %%
-# Initial imports
-# ---------------
-
-import numpy as np
-from scipy.stats import chi2
-import datetime
-from matplotlib import animation
-from matplotlib.patches import Ellipse
-
-from stonesoup.dataassociator.mfa import MFADataAssociator
-from stonesoup.hypothesiser.mfa import MFAHypothesiser
-from stonesoup.models.transition.linear import CombinedLinearGaussianTransitionModel, \
-                                               ConstantVelocity
-from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
-from stonesoup.types.detection import TrueDetection
-from stonesoup.types.detection import Clutter
-from stonesoup.models.measurement.linear import LinearGaussian
-from stonesoup.types.state import TaggedWeightedGaussianState
-from stonesoup.types.track import Track
-from stonesoup.types.update import GaussianMixtureUpdate
-from stonesoup.types.mixture import GaussianMixture
-from stonesoup.types.numeric import Probability
-from stonesoup.predictor.kalman import KalmanPredictor
-from stonesoup.updater.kalman import KalmanUpdater
-from stonesoup.hypothesiser.probability import PDAHypothesiser
 
 # %%
 # Global parameters
 # -----------------
+import numpy as np
+from scipy.stats import chi2
+
 np.random.seed(2001)  # Used to set the seed for the numpy random number generator
+
 prob_detect = 0.9  # Prob. of detection
 gate_level = 8  # Gate level
 prob_gate = chi2.cdf(gate_level, 2)  # Prob. of gating, computed from gate level for hypothesiser
@@ -48,8 +28,8 @@ slide_window = 3  # Slide window; used by MFA data associator
 # %%
 # Plotting functions
 # ------------------
-# These custom plotting functions are used to show the different assignments
-# present withing the sliding window.
+# First we'll need some custom plotting functions, used to show the different assignments
+# present within the slide window.
 
 def plot_covar(state, ax, color=None):
     w, v = np.linalg.eig(
@@ -83,24 +63,32 @@ def plot_tracks(tracks, ax, slide_window=None):
             parents.append(component)
             mini_tracks.append(parents)
 
-        drawn_states = []
-        for t in mini_tracks:
+        drawn_states = set()
+        for mini_track in mini_tracks:
             # Avoid re-plotting drawn trajectory
-            t_tmp = [x for x in t if x not in drawn_states]
-            if len(t_tmp) < len(t):
-                t_tmp.insert(0, t[-(len(t_tmp)+1)])
-            artists.extend(ax.plot([state.state_vector[0, 0] for state in t_tmp],
-                                   [state.state_vector[2, 0] for state in t_tmp], plot_style))
-            for state in t_tmp:
-                # Avoid re-plotting drawn error ellipses
-                if state not in drawn_states:
-                    artists.append(plot_covar(state, ax, plot_style[0]))
-                    drawn_states.append(state)
+            states_to_plot = [state for state in mini_track if state not in drawn_states]
+            # Insert state before so line is drawn
+            if len(states_to_plot) < len(mini_track):
+                states_to_plot.insert(0, mini_track[-(len(states_to_plot)+1)])
+            artists.extend(ax.plot([state.state_vector[0, 0] for state in states_to_plot],
+                                   [state.state_vector[2, 0] for state in states_to_plot],
+                                   plot_style))
+            # Avoid re-plotting drawn error ellipses
+            for state in set(states_to_plot) - drawn_states:
+                artists.append(plot_covar(state, ax, plot_style[0]))
+                drawn_states.add(state)
+
     return artists
 
 # %%
 # Models
 # ------
+# Create models for target motion :math:`[x \ \dot{x} \ y \ \dot{y}]` with near constant velocity
+# and measurements :math:`[x \ y]`.
+from stonesoup.models.transition.linear import (
+    CombinedLinearGaussianTransitionModel, ConstantVelocity)
+from stonesoup.models.measurement.linear import LinearGaussian
+
 transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.005),
                                                           ConstantVelocity(0.005)])
 measurement_model = LinearGaussian(ndim_state=4, mapping=(0, 2),
@@ -111,7 +99,11 @@ measurement_model = LinearGaussian(ndim_state=4, mapping=(0, 2),
 # Simulate ground truth
 # ---------------------
 # Simulate two targets moving at near constant velocity, crossing each other.
-truths = set()
+import datetime
+from ordered_set import OrderedSet
+
+from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
+truths = OrderedSet()
 start_time = datetime.datetime.now()
 
 truth = GroundTruthPath([GroundTruthState([0, 1, 0, 1], timestamp=start_time)])
@@ -126,19 +118,21 @@ for k in range(1, 21):
     truth.append(GroundTruthState(
         transition_model.function(truth[k-1], noise=True, time_interval=datetime.timedelta(seconds=1)),
         timestamp=start_time+datetime.timedelta(seconds=k)))
-truths.add(truth)
+_ = truths.add(truth)
 
 # %%
 # Simulate measurements
 # ---------------------
-# Simulate both measurements and clutter.
-scans = []
+# Simulate both measurements and clutter, using parameters and models.
+from stonesoup.types.detection import TrueDetection
+from stonesoup.types.detection import Clutter
 
+scans = []
 for k in range(20):
-    detections = set()
+    detections = OrderedSet()
 
     for truth in truths:
-        # Generate actual detection from the state with a 10% chance that no detection is received.
+        # Generate actual detection from the state with a chance that no detection is received.
         if np.random.rand() <= prob_detect:
             measurement = measurement_model.function(truth[k], noise=True)
             detections.add(TrueDetection(state_vector=measurement,
@@ -149,20 +143,30 @@ for k in range(20):
         truth_x = truth[k].state_vector[0]
         truth_y = truth[k].state_vector[2]
         for _ in range(np.random.poisson(lambdaV)):
-            x = np.random.uniform(v_bounds[0][0], v_bounds[0][1])
-            y = np.random.uniform(v_bounds[1][0], v_bounds[1][1])
-            detections.add(Clutter(np.array([[x], [y]]), timestamp=truth[k].timestamp))
+            x = np.random.uniform(*v_bounds[0, :])
+            y = np.random.uniform(*v_bounds[1, :])
+            detections.add(Clutter([[x], [y]], timestamp=truth[k].timestamp))
     scans.append(detections)
 
 # %%
 # Tracking Components
 # -------------------
 
-# Predictor/Updater
+# %%
+# For predictor and updater a linear Kalman filter.
+from stonesoup.predictor.kalman import KalmanPredictor
+from stonesoup.updater.kalman import KalmanUpdater
+
 predictor = KalmanPredictor(transition_model)
 updater = KalmanUpdater(measurement_model)
 
-# Hypothesiser and Data Associator
+# %%
+# For Hypothesiser and Data Associator, the MFA components will be used, wrapping
+# a PDA hypothesiser.
+from stonesoup.dataassociator.mfa import MFADataAssociator
+from stonesoup.hypothesiser.mfa import MFAHypothesiser
+from stonesoup.hypothesiser.probability import PDAHypothesiser
+
 hypothesiser = PDAHypothesiser(predictor, updater, lambda_, prob_gate=prob_gate, prob_detect=prob_detect)
 hypothesiser = MFAHypothesiser(hypothesiser)
 data_associator = MFADataAssociator(hypothesiser, slide_window=slide_window)
@@ -170,7 +174,14 @@ data_associator = MFADataAssociator(hypothesiser, slide_window=slide_window)
 # %%
 # Estimate
 # --------
-# Initiate tracks
+# Initiate priors and tracks. With MFA, a Gaussian mixture is used, where the component
+# represents the estimate as updated using the detections (or missed detection) assignments
+# as recorded on the components tag.
+from stonesoup.types.state import TaggedWeightedGaussianState
+from stonesoup.types.track import Track
+from stonesoup.types.mixture import GaussianMixture
+from stonesoup.types.numeric import Probability
+
 prior1 = GaussianMixture([TaggedWeightedGaussianState([[0], [1], [0], [1]],
                                                       np.diag([1.5, 0.5, 1.5, 0.5]),
                                                       timestamp=start_time,
@@ -179,12 +190,17 @@ prior2 = GaussianMixture([TaggedWeightedGaussianState([[0], [1], [20], [-1]],
                                                       np.diag([1.5, 0.5, 1.5, 0.5]),
                                                       timestamp=start_time,
                                                       weight=Probability(1), tag=[])])
-tracks = {Track([prior1]), Track([prior2])}
+tracks = OrderedSet((Track([prior1]), Track([prior2])))
 
 # %%
-# And finally run through the scans and plot the track and assignments
-# within sliding window.
+# And finally run through the scans, and plot the track and assignments
+# within slide window at each frame/timestep.
+from matplotlib import animation
+from matplotlib.patches import Ellipse
+
 from stonesoup.plotter import Plotter
+from stonesoup.types.update import GaussianMixtureUpdate
+
 plotter = Plotter()
 
 frames = []
