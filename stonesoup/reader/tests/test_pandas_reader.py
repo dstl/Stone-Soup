a838e0cdff5d1c270179e9f169ccb6013c6dfa60
BENJAMINFRASER <Ben-fraser@hotmail.co.uk>
2022-10-22 20:16:33 +0100

    Improved tests for pandas_reader.py within stonesoup/reader/tests directory. Now includes seperate tests for DataFrameGroundTruthReader and DataFrameDetectionReader.

--- stonesoup/reader/tests/test_pandas_reader.py
+++ stonesoup/reader/tests/test_pandas_reader.py
@@ -14,8 +14,8 @@ except ImportError as error:
         "Usage of Pandas Readers readers requires the dependency 'pandas' being installed. ") from error
 
 
-# generate dummy pandas dataframe for testing purposes
-test_dataframe =  pd.read_table(
+# generate dummy pandas dataframe for ground-truth testing purposes
+gt_test_df =  pd.read_table(
                     StringIO("""x,y,z,identifier,t
                     10,20,30,22018332,2018-01-01T14:00:00Z
                     11,21,31,22018332,2018-01-01T14:01:00Z
@@ -26,23 +26,21 @@ test_dataframe =  pd.read_table(
                     sep=',', parse_dates=['t'])
 
 
-@pytest.mark.parametrize(
-    'reader_type',
-    (DataFrameGroundTruthReader, DataFrameDetectionReader))
-def test_csv_gt_2d(reader_type):
+def test_gt_df_2d():
     # run test with:
     #   - 2d co-ordinates
     #   - default time field format
-    df_reader = reader_type(dataframe=test_dataframe,
-                            state_vector_fields=["x", "y"],
-                            time_field="t",
-                            path_id_field="identifier")
+    #   - no special options
+    df_reader = DataFrameGroundTruthReader(dataframe=gt_test_df,
+                                           state_vector_fields=["x", "y"],
+                                           time_field="t",
+                                           path_id_field="identifier")
 
     final_gt_paths = set()
     for _, gt_paths_at_timestep in df_reader:
         final_gt_paths.update(gt_paths_at_timestep)
     assert len(final_gt_paths) == 2
-    
+
     ground_truth_states = [
         gt_state
         for gt_path in final_gt_paths
@@ -57,14 +55,12 @@ def test_csv_gt_2d(reader_type):
         assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
 
 
-@pytest.mark.parametrize(
-    'reader_type',
-    (DataFrameGroundTruthReader, DataFrameDetectionReader))
-def test_csv_gt_3d_time(reader_type):
+def test_gt_df_3d_time():
     # run test with:
     #   - 3d co-ordinates
-    df_reader = reader_type(
-                    dataframe=test_dataframe,
+    #   - time field format specified
+    df_reader = DataFrameGroundTruthReader(
+                    dataframe=gt_test_df,
                     state_vector_fields=["x", "y", "z"],
                     time_field="t",
                     path_id_field="identifier")
@@ -88,12 +84,38 @@ def test_csv_gt_3d_time(reader_type):
         assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
 
 
-@pytest.mark.parametrize(
-    'reader_type',
-    (DataFrameGroundTruthReader, DataFrameDetectionReader))
-def test_csv_gt_multi_per_timestep(reader_type):
+def test_gt_df_3d_time():
+    # run test with:
+    #   - 3d co-ordinates
+    #   - time field format specified
+    df_reader = DataFrameGroundTruthReader(
+                    dataframe=gt_test_df,
+                    state_vector_fields=["x", "y", "z"],
+                    time_field="t",
+                    path_id_field="identifier")
+
+    final_gt_paths = set()
+    for _, gt_paths_at_timestep in df_reader:
+        final_gt_paths.update(gt_paths_at_timestep)
+    assert len(final_gt_paths) == 2
+
+    ground_truth_states = [
+        gt_state
+        for gt_path in final_gt_paths
+        for gt_state in gt_path]
+
+    for n, gt_state in enumerate(
+            sorted(ground_truth_states, key=attrgetter('timestamp'))):
+        assert np.array_equal(
+            gt_state.state_vector, np.array([[10 + n], [20 + n], [30 + n]]))
+        assert gt_state.timestamp.hour == 14
+        assert gt_state.timestamp.minute == n
+        assert gt_state.timestamp.date() == datetime.date(2018, 1, 1)
+
+
+def test_gt_df_multi_per_timestep():
     # test case with multiple entries per timestep
-    test_df =  pd.read_table(
+    det_test_df =  pd.read_table(
                 StringIO("""x,y,z,identifier,t
                 10,20,30,22018332,2018-01-01T14:00:00Z
                 11,21,31,22018332,2018-01-01T14:01:00Z
@@ -103,11 +125,11 @@ def test_csv_gt_multi_per_timestep(reader_type):
                 """), 
                     sep=',', parse_dates=['t'])
 
-    df_reader = reader_type(
-                        dataframe=test_df,
-                        state_vector_fields=["x", "y"],
-                        time_field="t",
-                        path_id_field="identifier")
+    df_reader = DataFrameGroundTruthReader(
+                            dataframe=det_test_df,
+                            state_vector_fields=["x", "y"],
+                            time_field="t",
+                            path_id_field="identifier")
 
     for time, ground_truth_paths in df_reader:
         # remove timestamp from pd timestamp before comparing
@@ -115,3 +137,62 @@ def test_csv_gt_multi_per_timestep(reader_type):
             assert len(ground_truth_paths) == 2
         else:
             assert len(ground_truth_paths) == 1
+
+
+def test_detections_df():
+
+    # generate dummy pandas dataframe for detection testing purposes
+    det_test_df =  pd.read_table(
+                StringIO("""x,y,z,identifier,t
+                10,20,30,22018332,2018-01-01T14:00:00Z
+                11,21,31,22018332,2018-01-01T14:01:00Z
+                12,22,32,22018332,2018-01-01T14:02:00Z
+                """), 
+                sep=',', parse_dates=['t'])
+
+    df_reader = DataFrameDetectionReader(
+        dataframe=det_test_df,
+        state_vector_fields=["x", "y"], 
+        time_field="t")
+
+    detections = [detection for _, detections in df_reader 
+                  for detection in detections]
+
+    for n, detection in enumerate(detections):
+        assert np.array_equal(
+            detection.state_vector, np.array([[10 + n], [20 + n]]))
+        assert detection.timestamp.hour == 14
+        assert detection.timestamp.minute == n
+        assert detection.timestamp.date() == datetime.date(2018, 1, 1)
+        assert isinstance(detection.metadata, dict)
+        assert len(detection.metadata) == 2
+        assert 'z' in detection.metadata.keys()
+        assert 'identifier' in detection.metadata.keys()
+        assert int(detection.metadata['z']) == 30 + n
+        assert str(detection.metadata['identifier']) == '22018332'
+
+
+def test_detections_df_multi_per_timestep():
+    # create test df example with multi-detections per timestep
+    det_test_df =  pd.read_table(
+                    StringIO("""x,y,z,identifier,t
+                    10,20,30,22018332,2018-01-01T14:00:00Z
+                    11,21,31,22018332,2018-01-01T14:01:00Z
+                    12,22,32,22018332,2018-01-01T14:02:00Z
+                    13,23,33,32018332,2018-01-01T14:02:00Z
+                    14,24,34,32018332,2018-01-01T14:03:00Z
+                    """), 
+                    sep=',', parse_dates=['t'])
+
+    # test detections with multiple entries per timestep
+    df_reader = DataFrameDetectionReader(
+        dataframe=det_test_df,
+        state_vector_fields=["x", "y"], 
+        time_field="t")
+
+    for time, detections in df_reader:
+        # remove timestamp from pd timestamp before comparing
+        if time.tz_convert(None) == datetime.datetime(2018, 1, 1, 14, 2):
+            assert len(detections) == 2
+        else:
+            assert len(detections) == 1
