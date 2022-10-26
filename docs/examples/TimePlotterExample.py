cc61fb4bf1eb39e347adf2f35b4665775a5e9a42
G Webb <29946934+gawebb-dstl@users.noreply.github.com>
2022-10-26 09:18:14 +0100

    Added better option for displaying animation in jupyter notebook. Edited some of the simulation parameters

--- docs/examples/TimePlotterExample.py
+++ docs/examples/TimePlotterExample.py
@@ -44,7 +44,7 @@ transition_model = CombinedLinearGaussianTransitionModel(
 measurement_model = LinearGaussian(4, [0, 2], np.diag([0.5, 0.5]))
 
 start_time = datetime.datetime.now()
-
+timestep = datetime.timedelta(seconds=5)
 
 # Simulators
 groundtruth_sim = MultiTargetGroundTruthSimulator(
@@ -53,9 +53,9 @@ groundtruth_sim = MultiTargetGroundTruthSimulator(
         StateVector([[0], [0], [0], [0]]),
         CovarianceMatrix(np.diag([1000, 10, 1000, 10])),
         timestamp=start_time),
-    timestep=datetime.timedelta(seconds=5),
+    timestep=timestep,
     number_steps=60,
-    birth_rate=0.2,
+    birth_rate=0.15,
     death_probability=0.05
 )
 detection_sim = SimpleDetectionSimulator(
@@ -78,7 +78,7 @@ hypothesiser = DistanceHypothesiser(predictor, updater, Mahalanobis(), missed_di
 data_associator = GNNWith2DAssignment(hypothesiser)
 
 # Initiator & Deleter
-deleter = CovarianceBasedDeleter(covar_trace_thresh=1E2)
+deleter = CovarianceBasedDeleter(covar_trace_thresh=1E3)
 initiator = MultiMeasurementInitiator(
     GaussianState(np.array([[0], [0], [0], [0]]),
                   np.diag([0, 100, 0, 1000]),
@@ -87,7 +87,7 @@ initiator = MultiMeasurementInitiator(
     deleter=deleter,
     data_associator=data_associator,
     updater=updater,
-    min_points=2,
+    min_points=3,
 )
 
 # Tracker
@@ -110,6 +110,23 @@ for time, tracks in tracker:
     groundtruth.update(groundtruth_sim.groundtruth_paths)
     detections.update(detection_sim.detections)
     all_tracks.update(tracks)
+
+
+# %%
+# Simulation Results
+# --------------------
+from stonesoup.types.detection import Clutter, TrueDetection
+
+average_life_of_gt = timestep * sum(len(gt) for gt in groundtruth)/len(groundtruth)
+n_clutter = sum(isinstance(det, Clutter) for det in detections)
+n_true_detections = sum(isinstance(det, TrueDetection) for det in detections)
+average_life_of_track = timestep * sum(len(track) for track in all_tracks)/len(all_tracks)
+
+print("The simulation produced:\n",
+      len(groundtruth), "Ground truth paths with an average life of", average_life_of_gt, "\n",
+      n_clutter, "Clutter Detections\n",
+      n_true_detections, "True Detections\n",
+      len(all_tracks), "Tracks with an average life of", average_life_of_track, "\n", )
 
 
 # %%
@@ -142,9 +159,14 @@ plt.show()
 # %%
 # Save the Animation
 # ------------------
-# Save the animation to a gif format. Other formats are available. Uncomment the last line to reload the gif in a Jupyter notebook version.
-
-from IPython.display import Image
-
+# Save the animation to a gif format. Other formats are available.
 plotter.save('example_animation.gif')
-Image('example_animation.gif')
+
+# %%
+# Viewing the Animation in a Jupyter notebook
+# -------------------------------------------
+# The following section displays the animation when using a Jupyter notebook. This option is
+# commented out for the sphinx documentation to build properly. Uncomment `HTML(ani.to_jshtml())`
+# to view the animation in a jupyter notebook
+from IPython.display import HTML
+# HTML(ani.to_jshtml())
