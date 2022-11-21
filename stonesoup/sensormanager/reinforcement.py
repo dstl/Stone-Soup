a1d114139aed527e89472855ef7cc64ef62e280b
Benedict Oakes <sgboakes@liverpool.ac.uk>
2022-11-21 12:18:06 +0000

    Tidied up reinforcement sensor manager

--- stonesoup/sensormanager/reinforcement.py
+++ stonesoup/sensormanager/reinforcement.py
@@ -1,13 +1,6 @@
-import abc
 from abc import abstractmethod, ABC
-from typing import Callable, Set
-import random
-import numpy as np
-import itertools as it
-
 from .base import SensorManager
-from ..base import Base, Property
-from ..sensor.sensor import Sensor
+from ..base import Property
 
 try:
     import tensorflow as tf
@@ -37,6 +30,9 @@ except ImportError as error:
 
 
 class PyEnvironment(object):
+    """The Base PyEnvironment class for generating tf-agent environments.
+
+    """
 
     def __init__(self):
         self._current_time_step = None
@@ -59,33 +55,27 @@ class PyEnvironment(object):
     def time_step_spec(self):
         """Return time_step_spec."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def observation_spec(self):
         """Return observation_spec."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def action_spec(self):
         """Return action_spec."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def _reset(self):
         """Return initial_time_step."""
 
-    @abc.abstractmethod
+    @abstractmethod
     def _step(self, a):
         """Apply action and return new time_step."""
 
 
-class BaseEnvironment(py_environment.PyEnvironment):
-    # def __init__(self):
-    #     super().__init__()
-    #     self._action_spec = array_spec.BoundedArraySpec(
-    #         shape=(), dtype=np.int32, minimum=0, maximum=1, name='action')
-    #     self._observation_spec = array_spec.BoundedArraySpec(
-    #         shape=(self.observation_size,), dtype=np.float32, minimum=0, name='observation')  # distance
-    #     self._episode_ended = False
-    #     self._current_episode = 0
-    #     self._max_episode_length = self.episode_length
+class BaseEnvironment(py_environment.PyEnvironment, ABC):
+    """Base Stone Soup class for implementing tf-agents environments.
+
+    """
 
     def action_spec(self):
         return self._action_spec
@@ -121,27 +111,25 @@ class BaseEnvironment(py_environment.PyEnvironment):
 class ReinforcementLearningSensorManager(SensorManager):
     """A sensor manager that employs reinforcement learning algorithms from tensorflow-agents
     """
+    env: BaseEnvironment = Property(doc="The environment which the agent learns the policy with.")
 
     """
     Things I need:
-        Do I need import the environment? (env validation)
-        If not, need action_spec, observation_spec and reward_function
-        action_spec -> sensors.action(timestep) ? can be multi-D
-        observation_spec -> user defined?
-        reward_function -> user defined, passed into sensor manager (e.g. brute force)
-        need to set hyper-parameters
-        need to set NN size/shape
-        need to choose which RL agent/network used
-        set up reverb table
-        return policy/training performance?
-        or just implement policy with choose_actions
+        Work out reward functions in env
         what to do for multiple sensors? MARL?
     """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        # self.agent = None
+        # self.eval_env = None
+        # self.train_env = None
 
-    def compute_avg_return(self, environment, policy, num_episodes=10):
+    @staticmethod
+    def compute_avg_return(environment, policy, num_episodes=10):
+        """Used to calculate the average reward over a set of episodes.
+
+        """
         time_step = None
         episode_return = None
         total_return = 0.0
@@ -158,7 +146,11 @@ class ReinforcementLearningSensorManager(SensorManager):
         avg_return = total_return / num_episodes
         return avg_return.numpy()[0]
 
-    def dense_layer(self, num_units):
+    @staticmethod
+    def dense_layer(num_units):
+        """Method for generating fully connected layers for use in the neural network.
+
+        """
         # Define a helper function to create Dense layers configured with the right
         # activation and kernel initializer.
         return tf.keras.layers.Dense(
@@ -167,185 +159,187 @@ class ReinforcementLearningSensorManager(SensorManager):
             kernel_initializer=tf.keras.initializers.VarianceScaling(
                 scale=2.0, mode='fan_in', distribution='truncated_normal'))
 
-    def train(self, env, hyperparameters, **kwargs):
-        env.reset()
+    def train(self, hyper_parameters):
+        """Trains a DQN agent on the specified environment to learn a policy that is later used to select actions.
 
-        train_py_env = env
-        eval_py_env = env
-        train_env = tf_py_environment.TFPyEnvironment(train_py_env)
-        eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
+        """
+        if self.env is not None:
+            self.env.reset()
 
-        fc_layer_params = (100, 50)
-        action_tensor_spec = tensor_spec.from_spec(env.action_spec())
-        num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
+            train_py_env = self.env
+            eval_py_env = self.env
+            self.train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+            self.eval_env = tf_py_environment.TFPyEnvironment(eval_py_env)
 
-        # QNetwork consists of a sequence of Dense layers followed by a dense layer
-        # with `num_actions` units to generate one q_value per available action as
-        # its output.
+            fc_layer_params = (100, 50)
+            action_tensor_spec = tensor_spec.from_spec(self.env.action_spec())
+            num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
 
-        dense_layers = [self.dense_layer(num_units) for num_units in fc_layer_params]
-        q_values_layer = tf.keras.layers.Dense(
-            num_actions,
-            activation=None,
-            kernel_initializer=tf.keras.initializers.RandomUniform(
-                minval=-0.03, maxval=0.03),
-            bias_initializer=tf.keras.initializers.Constant(-0.2))
-        q_net = sequential.Sequential(dense_layers + [q_values_layer])
+            # QNetwork consists of a sequence of Dense layers followed by a dense layer
+            # with `num_actions` units to generate one q_value per available action as
+            # its output.
 
-        optimizer = tf.keras.optimizers.Adam(hyperparameters.learning_rate)
+            dense_layers = [self.dense_layer(num_units) for num_units in fc_layer_params]
+            q_values_layer = tf.keras.layers.Dense(
+                num_actions,
+                activation=None,
+                kernel_initializer=tf.keras.initializers.RandomUniform(
+                    minval=-0.03, maxval=0.03),
+                bias_initializer=tf.keras.initializers.Constant(-0.2))
+            q_net = sequential.Sequential(dense_layers + [q_values_layer])
 
-        train_step_counter = tf.Variable(0)
+            optimizer = tf.keras.optimizers.Adam(hyper_parameters['learning_rate'])
 
-        self.agent = dqn_agent.DdqnAgent(
-            train_env.time_step_spec(),
-            train_env.action_spec(),
-            q_network=q_net,
-            optimizer=optimizer,
-            td_errors_loss_fn=common.element_wise_squared_loss,
-            train_step_counter=train_step_counter)
+            train_step_counter = tf.Variable(0)
 
-        self.agent.initialize()
+            self.agent = dqn_agent.DdqnAgent(
+                self.train_env.time_step_spec(),
+                self.train_env.action_spec(),
+                q_network=q_net,
+                optimizer=optimizer,
+                td_errors_loss_fn=common.element_wise_squared_loss,
+                train_step_counter=train_step_counter)
 
-        eval_policy = self.agent.policy
-        collect_policy = self.agent.collect_policy
-        random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(), train_env.action_spec())
+            self.agent.initialize()
 
-        # @test {"skip": true}
-        # See also the metrics module for standard implementations of different metrics.
-        # https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
+            eval_policy = self.agent.policy
+            collect_policy = self.agent.collect_policy
+            random_policy = random_tf_policy.RandomTFPolicy(self.train_env.time_step_spec(), self.train_env.action_spec())
 
-        self.compute_avg_return(eval_env, random_policy, hyperparameters.num_eval_episodes)
+            # @test {"skip": true}
+            # See also the metrics module for standard implementations of different metrics.
+            # https://github.com/tensorflow/agents/tree/master/tf_agents/metrics
 
-        table_name = 'uniform_table'
-        replay_buffer_signature = tensor_spec.from_spec(
-            self.agent.collect_data_spec)
-        replay_buffer_signature = tensor_spec.add_outer_dim(
-            replay_buffer_signature)
+            self.compute_avg_return(self.eval_env, random_policy, hyper_parameters['num_eval_episodes'])
 
-        table = reverb.Table(
-            table_name,
-            max_size=hyperparameters.replay_buffer_max_length,
-            sampler=reverb.selectors.Uniform(),
-            remover=reverb.selectors.Fifo(),
-            rate_limiter=reverb.rate_limiters.MinSize(1),
-            signature=replay_buffer_signature)
+            table_name = 'uniform_table'
+            replay_buffer_signature = tensor_spec.from_spec(
+                self.agent.collect_data_spec)
+            replay_buffer_signature = tensor_spec.add_outer_dim(
+                replay_buffer_signature)
 
-        reverb_server = reverb.Server([table])
+            table = reverb.Table(
+                table_name,
+                max_size=hyper_parameters['replay_buffer_max_length'],
+                sampler=reverb.selectors.Uniform(),
+                remover=reverb.selectors.Fifo(),
+                rate_limiter=reverb.rate_limiters.MinSize(1),
+                signature=replay_buffer_signature)
 
-        replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
-            self.agent.collect_data_spec,
-            table_name=table_name,
-            sequence_length=2,
-            local_server=reverb_server)
+            reverb_server = reverb.Server([table])
 
-        rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
-            replay_buffer.py_client,
-            table_name,
-            sequence_length=2)
+            replay_buffer = reverb_replay_buffer.ReverbReplayBuffer(
+                self.agent.collect_data_spec,
+                table_name=table_name,
+                sequence_length=2,
+                local_server=reverb_server)
 
-        # agent.collect_data_spec
-        # agent.collect_data_spec._fields
+            rb_observer = reverb_utils.ReverbAddTrajectoryObserver(
+                replay_buffer.py_client,
+                table_name,
+                sequence_length=2)
 
-        # @test {"skip": true}
-        py_driver.PyDriver(
-            env,
-            py_tf_eager_policy.PyTFEagerPolicy(
-                random_policy, use_tf_function=True),
-            [rb_observer],
-            max_steps=hyperparameters.initial_collect_steps).run(train_py_env.reset())
+            # agent.collect_data_spec
+            # agent.collect_data_spec._fields
 
-        # Dataset generates trajectories with shape [Bx2x...]
-        dataset = replay_buffer.as_dataset(
-            num_parallel_calls=3,
-            sample_batch_size=hyperparameters.batch_size,
-            num_steps=2).prefetch(3)
+            # @test {"skip": true}
+            py_driver.PyDriver(
+                self.env,
+                py_tf_eager_policy.PyTFEagerPolicy(
+                    random_policy, use_tf_function=True),
+                [rb_observer],
+                max_steps=hyper_parameters['initial_collect_steps']).run(train_py_env.reset())
 
-        iterator = iter(dataset)
+            # Dataset generates trajectories with shape [Bx2x...]
+            dataset = replay_buffer.as_dataset(
+                num_parallel_calls=3,
+                sample_batch_size=hyper_parameters['batch_size'],
+                num_steps=2).prefetch(3)
 
-        # #@test {"skip": true}
-        # try:
-        #   % % time
-        # except:
-        #   pass
+            iterator = iter(dataset)
 
-        # (Optional) Optimize by wrapping some code in a graph using TF function.
-        self.agent.train = common.function(self.agent.train)
+            # (Optional) Optimize by wrapping some code in a graph using TF function.
+            self.agent.train = common.function(self.agent.train)
 
-        # Reset the train step.
-        self.agent.train_step_counter.assign(0)
+            # Reset the train step.
+            self.agent.train_step_counter.assign(0)
 
-        # Evaluate the agent's policy once before training.
-        avg_return = self.compute_avg_return(eval_env, self.agent.policy, hyperparameters.num_eval_episodes)
-        returns = [avg_return]
+            # Evaluate the agent's policy once before training.
+            avg_return = self.compute_avg_return(self.eval_env, self.agent.policy,
+                                                 hyper_parameters['num_eval_episodes'])
+            returns = [avg_return]
 
-        # Reset the environment.
-        time_step = train_py_env.reset()
+            # Reset the environment.
+            time_step = train_py_env.reset()
 
-        # Create a driver to collect experience.
-        collect_driver = py_driver.PyDriver(
-            env,
-            py_tf_eager_policy.PyTFEagerPolicy(
-                self.agent.collect_policy, use_tf_function=True),
-            [rb_observer],
-            max_steps=hyperparameters.collect_steps_per_iteration)
+            # Create a driver to collect experience.
+            collect_driver = py_driver.PyDriver(
+                self.env,
+                py_tf_eager_policy.PyTFEagerPolicy(
+                    self.agent.collect_policy, use_tf_function=True),
+                [rb_observer],
+                max_steps=hyper_parameters['collect_steps_per_iteration'])
 
-        for _ in range(hyperparameters.num_iterations):
-            # print('Iteration: {i}'.format(i=_))
-            # Collect a few steps and save to the replay buffer.
-            time_step, _ = collect_driver.run(time_step)
+            for _ in range(hyper_parameters['num_iterations']):
+                # print('Iteration: {i}'.format(i=_))
+                # Collect a few steps and save to the replay buffer.
+                time_step, _ = collect_driver.run(time_step)
 
-            # Sample a batch of data from the buffer and update the agent's network.
-            experience, unused_info = next(iterator)
-            # import pdb; pdb.set_trace()
-            train_loss = self.agent.train(experience).loss
+                # Sample a batch of data from the buffer and update the agent's network.
+                experience, unused_info = next(iterator)
+                # import pdb; pdb.set_trace()
+                train_loss = self.agent.train(experience).loss
 
-            step = self.agent.train_step_counter.numpy()
+                step = self.agent.train_step_counter.numpy()
 
-            if step % hyperparameters.log_interval == 0:
-                print('step = {0}: loss = {1}'.format(step, train_loss))
+                if step % hyper_parameters['log_interval'] == 0:
+                    print('step = {0}: loss = {1}'.format(step, train_loss))
 
-            if step % hyperparameters.eval_interval == 0:
-                # Agent Policy Output
-                avg_return = self.compute_avg_return(eval_env, self.agent.policy, hyperparameters.num_eval_episodes)
-                returns.append(avg_return)
-                print('step = {0}: Average Return = {1}'.format(step, avg_return))
+                if step % hyper_parameters['eval_interval'] == 0:
+                    # Agent Policy Output
+                    avg_return = self.compute_avg_return(self.eval_env, self.agent.policy,
+                                                         hyper_parameters['num_eval_episodes'])
+                    returns.append(avg_return)
+                    print('step = {0}: Average Return = {1}'.format(step, avg_return))
 
-    def choose_actions(self, tracks, timestamp, env, nchoose=1, **kwargs):
-            """Returns a chosen [list of] action(s) from the action set for each sensor.
-            Chosen action(s) is selected by finding the configuration of sensors: actions which returns
-            the maximum reward, as calculated by a reward function.
+                if step == hyper_parameters['num_iterations']:
+                    print('\n~~~~~\nTraining complete\n~~~~~')
 
-            Parameters
-            ----------
-            tracks: set of :class:`~Track`
-                Set of tracks at given time. Used in reward function.
-            timestamp: :class:`datetime.datetime`
-                Time at which the actions are carried out until
-            nchoose : int
-                Number of actions from the set to choose (default is 1)
-            env : :class:`BaseEnvironment`
-                Environment used during training
+    def choose_actions(self, tracks, timestamp, nchoose=1, **kwargs):
+        """Returns a chosen [list of] action(s) from the action set for each sensor.
+        Chosen action(s) is selected by finding the configuration of sensors: actions which returns
+        the maximum reward, as calculated by a reward function.
 
-            Returns
-            -------
-            : dict
-                The pairs of :class:`~.Sensor`: [:class:`~.Action`] selected
-            """
+        Parameters
+        ----------
+        tracks: set of :class:`~Track`
+            Set of tracks at given time. Used in reward function.
+        timestamp: :class:`tf_agents.trajectories.TimeSpec`
+            Time at which the actions are carried out until
+        nchoose : int
+            Number of actions from the set to choose (default is 1)
 
-            # Need to convert timestamp -> time_step somehow?
-            # Maybe by comparing to start timestamp?
-            # time_step is observation at timestamp?
-            # This is a bit different as we need to use policy to select actions.
-            # Want to select actions from possible actions
-            action_spec = self.agent.policy.action(timestamp)
+        Returns
+        -------
+        : dict
+            The pairs of :class:`~.Sensor`: [:class:`~.Action`] selected
+        """
 
-            configs = [dict() for _ in range(nchoose)]
-            for sensor_action_assignment in configs:
-                for sensor in self.sensors:
-                    chosen_actions = []
-                    action_step = self.agent.policy.action(timestamp)
-                    action = self.agent.policy.action(action_step)
-                    chosen_actions.append(action)
-                    sensor_action_assignment[sensor] = chosen_actions
+        # Need to convert timestamp -> time_step somehow?
+        # Maybe by comparing to start timestamp?
+        # time_step is observation at timestamp?
+        # This is a bit different as we need to use policy to select actions.
+        # Want to select actions from possible actions
+        # action_spec = self.agent.policy.action(timestamp)
+
+        configs = [dict() for _ in range(nchoose)]
+        for sensor_action_assignment in configs:
+            for sensor in self.sensors:
+                chosen_actions = []
+                action_step = self.agent.policy.action(timestamp)
+                action = action_step.action
+                stonesoup_action = self.env.generate_action(action)
+                chosen_actions.append(stonesoup_action)
+                sensor_action_assignment[sensor] = chosen_actions
 
             return configs
