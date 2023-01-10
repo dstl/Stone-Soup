2ed9c7cc948cc9feda5dd87172da7572e260a5e6
spike <spike@dstl.gov.uk>
2023-01-10 14:57:20 +0000

    Update copyright year to 2023

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,7 @@ toc_object_entries = False
 
 # General information about the project.
 project = 'Stone Soup'
-copyright = '2017-2022 Stone Soup contributors'
+copyright = '2017-2023 Stone Soup contributors'
 author = 'Dstl'
 
 # The version info for the project you're documenting, acts as replacement for
