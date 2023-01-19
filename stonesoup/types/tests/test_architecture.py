506c1ab07aba320b2a77e9649d1acbdd3b53dc1a
Oliver Rosoman <orosoman@dstl.gov.uk>
2023-01-19 19:11:43 +0000

    Create basic functionality for Architecture.plot method

--- stonesoup/types/tests/test_architecture.py
+++ stonesoup/types/tests/test_architecture.py
@@ -1,25 +1,19 @@
 from ..architecture import Architecture, NetworkArchitecture, InformationArchitecture, \
     CombinedArchitecture, ProcessingNode, RepeaterNode
-import networkx as nx
 
 import pytest
 
 
 def test_architecture():
-    a, b, c, d, e = RepeaterNode(), RepeaterNode(), RepeaterNode(), RepeaterNode(), RepeaterNode()
-    edge_list_fail = [(a, b), (b, d)]
-    with pytest.raises(ValueError):
-        # Should fail, as the node set does not match the nodes used in the edge list
-        Architecture(edge_list=edge_list_fail, node_set={a, b, c, d}, force_connected=False)
+    a, b, c, d, e = RepeaterNode("a"), RepeaterNode("b"), RepeaterNode("c"), RepeaterNode("d"), \
+                    RepeaterNode("e")
+
     edge_list_unconnected = [(a, b), (c, d)]
     with pytest.raises(ValueError):
-        Architecture(edge_list=edge_list_unconnected, node_set={a, b, c, d}, force_connected=True)
-    a_test = Architecture(edge_list=edge_list_unconnected,
-                          node_set={a, b, c, d},
-                          force_connected=False)
-    edge_list_connected = [(a, b), (b, c)]
+        Architecture(edge_list=edge_list_unconnected, force_connected=True)
+    edge_list_connected = [(a, b), (b, c), (b, d)]
     a_test_hier = Architecture(edge_list=edge_list_connected,
-                               force_connected=False)
+                               force_connected=False, name="bleh")
     edge_list_loop = [(a, b), (b, c), (c, a)]
     a_test_loop = Architecture(edge_list=edge_list_loop,
                                force_connected=False)
@@ -27,6 +21,8 @@ def test_architecture():
     assert a_test_loop.is_connected and a_test_hier.is_connected
     assert a_test_hier.is_hierarchical
     assert not a_test_loop.is_hierarchical
+
+    a_test_hier.plot(dir_path='U:\\My Documents\\temp')
 
 
 def test_information_architecture():
