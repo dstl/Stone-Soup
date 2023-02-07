28e6679205ae827c8c5e43dd6a91275e7c28938c
Benedict Oakes <sgboakes@liverpool.ac.uk>
2023-02-07 12:10:08 +0000

    Removed (unecessary?) Reinforcement import to fix FLAKE8 error

--- stonesoup/sensormanager/__init__.py
+++ stonesoup/sensormanager/__init__.py
@@ -1,11 +1,7 @@
 from .base import SensorManager, RandomSensorManager, BruteForceSensorManager
 from .optimise import _OptimizeSensorManager, OptimizeBruteSensorManager, \
     OptimizeBasinHoppingSensorManager
-try:
-    from .reinforcement import ReinforcementSensorManager
-except:
-    pass
 
 __all__ = ['SensorManager', 'RandomSensorManager', 'BruteForceSensorManager',
            '_OptimizeSensorManager', 'OptimizeBruteSensorManager',
-           'OptimizeBasinHoppingSensorManager', 'ReinforcementSensorManager']
+           'OptimizeBasinHoppingSensorManager']
