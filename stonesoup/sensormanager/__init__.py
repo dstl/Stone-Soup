b3ec4ea9554a06e604785d2de19ec2ee3543207c
Benedict Oakes <sgboakes@liverpool.ac.uk>
2023-03-27 13:08:32 +0100

    Added import in init

--- stonesoup/sensormanager/__init__.py
+++ stonesoup/sensormanager/__init__.py
@@ -1,7 +1,8 @@
 from .base import SensorManager, RandomSensorManager, BruteForceSensorManager
 from .optimise import _OptimizeSensorManager, OptimizeBruteSensorManager, \
     OptimizeBasinHoppingSensorManager
+from .reinforcement import ReinforcementSensorManager
 
 __all__ = ['SensorManager', 'RandomSensorManager', 'BruteForceSensorManager',
            '_OptimizeSensorManager', 'OptimizeBruteSensorManager',
-           'OptimizeBasinHoppingSensorManager']
+           'OptimizeBasinHoppingSensorManager', 'ReinforcementSensorManager']
