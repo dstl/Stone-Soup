111413de7786c9fcdfbb5267085a70f12e9fe7ad
Timothy Glover <t.glover@lboro.ac.uk>
2023-04-13 13:44:12 +0100

    Correct documentation

--- stonesoup/sampler/particle.py
+++ stonesoup/sampler/particle.py
@@ -10,33 +10,33 @@ from ..types.array import StateVectors
 class ParticleSampler(Sampler):
 
     distribution_func: Callable = Property(
-        doc="Callable function that samples from the desired distribtuion.")
+        doc="Callable function that returns samples from the desired distribution.")
 
     params: dict = Property(
-        doc="Dictionary containing the key word arguments to the `distribtuion_func`.")
+        doc="Dictionary containing the keyword arguments for `distribution_func`.")
 
     ndim_state: int = Property(
         doc="Number of dimensions in each sample.")
 
     def sample(self, distribution_func=None, params=None, ndim_state=None, timestamp=None):
-        """Sample particles from the distribution
+        """Samples from the desired distribution and returns as a :class:`~.ParticleState`
 
         Parameters
         ----------
         distribution_func : :class:`~.Callable`, optional
-            Function defining the distribution to be sampled from. Overrides the class
-            property `distribution_func`.
+            Callable function that returns samples from the desired distribution. Overrides the
+            class property `distribution_func`.
         params : dict, optional
-            Pairs of `parameters and values to be input to `distribution_func`. These parameters
-            will update the parameters specified in the class properties and can either be
-            completely redefined or the subset of parameters that need changing. In the case where
-            `distribution_func` is defined here, params needs to be completely specified as the
-            parameters and values must be compatible with the new function.
+            Keyword arguments for `distribution_func`. These parameters will update the
+            parameters specified in the class properties and can either be completely redefined
+            or the subset of parameters that need changing. In the case where `distribution_func`
+            is defined here, params needs to be completely specified as the parameters and values
+            must be compatible with the new function.
         ndim_state : int, optional
             Number of dimensions in the state. Overrides the class property `ndim_state`.
             Only used when `distribution_func` is defined here.
         timestamp : datetime.datetime, optional
-            Timestamp for the returned :class:`~.ParticleState`
+            Timestamp for the returned :class:`~.ParticleState`. Default is ``None``.
 
         Returns
         -------
@@ -59,10 +59,13 @@ class ParticleSampler(Sampler):
             raise ValueError('New distribution_func provided without ndim_state')
 
         samples = distribution_func(**params)
+        # If samples is 1D, make it 2D
         if len(np.shape(samples)) == 1:
             samples = np.array([samples])
+        # get the number of samples returned
         nsamples = (set(np.shape(samples)) - set(np.array([ndim_state]))).pop()
 
+        # Ensure the correct shape of samples for the state_vector
         if np.shape(samples)[0] != ndim_state:
             samples = samples.T
 
