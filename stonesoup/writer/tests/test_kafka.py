f86c82a985b0e0907b50050818ff8e4c355552dd
sglvladi <sglvladi@gmail.com>
2023-05-03 16:37:15 +0100

    Added kafka reader/writer tests

---
+++ stonesoup/writer/tests/test_kafka.py
@@ -0,0 +1,19 @@
+from stonesoup.writer.kafka import KafkaWriter
+
+
+def test_writer_default():
+    # Verify that the writer can be instantiated
+    kafka_config = {
+        "bootstrap.servers": "localhost:9092",
+        "delivery.timeout.ms": 1,   # This is required, since actual sending of data is not tested
+    }
+    writer = KafkaWriter(
+        kafka_config=kafka_config,
+    )
+    assert writer.kafka_config == kafka_config
+
+    # This is a test to see if the write method can be called without errors
+    # No data is actually written to the topic, since no Kafka server is running
+    writer.write("test_topic", {"x": 1, "y": 2}, flush=False)
+    writer.write("test_topic", {"x": 3, "y": 4})
+    writer.write("test_topic", {"x": 5, "y": 6})
