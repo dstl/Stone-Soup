fbc1026f015aebdf02e6b8c377cc482d76ef9f05
rcgorman <rcgorman@dstl.gov.uk>
2023-05-05 17:49:19 +0100

    change basicmetrics to work with multimanager

--- stonesoup/metricgenerator/basicmetrics.py
+++ stonesoup/metricgenerator/basicmetrics.py
@@ -2,10 +2,22 @@ from .base import MetricGenerator
 from ..types.metric import TimeRangeMetric
 from ..types.time import TimeRange
 
+from ..base import Property
+
 
 class BasicMetrics(MetricGenerator):
     """Calculates simple metrics like number of tracks, truth and
     ratio of track-to-truth"""
+    tracks_key: str = Property(doc="Key to access desired set of tracks added to MultiManager")
+    groundtruths_key: str = Property(doc="Key to access desired set of groundtruths added to MultiManager")
+    generator_name: str = Property(doc="Name given to generator to use when accessing generated metrics from "
+                                       "MultiManager")
+
+    def get_ground_truths(self, manager):
+        return manager.states_sets[self.groundtruths_key]
+
+    def get_tracks(self, manager):
+        return manager.states_sets[self.tracks_key]
 
     def compute_metric(self, manager, *args, **kwargs):
         """Compute the metric using the data in the metric manager
@@ -20,19 +32,20 @@ class BasicMetrics(MetricGenerator):
         : list of :class:`~.Metric`
             Contains the metric information
         """
-
+        groundtruth_paths = self.get_ground_truths(manager)
+        tracks = self.get_tracks(manager)
         metrics = []
 
         # Make a list of all the unique timestamps used
-        timestamps = {state.timestamp for state in manager.tracks}
+        timestamps = {state.timestamp for state in tracks}
         timestamps |= {state.timestamp
-                       for path in manager.groundtruth_paths
+                       for path in groundtruth_paths
                        for state in path}
 
         # Number of tracks
         metrics.append(TimeRangeMetric(
             title='Number of targets',
-            value=len(manager.groundtruth_paths),
+            value=len(groundtruth_paths),
             time_range=TimeRange(
                 start_timestamp=min(timestamps),
                 end_timestamp=max(timestamps)),
@@ -40,7 +53,7 @@ class BasicMetrics(MetricGenerator):
 
         metrics.append(TimeRangeMetric(
             title='Number of tracks',
-            value=len(manager.tracks),
+            value=len(tracks),
             time_range=TimeRange(
                 start_timestamp=min(timestamps),
                 end_timestamp=max(timestamps)),
@@ -48,7 +61,7 @@ class BasicMetrics(MetricGenerator):
 
         metrics.append(TimeRangeMetric(
             title='Track-to-target ratio',
-            value=len(manager.tracks) / len(manager.groundtruth_paths),
+            value=len(tracks) / len(groundtruth_paths),
             time_range=TimeRange(
                 start_timestamp=min(timestamps),
                 end_timestamp=max(timestamps)),
