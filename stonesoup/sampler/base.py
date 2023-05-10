000d366f5cd27bd1ed35528258a8a62784fd6e5e
Jordi Barr <31851985+jmbarr@users.noreply.github.com>
2023-05-10 10:54:18 +0100

    Update base.py
    Modified Sampler base class description

--- stonesoup/sampler/base.py
+++ stonesoup/sampler/base.py
@@ -4,8 +4,7 @@ from ..base import Base
 class Sampler(Base):
     """Sampler base class
 
-    A sampler is used to generate discrete samples from a continuous distribution specified by the
-    user. This class is intends to generate any form of sample from any kind from any specified
-    distribution, depending on what is required. A sample may culminate in different forms but the
-    most common would be a :class:`~.State` type.
+    A sampler is used to generate samples from a probability distribution specified by the
+    user. This class is provided to allow any set of samples to be generated from any specified
+    distribution. A :class:`Sampler` should return sub-types of :class:`~.State`.
     """
