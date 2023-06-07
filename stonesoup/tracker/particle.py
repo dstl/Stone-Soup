2a21b3e5c4b20b8b2a55b72dd81c18c90584d198
sglvladi <sglvladi@gmail.com>
2023-06-07 17:11:41 +0100

    Update ELPF classes to make the code DRY

--- stonesoup/tracker/particle.py
+++ stonesoup/tracker/particle.py
@@ -9,11 +9,97 @@ from stonesoup.reader import DetectionReader
 from stonesoup.resampler import Resampler
 from stonesoup.resampler.particle import SystematicResampler
 from stonesoup.tracker import Tracker
+from stonesoup.types.prediction import Prediction
 from stonesoup.types.update import Update
 from stonesoup.updater.particle import ParticleUpdater
 
 
-class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
+class _BaseExpectedLikelihoodParticleFilter(Tracker):
+    initiator: Initiator = Property(doc="Initiator used to initialise the track.")
+    deleter: Deleter = Property(doc="Deleter used to delete the track")
+    detector: DetectionReader = Property(doc="Detector used to generate detection objects.")
+    data_associator: DataAssociator = Property(
+        doc="Association algorithm to pair predictions to detections")
+    updater: ParticleUpdater = Property(
+        doc="Updater used to update the tracks. It is important that no resampling is performed "
+            "in the updater, as this is handled by the tracker.")
+    resampler: Resampler = Property(
+        default=None,
+        doc="Resampler used to resample the particles after the update step. If None, then "
+            ":class:`~.SystematicResampler` is used.")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.updater.resampler is not None:
+            raise ValueError("Updater must not have a resampler")
+        if self.resampler is None:
+            self.resampler = SystematicResampler()
+
+    def __iter__(self):
+        self.detector_iter = iter(self.detector)
+        return super().__iter__()
+
+    def _get_detections(self):
+        time, detections = next(self.detector_iter)
+        timestamps = set([detection.timestamp for detection in detections])
+        if len(timestamps) > 1:
+            raise ValueError("All detections must have the same timestamp")
+        return time, detections
+
+    def _get_new_state(self, multihypothesis):
+        associated_detections = set()
+        missed_detection_weight = next(hyp.weight for hyp in multihypothesis if not hyp)
+
+        # Compute new weights as the sum of the updated weights of the particles
+        # multiplied by the weight of each hypothesis
+        particle_weights_per_hypothesis = []
+        for hypothesis in multihypothesis:
+            if not hypothesis:
+                particle_weights_per_hypothesis.append(
+                    hypothesis.weight.log() + hypothesis.prediction.log_weight
+                )
+            else:
+                # Run the updater on the hypothesis
+                # NOTE: We MUST NOT resample here, as we do that after computing the new weights
+                update = self.updater.update(hypothesis)
+                particle_weights_per_hypothesis.append(
+                    hypothesis.weight.log() + update.log_weight
+                )
+                if hypothesis.weight > missed_detection_weight:
+                    associated_detections.add(hypothesis.measurement)
+
+        # Compute the new state
+        new_log_weights = np.logaddexp.reduce(particle_weights_per_hypothesis, axis=0)
+
+        # Normalise the weights
+        new_log_weights -= logsumexp(new_log_weights)
+
+        # Check if at least one reasonable measurement...
+        if any(hypothesis.weight > missed_detection_weight for hypothesis in multihypothesis):
+            # ...and if so use update type
+            new_state = Update.from_state(
+                state=multihypothesis[0].prediction,
+                state_vector=multihypothesis[0].prediction.state_vector,
+                log_weight=new_log_weights,
+                hypothesis=multihypothesis,
+                timestamp=multihypothesis[0].measurement.timestamp
+            )
+        else:
+            # ...and if not, treat as a prediction
+            new_state = Prediction.from_state(
+                state=multihypothesis[0].prediction,
+                state_vector=multihypothesis[0].prediction.state_vector,
+                log_weight=new_log_weights,
+                timestamp=multihypothesis[0].measurement.timestamp
+            )
+
+        # Resample
+        new_state = self.resampler.resample(new_state)
+
+        return new_state, associated_detections
+
+
+class SingleTargetExpectedLikelihoodParticleFilter(_BaseExpectedLikelihoodParticleFilter):
     """An expected likelihood particle filter (ELPF) [#]_ for tracking a single target.
 
     Track a single object using Stone Soup components. The tracker works by
@@ -40,18 +126,6 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
        with particle filters”, in Signal and Data Processing of Small Targets 2002, 2002,
        vol. 4728, pp. 230–239. doi:10.1117/12.478507.
     """
-    initiator: Initiator = Property(doc="Initiator used to initialise the track.")
-    deleter: Deleter = Property(doc="Deleter used to delete the track")
-    detector: DetectionReader = Property(doc="Detector used to generate detection objects.")
-    data_associator: DataAssociator = Property(
-        doc="Association algorithm to pair predictions to detections")
-    updater: ParticleUpdater = Property(
-        doc="Updater used to update the tracks. It is important that no resampling is performed "
-            "in the updater, as this is handled by the tracker.")
-    resampler: Resampler = Property(
-        default=None,
-        doc="Resampler used to resample the particles after the update step. If None, then "
-            ":class:`~.SystematicResampler` is used.")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -65,61 +139,19 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
     def tracks(self):
         return {self._track} if self._track else set()
 
-    def __iter__(self):
-        self.detector_iter = iter(self.detector)
-        return super().__iter__()
-
     def __next__(self):
-        time, detections = next(self.detector_iter)
-        if self._track is not None:
-            associations = self.data_associator.associate(
-                self.tracks, detections, time)
+        time, detections = self._get_detections()
 
+        if self._track is not None:
+            # Perform data association
+            associations = self.data_associator.associate(self.tracks, detections, time)
             multihypothesis = associations[self._track]
 
-            if len(multihypothesis) == 1 and not multihypothesis[0]:
-
-                # if there is only one hypothesis, and it is the missed detection hypothesis,
-
-                # then there is nothing to update, so we just append the prediction
-                new_state = multihypothesis[0].prediction
-            else:
-                # Compute new weights as the sum of the updated weights of the particles
-                # multiplied by the weight of each hypothesis
-                particle_weights_per_hypothesis = []
-                for hypothesis in multihypothesis:
-                    if not hypothesis:
-                        particle_weights_per_hypothesis.append(
-                            hypothesis.weight.log() + hypothesis.prediction.log_weight
-                        )
-                    else:
-                        # Run the updater on the hypothesis
-                        # NOTE: We MUST NOT resample here, as we do that after computing the
-                        #       new weights
-                        update = self.updater.update(hypothesis)
-                        particle_weights_per_hypothesis.append(
-                            hypothesis.weight.log() + update.log_weight
-                        )
-
-                # Sum the log weights of the particles
-                new_log_weights = np.logaddexp.reduce(particle_weights_per_hypothesis, axis=0)
-
-                # Normalise the weights
-                new_log_weights -= logsumexp(new_log_weights)
-
-                new_state = Update.from_state(
-                    state=multihypothesis[0].prediction,
-                    state_vector=multihypothesis[0].prediction.state_vector,
-                    log_weight=new_log_weights,
-                    hypothesis=multihypothesis,
-                    timestamp=multihypothesis[0].measurement.timestamp
-                )
-
-                # Resample
-                new_state = self.resampler.resample(new_state)
-
+            # Update the track
+            new_state, _ = self._get_new_state(multihypothesis)
             self._track.append(new_state)
 
+        # Track initiation/deletion
         if self._track is None or self.deleter.delete_tracks(self.tracks):
             new_tracks = self.initiator.initiate(detections, time)
             if new_tracks:
@@ -130,7 +162,7 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
         return time, self.tracks
 
 
-class MultiTargetExpectedLikelihoodParticleFilter(Tracker):
+class MultiTargetExpectedLikelihoodParticleFilter(_BaseExpectedLikelihoodParticleFilter):
     """An expected likelihood particle filter (ELPF) [#]_ for tracking multiple targets.
 
     Track multiple objects using Stone Soup components. The tracker works by
@@ -151,39 +183,17 @@ class MultiTargetExpectedLikelihoodParticleFilter(Tracker):
        with particle filters”, in Signal and Data Processing of Small Targets 2002, 2002,
        vol. 4728, pp. 230–239. doi:10.1117/12.478507.
     """
-    initiator: Initiator = Property(doc="Initiator used to initialise the track.")
-    deleter: Deleter = Property(doc="Initiator used to initialise the track.")
-    detector: DetectionReader = Property(doc="Detector used to generate detection objects.")
-    data_associator: DataAssociator = Property(
-        doc="Association algorithm to pair predictions to detections")
-    updater: ParticleUpdater = Property(
-        doc="Updater used to update the tracks. It is important that no resampling is performed "
-            "in the updater, as this is handled by the tracker.")
-    resampler: Resampler = Property(
-        default=None,
-        doc="Resampler used to resample the particles after the update step. If None, then "
-            ":class:`~.SystematicResampler` is used.")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if self.resampler is None:
-            self.resampler = SystematicResampler()
         self._tracks = set()
 
     @property
     def tracks(self):
         return self._tracks
 
-    def __iter__(self):
-        self.detector_iter = iter(self.detector)
-        return super().__iter__()
-
     def __next__(self):
-        time, detections = next(self.detector_iter)
-
-        timestamps = set([detection.timestamp for detection in detections])
-        if len(timestamps) > 1:
-            raise ValueError("All detections must have the same timestamp")
+        time, detections = self._get_detections()
 
         # Perform data association
         associations = self.data_associator.associate(self.tracks, detections, time)
@@ -191,49 +201,14 @@ class MultiTargetExpectedLikelihoodParticleFilter(Tracker):
         unassociated_detections = set(detections)
         for track, multihypothesis in associations.items():
 
-            if len(multihypothesis) == 1 and not multihypothesis[0]:
-                # if there is only one hypothesisa, and it is the missed detection hypothesis,
-                # then there is nothing to update, so we just append the prediction
-                new_state = multihypothesis[0].prediction
-            else:
-                # Compute new weights as the sum of the updated weights of the particles
-                # multiplied by the weight of each hypothesis
-                particle_weights_per_hypothesis = []
-                for hypothesis in multihypothesis:
-                    if not hypothesis:
-                        particle_weights_per_hypothesis.append(
-                            hypothesis.weight.log() + hypothesis.prediction.log_weight
-                        )
-                    else:
-                        # Run the updater on the hypothesis
-                        # NOTE: We MUST NOT resample here, as we do that after computing the
-                        #       new weights
-                        update = self.updater.update(hypothesis)
-                        particle_weights_per_hypothesis.append(
-                            hypothesis.weight.log() + update.log_weight
-                        )
-                        unassociated_detections.discard(hypothesis.measurement)
-
-                # Sum the log weights of the particles
-                new_log_weights = np.logaddexp.reduce(particle_weights_per_hypothesis, axis=0)
-
-                # Normalise the weights
-                new_log_weights -= logsumexp(new_log_weights)
-
-                new_state = Update.from_state(
-                    state=multihypothesis[0].prediction,
-                    state_vector=multihypothesis[0].prediction.state_vector,
-                    log_weight=new_log_weights,
-                    hypothesis=multihypothesis,
-                    timestamp=multihypothesis[0].measurement.timestamp
-                )
-
-                # Resample
-                new_state = self.resampler.resample(new_state)
-
-            # Append the new state to the track
+            # Update the track
+            new_state, associated_detections = self._get_new_state(multihypothesis)
             track.append(new_state)
 
+            # Remove associated detections from the set of unassociated detections
+            unassociated_detections -= associated_detections
+
+        # Initiate new tracks and delete old tracks
         self._tracks -= self.deleter.delete_tracks(self.tracks)
         self._tracks |= self.initiator.initiate(unassociated_detections, time)
 
