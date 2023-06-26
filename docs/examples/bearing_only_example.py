d55c1b379fe4e620095046d057d6f50a321aca96
Alberto Acuto <alberto.acuto9@gmail.com>
2023-06-26 10:47:40 +0100

    fixes for failing tests

--- docs/examples/bearing_only_example.py
+++ docs/examples/bearing_only_example.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-Bearings-only tracking
-====================================
+Bearings-only tracking example
+==============================
 
 Non-linear bearing-only target tracking is a complex problem since the model has only the knowledge of
 the target direction towards the sensor, leaving a lot of information that needs to be extracted from the measurements
@@ -11,7 +11,7 @@ from the sensor.
 The purpose of this short tutorial we show how we can run a bearing-only simulation inside Stone Soup framework.
 
 In this tutorial we simulate a radar placed on top of a moving platform collecting measurements,
-then using :class:~`ExtendedKalmanFilter` we provide the tracking of the target. In this example we employ a
+then using :class:`~.ExtendedKalmanFilter` we provide the tracking of the target. In this example we employ a
 distance based data associator to merge the hypothesis and the detections from the sensor.
 
 """
@@ -21,7 +21,7 @@ distance based data associator to merge the hypothesis and the detections from t
 # ^^^^^^
 # The layout of this example follows:
 #
-# 1) Create the moving platform and the :class:~`RadarBearing` detector;
+# 1) Create the moving platform and the :class:`~.RadarBearing` detector;
 # 2) Generate the target ground truths tracks;
 # 3) Setup the simulation for generating detections and ground truths;
 # 4) Run the simulation and create the plots
@@ -50,9 +50,9 @@ from stonesoup.tracker.simple import SingleTargetTracker
 np.random.seed(2001)
 start_time = datetime.now()
 
-#%%
+# %%
 # 1) Create the moving platform and the Bearing-Only radar
-# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Firstly, we create the initial state of the platform, including the origin point and the
 # cartesian (x,y) movement direction. Then, we create a transition motion (in 2D cartesian coordinates)
 # of the platform.
@@ -66,9 +66,9 @@ from stonesoup.platform.base import MovingPlatform
 # Define the platform location, place it in the origin, and define its cartesian movements.
 # In addition specify the position and velocity mapping. This is done in 2D cartesian coordinates.
 
-platform_state_vector = StateVector([[0],[-5], [0],[-7]])
-position_mapping = (0,2)
-velocity_mapping = (1,3)
+platform_state_vector = StateVector([[0], [-5], [0], [-7]])
+position_mapping = (0, 2)
+velocity_mapping = (1, 3)
 
 # Create the initial state (position and time)
 platform_state = State(platform_state_vector, start_time)
@@ -89,10 +89,10 @@ from stonesoup.sensor.radar.radar import RadarBearing
 
 # Configure the radar noise, since we are using just a dimension we need to specify only the
 # noise associated to the bearing, we assume a bearing accuracy  of +/- 0.025 degrees for each measurements
-noise_covar = CovarianceMatrix(np.array(np.diag([np.deg2rad(0.025)**2])))
+noise_covar = CovarianceMatrix(np.array(np.diag([np.deg2rad(0.025) ** 2])))
 
 # This radar needs to be informed of the x and y mapping of the space target.
-radar_mapping = (0,2)
+radar_mapping = (0, 2)
 
 # Instantiate the radar
 radar = RadarBearing(ndim_state=4,
@@ -106,6 +106,7 @@ platform.add_sensor(radar)
 
 # %%
 # 2) Generate the target movements and ground truths
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # In this tutorial we build a single target ground truth simulator using a simple transition model
 # and a known initial target state.
 #
@@ -120,27 +121,28 @@ transition_model = CombinedLinearGaussianTransitionModel([
 # Define the initial state of the target using a gaussian state specifying the origin point and the
 # accuracy of the predictions on the 2D Cartesian location and speed (using the covariance matrix).
 initial_target_state = GaussianState([50, 0, 50, 0],
-                                     np.diag([1, 1, 1, 1])**2,
+                                     np.diag([1, 1, 1, 1]) ** 2,
                                      timestamp=start_time)
 
 # Setup the ground truth simulation
 groundtruth_simulation = SingleTargetGroundTruthSimulator(
     transition_model=transition_model,
-    initial_state = initial_target_state,
+    initial_state=initial_target_state,
     timestep=timedelta(seconds=1),
-    number_steps = 100
+    number_steps=100
 )
 
 # %%
 # 3) Setup the simulation generating measurements and ground truths
-# After defining the measurement model and simulation,  we will have the needed components to start running our example.
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+# After defining the measurement model and simulation, we will have the needed components to start running our example.
 # The measurement model is the :class:`~.Cartesian2DToBearing`.
 #
 
 # Define the measurement model using a Cartesian to bearing
 meas_model = Cartesian2DToBearing(
     ndim_state=4,
-    mapping=(0,2),
+    mapping=(0, 2),
     noise_covar=noise_covar)
 
 # Import the PlatformDetectionSimulator
@@ -163,13 +165,13 @@ from stonesoup.initiator.simple import SinglePointInitiator
 # same initial state to both the ground truths measurements and tracker.
 
 initiator = SinglePointInitiator(
-    prior_state = initial_target_state,
+    prior_state=initial_target_state,
     measurement_model=meas_model)
 
 # %%
 # Add the hypothesiser components, we use a distance based hypothesiser using a Malahonobis
-# distance to measure the data association between the detections and the tracks.
-# Since in this case we use a single target a simple nearest neighbour will work just fine.
+# distance to do the data association between the detections and the tracks.
+# Since we consider a single target case a simple nearest neighbour will be enough for the data associator.
 #
 
 # Load the hypothesiser and data associator
@@ -177,16 +179,17 @@ from stonesoup.hypothesiser.distance import DistanceHypothesiser
 from stonesoup.measures import Mahalanobis
 
 hypothesiser = DistanceHypothesiser(predictor, updater,
-                                     measure=Mahalanobis(),
-                                     missed_distance=5)
+                                    measure=Mahalanobis(),
+                                    missed_distance=5)
 
 from stonesoup.dataassociator.neighbour import NearestNeighbour
+
 data_associator = NearestNeighbour(hypothesiser)
 
 # Instantiate the time based deleter
 deleter = UpdateTimeStepsDeleter(time_steps_since_update=3)
 
-# Build the kalman tracker
+# Build the Kalman tracker
 kalman_tracker = SingleTargetTracker(
     initiator=initiator,
     deleter=deleter,
@@ -197,6 +200,7 @@ kalman_tracker = SingleTargetTracker(
 
 # %%
 # 4) Run the simulation and create the plots
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # We have everything for running the simulation, we have the tracker, the sensor
 # detections and detector movements.
 #
@@ -227,22 +231,22 @@ fig = plt.figure(figsize=(10, 6))
 ax = fig.add_subplot(1, 1, 1)
 ax.set_xlabel("$East$ (m)")
 ax.set_ylabel("$North$ (m)")
-ax.set_ylim(-1000, 400) # change eventually in case there is a different model or seed
+ax.set_ylim(-1000, 400)  # change eventually in case there is a different model or seed
 ax.set_xlim(-900, 100)
 
 for key in groundtruth_paths:
     X = [coord[0] for coord in groundtruth_paths[key]]
     Y = [coord[1] for coord in groundtruth_paths[key]]
-    ax.plot(X, Y, color='r',lw=3, alpha=0.9, label='Ground truth track')  # Plot true locations in red
+    ax.plot(X, Y, color='r', lw=3, alpha=0.9, label='Ground truth track')  # Plot true locations in red
 
 for key in kalman_tracks:
     X = [coord[0] for coord in kalman_tracks[key]]
     Y = [coord[1] for coord in kalman_tracks[key]]
-    ax.plot(X, Y, color='b',lw=3, alpha=0.9, label='Track estimates')  # Plot track estimates in blue
+    ax.plot(X, Y, color='b', lw=3, alpha=0.9, label='Track estimates')  # Plot track estimates in blue
 
 # plot platform location
-ax.plot(Xp, Yp, color='y',lw=2, label='Platform track')
-ax.scatter(0,0, color='k', alpha=0.8, label='Platform origin')
+ax.plot(Xp, Yp, color='y', lw=2, label='Platform track')
+ax.scatter(0, 0, color='k', alpha=0.8, label='Platform origin')
 plt.legend(frameon=False, fontsize='medium')
 plt.show()
 
@@ -252,11 +256,11 @@ plt.show()
 #
 
 
-"""
-References
-[1] Xiangdong Lin, Thiagalingam Kirubarajan, Yaakov Bar-Shalom, Simon Maskell, 
-"Comparison of EKF, pseudomeasurement, and particle filters for a bearing-only target tracking problem", in 
-Signal and Data Processing of Small Targets 2002, 2002, vol 4728, pp. 240-250. doi:10.1117/12.478508
-[2] Vincent J. Aidala, "Kalman Filter Behavior in Bearing-Only Tracking Applications", IEEE Transactions
-on Aerospace Electronic Systems, Vol. 15, January 1979
-"""
+# %%
+# References
+# ^^^^^^^^^^
+# ..[#] Xiangdong Lin, Thiagalingam Kirubarajan, Yaakov Bar-Shalom, Simon Maskell,
+#   "Comparison of EKF, pseudomeasurement, and particle filters for a bearing-only target tracking problem", in
+#   Signal and Data Processing of Small Targets 2002, 2002, vol 4728, pp. 240-250. doi:10.1117/12.478508
+#.. [#] Vincent J. Aidala, "Kalman Filter Behavior in Bearing-Only Tracking Applications", IEEE Transactions
+#   on Aerospace Electronic Systems, Vol. 15, January 1979
