6768800a06c6154b7a4fb6c323ecad7aede4596c
spike <spike@dstl.gov.uk>
2023-07-06 17:10:22 +0100

    More work on tracker, still not working

--- stonesoup/tracker/fusion.py
+++ stonesoup/tracker/fusion.py
@@ -1,4 +1,5 @@
 import datetime
+import numpy as np
 
 from .base import Tracker
 from ..base import Property
@@ -8,11 +9,24 @@ from stonesoup.reader.base import DetectionReader
 from stonesoup.types.detection import Detection
 from stonesoup.types.track import Track
 from stonesoup.tracker.pointprocess import PointProcessMultiTargetTracker
+from stonesoup.tracker.simple import MultiTargetTracker
 from stonesoup.feeder.track import Tracks2GaussianDetectionFeeder
+from stonesoup.architecture.edge import FusionQueue
+
+from stonesoup.models.transition.categorical import MarkovianTransitionModel
+from stonesoup.types.state import CategoricalState
+from stonesoup.predictor.categorical import HMMPredictor
+from stonesoup.updater.categorical import HMMUpdater
+from stonesoup.hypothesiser.categorical import HMMHypothesiser
+from stonesoup.dataassociator.neighbour import GNNWith2DAssignment
+from stonesoup.initiator.categorical import SimpleCategoricalMeasurementInitiator
+from stonesoup.deleter.time import UpdateTimeStepsDeleter
+from stonesoup.tracker.tests.conftest import detector
 
 
 class DummyDetector(DetectionReader):
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.current = kwargs['current']
 
     @BufferedGenerator.generator_method
@@ -24,11 +38,25 @@ class SimpleFusionTracker(Tracker):  # implement tracks method
     """Presumes data from this node are detections, and from every other node are tracks
     Acts as a wrapper around a base tracker. Track is fixed after the sliding window.
     It exists within it, but the States may change. """
-    base_tracker: Tracker = Property(doc="Tracker given to the fusion node")
+    base_tracker: Tracker = Property(
+        default=MultiTargetTracker(initiator=SimpleCategoricalMeasurementInitiator(
+            prior_state=CategoricalState([1 / 2, 1 / 2],
+                                         categories=['Friendly', 'Enemy']),
+            updater=HMMUpdater()),
+                                   deleter=UpdateTimeStepsDeleter(2),
+                                   detector=detector,
+                                   data_associator=GNNWith2DAssignment(
+                                       HMMHypothesiser(
+                                           predictor=HMMPredictor(MarkovianTransitionModel(
+                                               transition_matrix=np.array([[0.6, 0.4],
+                                                                           [0.39, 0.61]]))),
+                                           updater=HMMUpdater())),
+                                   updater=HMMUpdater()),
+        doc="Tracker given to the fusion node")
     sliding_window: int = Property(default=30,
                               doc="The number of time steps before the result is fixed")
-    queue: = Property(default=None, doc="Queue which feeds in data")
-    current_time: datetime = Property()
+    queue: FusionQueue = Property(default=None, doc="Queue which feeds in data")
+    current_time: datetime.datetime = Property(default=datetime.datetime(2000, 1, 1, 0, 0, 0, 1))
     detector: DetectionReader = Property(doc= "Detection reader to read detections from the queue")
 
     def __init__(self, *args, **kwargs):
@@ -44,6 +72,9 @@ class SimpleFusionTracker(Tracker):  # implement tracks method
         return super().__iter__()
 
     def __next__(self):
+        if self.current_time == datetime.datetime(2000, 1, 1, 0, 0, 0, 1):
+            raise ValueError("current_time tracker property hasn't updated to match Architecture "
+                             "current_time")
         # Get data in time window
 
         # I think this block of code is equivalent to a detection reader?
