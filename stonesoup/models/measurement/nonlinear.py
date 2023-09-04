030524fa60bc90e811daea9bc21810479e826ab0
Timothy Glover <t.glover@lboro.ac.uk>
2023-09-04 14:50:57 +0100

    Fix documentation error

--- stonesoup/models/measurement/nonlinear.py
+++ stonesoup/models/measurement/nonlinear.py
@@ -1364,25 +1364,25 @@ class IsotropicPlume(GaussianModel, MeasurementModel):
 
     def function(self, state: State, noise: Union[bool, np.ndarray] = False, **kwargs) -> Union[
                  StateVector, StateVectors]:
-        r"""Model function :math:`h(\vec{x}_t,\vec{v}_t)`
+    r"""Model function :math:`h(\vec{x}_t,\vec{v}_t)`
 
-        Parameters
-        ----------
-        state: :class:`~.StateVector`
-            An input source term state vector
+    Parameters
+    ----------
+    state: :class:`~.StateVector`
+        An input source term state vector
 
-        noise: :class:`numpy.ndarray` or bool
-            An externally generated random process noise sample (the default is
-           `False`, in which case no noise will be added
-            if 'True', the output of :meth:`~.Model.rvs` is added). If `False`,
-            then the model also does not consider the :attr:`sensing_threshold`
-            and :attr:`missed_detection_probability`
+    noise: :class:`numpy.ndarray` or bool
+        An externally generated random process noise sample (the default is
+       `False`, in which case no noise will be added
+        if 'True', the output of :meth:`~.Model.rvs` is added). If `False`,
+        then the model also does not consider the :attr:`sensing_threshold`
+        and :attr:`missed_detection_probability`
 
-        Returns
-        -------
-        : :class:`numpy.ndarray` of shape (1, 1)
-            The model function evaluated with the provided source term
-        """
+    Returns
+    -------
+    : :class:`numpy.ndarray` of shape (1, 1)
+        The model function evaluated with the provided source term
+    """
 
         x, y, z, Q, u, phi, ci, cii = state.state_vector[self.mapping].view(np.ndarray)
 
