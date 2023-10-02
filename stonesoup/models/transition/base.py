0172ac9f026323b0206a911e8bfb75b944fa05f5
Timothy Glover <t.glover@lboro.ac.uk>
2023-10-02 10:32:38 +0100

    Add StateVectors handling to CombinedGaussianTransitionModel

--- stonesoup/models/transition/base.py
+++ stonesoup/models/transition/base.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from ..base import Model, GaussianModel
 from ...base import Property
+from ...types.array import StateVectors
 from ...types.state import StateVector
 
 
@@ -53,7 +54,10 @@ class CombinedGaussianTransitionModel(TransitionModel, GaussianModel):
         """
         temp_state = copy.copy(state)
         ndim_count = 0
-        state_vector = np.zeros(state.state_vector.shape).view(StateVector)
+        if state.state_vector.shape[1] > 1:
+            state_vector = np.zeros(state.state_vector.shape).view(StateVectors)
+        else:
+            state_vector = np.zeros(state.state_vector.shape).view(StateVector)
         # To handle explicit noise vector(s) passed in we set the noise for the individual models
         # to False and add the noise later. When noise is Boolean, we just pass in that value.
         if noise is None:
