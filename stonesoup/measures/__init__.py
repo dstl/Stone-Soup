bf91035819aae344d4b4dc6c20ba9125722008f3
G Webb <gawebb@dstl.gov.uk>
2023-11-01 21:15:37 +0000

    Minor change to imports

--- stonesoup/measures/__init__.py
+++ stonesoup/measures/__init__.py
@@ -1,3 +1,3 @@
-from .state import Measure, Euclidean, EuclideanWeighted, SquaredMahalanobis, \
-    Mahalanobis, SquaredGaussianHellinger, GaussianHellinger, ObservationAccuracy  # noqa: F401
+from .state import Measure, Euclidean, EuclideanWeighted, SquaredMahalanobis, Mahalanobis, \
+    SquaredGaussianHellinger, GaussianHellinger, ObservationAccuracy, KLDivergence  # noqa: F401
 # State measures are imported to avoid breaking changes
