d1e4e09dba2b0a36bfccd8bff8f4ee2518f5a626
jwright2 <jwright2@dstl.gov.uk>
2024-01-04 17:50:41 +0000

    Update copyright year

--- stonesoup/__init__.py
+++ stonesoup/__init__.py
@@ -9,12 +9,12 @@ except DistributionNotFound:
     pass
 
 __copyright__ = '''\
-© Crown Copyright 2017-2023 Defence Science and Technology Laboratory UK
-© Crown Copyright 2018-2023 Defence Research and Development Canada / Recherche et développement pour la défense Canada
-© Copyright 2018-2023 University of Liverpool UK
-© Copyright 2020-2023 Fraunhofer FKIE
-© Copyright 2020-2023 John Hiles
-© Copyright 2021-2023 Roke Manor Research Ltd UK
-© Copyright 2023 Loughborough University UK
+© Crown Copyright 2017-2024 Defence Science and Technology Laboratory UK
+© Crown Copyright 2018-2024 Defence Research and Development Canada / Recherche et développement pour la défense Canada
+© Copyright 2018-2024 University of Liverpool UK
+© Copyright 2020-2024 Fraunhofer FKIE
+© Copyright 2020-2024 John Hiles
+© Copyright 2021-2024 Roke Manor Research Ltd UK
+© Copyright 2023-2024 Loughborough University UK
 '''  # noqa: E501
 __license__ = 'MIT'
