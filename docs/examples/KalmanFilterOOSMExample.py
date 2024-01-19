480623d811ce0d9390702e099554479abd6077d2
Alberto Acuto <alberto.acuto9@gmail.com>
2024-01-19 15:36:29 +0000

    Adding a comparison with EKF to try to prove the EnKF functionality with OOSM

--- docs/examples/KalmanFilterOOSMExample.py
+++ docs/examples/KalmanFilterOOSMExample.py
@@ -8,13 +8,18 @@ Kalman filter with Out-of-Sequence measurements
 """
 
 # %%
-# In this example we present how to deal with out of sequence measurements (OOSM) using Kalman
-# filters. This problem is significant in real-world applications where data from different sources
-# can have some delays and different timesteps (e.g., two sensors observing a target).
+# In other examples we have shown how to deal with out-of-sequence measurements (OOSM)
+# with methods using inverse-time dynamics or creating a buffer where store and re-order
+# the measurements.
+# In this example we present how to deal with OOS measurements using Kalman
+# filters. The problem of OOS measurements is significant in real-world applications
+# where data from different sources can have some delays and different timesteps
+# (e.g., two sensors observing a target).
 # In the literature, there are examples (e.g., [#]_) on how to deal with such time-delays and
 # uncertain timesteps.
 # In this example, we present a simpler application using the Ensemble Kalman Filter algorithm
-# available in Stone Soup to tackle this problem.
+# available in Stone Soup to tackle this problem and we compare against an Extended Kalman
+# filter to measure the differences.
 #
 # This example follows the following structure:
 #
@@ -44,8 +49,8 @@ from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
 from stonesoup.types.state import GaussianState
 
 # instantiate the transition model
-transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.05),
-                                                          ConstantVelocity(0.05)])
+transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.5),
+                                                          ConstantVelocity(0.5)])
 
 # %%
 # 1) Prepare the ground truth;
@@ -78,13 +83,13 @@ from stonesoup.models.measurement.nonlinear import CartesianToBearingRange
 measurement_model_1 = CartesianToBearingRange(  # relative to the first sensor
     ndim_state=4,
     mapping=(0, 2),
-    noise_covar=np.diag([np.radians(0.1), 3]),
+    noise_covar=np.diag([np.radians(1), 20]),
     translation_offset=np.array([[-60], [0]]))
 
 measurement_model_2 = CartesianToBearingRange(  # relative to the second sensor
     ndim_state=4,
     mapping=(0, 2),
-    noise_covar=np.diag([np.radians(0.1), 3]),
+    noise_covar=np.diag([np.radians(1), 20]),
     translation_offset=np.array([[20], [60]]))
 
 # Generate the detections
@@ -141,21 +146,35 @@ plotter.show()
 # 3) Instantiate the tracking components;
 # ---------------------------------------
 # In this example we employ an Ensemble Kalman filter by loading the predictor and updater using
-# :class:`~.EnsemblePredictor` and :class:`~.EnsembleUpdater`. This filter combines the
-# functionality of the Kalman filter and the particle filter. To access the two sets of detections
-# we initialise two different updaters, with the correct measurement models, to take into account
-# the detection translation offset.
+# :class:`~.EnsemblePredictor` and :class:`~.EnsembleUpdater` and an Extended Kalman filter using
+# :class:`~.ExtendedKalmanPredictor` and :class:`~.ExtendedKalmanUpdater`.
+# The Ensemble Kalman filter combines the functionality of the Kalman filter and the
+# particle filter. To access the two sets of detections we initialise two different updaters,
+# with the correct measurement models, to take into account the detection translation offset.
 # For simplicity, we assume that the detections obtained by the second sensor can be corrected by
 # the delay and correspond to the first set of measurements.
 #
 
+# Load the ensemble components
 from stonesoup.predictor.ensemble import EnsemblePredictor
 from stonesoup.updater.ensemble import EnsembleUpdater
 
-predictor = EnsemblePredictor(transition_model)
+# load the extended kalman filter components
+from stonesoup.updater.kalman import ExtendedKalmanUpdater
+from stonesoup.predictor.kalman import ExtendedKalmanPredictor
+
+# EKF components
+predictor = ExtendedKalmanPredictor(transition_model)
+
 # We employ two updaters to account for the different sensor translation offsets
-updater1 = EnsembleUpdater(measurement_model_1)
-updater2 = EnsembleUpdater(measurement_model_2)
+updater1 = ExtendedKalmanUpdater(measurement_model_1)
+updater2 = ExtendedKalmanUpdater(measurement_model_2)
+
+# EnKF componens
+EnKFpredictor = EnsemblePredictor(transition_model)
+
+EnKFupdater1 = EnsembleUpdater(measurement_model_1)
+EnKFupdater2 = EnsembleUpdater(measurement_model_2)
 
 # Load the Ensemble for the prior
 from stonesoup.types.state import EnsembleState
@@ -165,12 +184,17 @@ ensemble = EnsembleState.generate_ensemble(
     covar=np.diag([5, 2, 5, 2]),
     num_vectors=100)
 # This approach is similar to setting up the priors for Particle filter tracking
-prior = EnsembleState(state_vector=ensemble, timestamp=start_time)
+EnKFprior = EnsembleState(state_vector=ensemble, timestamp=start_time)
+
+# EKF prior
+prior = GaussianState(state_vector=np.array([0, 1, 0, 1]),
+                        covar=np.diag([1, 1, 1, 1]),
+                        timestamp=start_time)
 
 # %%
 # 4) Run the tracker and visualise the results.
 # ---------------------------------------------
-# We have prepared the tracker components and we are ready to generate the final track.
+# We have prepared the tracker components and we are ready to generate the final tracks.
 # Before passing the measurements to the tracking algorithm we need to correct the detections by
 # the delay so we can process them correctly. Finally we can plot the resulting track.
 #
@@ -182,9 +206,12 @@ from stonesoup.types.track import Track
 delay = measurements2[0].timestamp - measurements1[0].timestamp
 
 # Initiate the empty track
-track = Track()
+track = Track(prior)
+EnKFtrack = Track(EnKFprior)
+
 for k in range(num_steps):  # loop over the timestep
-    # We consider the same prior
+
+    # We consider the same prior - EKF
     prediction = predictor.predict(prior, timestamp=measurements1[k].timestamp)
     hypothesis = SingleHypothesis(prediction, measurements1[k])
     post = updater1.update(hypothesis)
@@ -194,18 +221,37 @@ for k in range(num_steps):  # loop over the timestep
     prediction = predictor.predict(prior, timestamp=measurements2[k].timestamp - delay)
     hypothesis = SingleHypothesis(prediction, measurements2[k])
     post = updater2.update(hypothesis)
-    track.append(post)
+    EnKFtrack.append(post)
     prior = track[-2]
 
-plotter.plot_tracks(track, [0, 2], uncertainty=True, particle=True)
+    # EnKF
+    prediction = EnKFpredictor.predict(EnKFprior, timestamp=measurements1[k].timestamp)
+    hypothesis = SingleHypothesis(prediction, measurements1[k])
+    post = EnKFupdater1.update(hypothesis)
+    EnKFtrack.append(post)
+
+    # correct the measurements timestamp with the delay
+    prediction = EnKFpredictor.predict(EnKFprior, timestamp=measurements2[k].timestamp - delay)
+    hypothesis = SingleHypothesis(prediction, measurements2[k])
+    post = EnKFupdater2.update(hypothesis)
+    EnKFtrack.append(post)
+    EnKFprior = EnKFtrack[-2]
+
+plotter.plot_tracks(track, [0, 2], track_label='EKF')
+plotter.plot_tracks(EnKFtrack, [0, 2], uncertainty=False, particle=False,
+                    track_label='EnKF', line=dict(color='red'))
 plotter.show()
 
 # %%
 # Conclusions
 # -----------
 # In this simple example we have presented how it is possible to perform the tracking with the
-# presence of out of sequence or delayed measurements from a sensor. We have employed an Ensemble
-# Kalman filter to ease the tracking.
+# presence of out of sequence or delayed measurements from a sensor.
+# We have shown two cases one using an Extended Kalman filter and the Ensemble Kalman filter.
+# In both cases we have corrected the measurements with delay and we have performed the tracking,
+# in this simpler example we don't appreciate significant differences between the two algorithms.
+# In more complex scenario (multi-target and/or cluttered scans) the particle-filter like approach of the
+# Ensemble Kalman Filter can provide better tracking performances.
 
 # %%
 # References
