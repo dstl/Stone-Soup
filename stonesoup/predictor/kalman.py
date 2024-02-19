38a7e6be27475ae24a9b50fadba8935e180bbe77
Jordi Barr <31851985+jmbarr@users.noreply.github.com>
2024-02-19 12:02:22 +0000

    Update stonesoup/predictor/kalman.py
    (Can't actually see the difference here.)
    
    Co-authored-by: Steven Hiscocks <sdhiscocks@dstl.gov.uk>

--- stonesoup/predictor/kalman.py
+++ stonesoup/predictor/kalman.py
@@ -495,11 +495,6 @@ class CubatureKalmanPredictor(KalmanPredictor):
         doc="Scaling parameter. Default is 1.0. Lower values select points closer to the mean and "
             "vice versa.")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self._time_interval = None
-
     def _transition_and_control_function(self, prior_state, **kwargs):
         r"""Returns the result of applying the transition and control functions
         for the cubature transform
