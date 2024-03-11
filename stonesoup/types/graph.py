ee9fcb34906287e97dd979fb21c5f8cf0d5b602e
sglvladi <sglvladi@gmail.com>
2024-03-11 14:34:06 +0000

    Remove plotting functions from RoadNetwork

--- stonesoup/types/graph.py
+++ stonesoup/types/graph.py
@@ -246,29 +246,6 @@ class RoadNetwork(nx.DiGraph):
 
         return net
 
-    def plot_network(self, ax, node_size=0.1, width=0.5, with_labels=False):
-        # Get node positions
-        pos = nx.get_node_attributes(self, 'pos')
-
-        nx.draw_networkx(self, pos, arrows=False, ax=ax, node_size=node_size, width=width,
-                         with_labels=with_labels)
-
-    def highlight_nodes(self, ax, nodes, node_size=0.1, node_color='m', node_shape='s', label=None):
-        # Get node positions
-        pos = nx.get_node_attributes(self, 'pos')
-
-        return nx.draw_networkx_nodes(self, pos, nodelist=nodes, ax=ax, node_size=node_size,
-                                      node_color=node_color, node_shape=node_shape, label=label)
-
-    def highlight_edges(self, ax, edges_idx, width=2.0, edge_color='m', style='solid', arrows=False,
-                        label=None):
-        edges = self.edge_list[edges_idx]
-        pos = nx.get_node_attributes(self, 'pos')
-
-        return nx.draw_networkx_edges(self, pos, edgelist=edges, ax=ax, width=width,
-                                      edge_color=edge_color,
-                                      style=style, arrows=arrows, label=label)
-
     def shortest_path(self, source=None, target=None, weight='weight', method='dijkstra',
                       path_type='node'):
         """Compute the shortest path(s) between source and target.
