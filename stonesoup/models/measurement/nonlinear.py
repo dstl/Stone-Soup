7cb20f9b07a72e16727aed4b50321a47c6bce846
Alberto Acuto <alberto.acuto9@gmail.com>
2024-03-21 15:18:03 +0000

    change from StateVector to Statevectors in measurement model

--- stonesoup/models/measurement/nonlinear.py
+++ stonesoup/models/measurement/nonlinear.py
@@ -1634,8 +1634,8 @@ class CartesianAzimuthElevationMeasurementModel(NonLinearGaussianMeasurement):
         azimuths = [Azimuth(angle) for angle in mod_bearing(absolute_azimuth - heading)]
 
         elevations = [Elevation(angle) for angle in absolute_elevation - pitch]
-
-        return StateVector([azimuths, elevations]) + noise
+        #print(len(azimuths), len(elevations))
+        return StateVectors([azimuths, elevations]) + noise
 
     def rvs(self, num_samples=1, **kwargs) -> Union[StateVector, StateVectors]:
         out = super().rvs(num_samples, **kwargs)
