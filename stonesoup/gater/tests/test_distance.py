6571cdcc79bfc9c44358d929a89b55ccb4077022
G Webb <gawebb@dstl.gov.uk>
2024-03-25 16:06:13 +0000

    Added extra tests

--- stonesoup/gater/tests/test_distance.py
+++ stonesoup/gater/tests/test_distance.py
@@ -7,7 +7,9 @@ from ..distance import DistanceGater, TrackingStateSpaceDistanceGater
 from ... import measures as measures
 from ...hypothesiser.distance import DistanceHypothesiser
 from ...hypothesiser.probability import PDAHypothesiser
-from ...models.measurement.nonlinear import CartesianToBearingRange
+from ...models.measurement.linear import LinearGaussian
+from ...models.measurement.nonlinear import CartesianToBearingRange, Cartesian2DToBearing, \
+    CombinedReversibleGaussianMeasurementModel
 from ...models.transition.linear import CombinedLinearGaussianTransitionModel, \
     ConstantVelocity
 from ...predictor.kalman import ExtendedKalmanPredictor
@@ -81,7 +83,7 @@ def standard_distance_hypothesiser():
     transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(0.05),
                                                               ConstantVelocity(0.05)])
     predictor = ExtendedKalmanPredictor(transition_model)
-    updater = ExtendedKalmanUpdater(measurement_model)
+    updater = ExtendedKalmanUpdater(bearing_range_measurement_model)
     hypothesiser = DistanceHypothesiser(predictor, updater, measure=measures.Euclidean())
     return hypothesiser
 
@@ -104,13 +106,19 @@ track = Track([GaussianState(
     covar=np.diag([1.5, 0.5, 1.5, 0.5])
 )])
 
-measurement_model = CartesianToBearingRange(
+bearing_range_measurement_model = CartesianToBearingRange(
     ndim_state=4,
     mapping=(0, 2),
     noise_covar=np.diag([np.radians(0.2), 1]),  # Covariance matrix. 0.2 degree variance in
     # bearing and 1 metre in range
     translation_offset=np.array([[0], [0]])  # Offset measurements to location of
     # sensor in cartesian.
+)
+
+linear_measurement_model = LinearGaussian(
+    ndim_state=4,
+    mapping=(0, 2),
+    noise_covar=np.diag([1, 1]),  # Covariance matrix. 1 each in x and y
 )
 
 
@@ -120,17 +128,21 @@ measurement_model = CartesianToBearingRange(
         # Perfect detection
         Detection(state_vector=[np.pi/2, 100],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
         # Detection with slightly wrong range
         Detection(state_vector=[np.pi/2, 109],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
         # Detection with slightly wrong bearing
         Detection(state_vector=[0.01 + np.pi/2, 109],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
+        # Perfect Linear Detection
+        Detection(state_vector=[0, 100],
+                  timestamp=start_time,
+                  measurement_model=linear_measurement_model),
     ],
-    ids=["test1", "test2", "test3"]
+    ids=["bearing_range_1", "bearing_range_2", "bearing_range_3", "linear_4"]
 )
 def test_tracking_state_space_distance_gater_good_detections(
         detection, standard_distance_hypothesiser,
@@ -152,21 +164,26 @@ def test_tracking_state_space_distance_gater_good_detections(
         # Correct range. Wrong bearing
         Detection(state_vector=[0, 100],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
         # Correct bearing. Wrong range
         Detection(state_vector=[np.pi / 2, 111],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
         # Detection with slightly wrong bearing
         Detection(state_vector=[-np.pi / 2, 109],
                   timestamp=start_time,
-                  measurement_model=measurement_model),
+                  measurement_model=bearing_range_measurement_model),
+        # Wrong Linear Detection
+        Detection(state_vector=[10, 110],
+                  timestamp=start_time,
+                  measurement_model=linear_measurement_model),
     ],
-    ids=["test1", "test2", "test3"]
+    ids=["bearing_range_1", "bearing_range_2", "bearing_range_3", "linear_4"]
 )
 def test_tracking_state_space_distance_gater_bad_detections(
         detection, standard_distance_hypothesiser,
         hypothesiser_with_tracking_state_space_distance_gater):
+
     hypothesiser_with_gate = hypothesiser_with_tracking_state_space_distance_gater
 
     hypotheses_no_gate = standard_distance_hypothesiser.hypothesise(
@@ -176,3 +193,53 @@ def test_tracking_state_space_distance_gater_bad_detections(
 
     assert len(hypotheses_no_gate) == 2
     assert len(hypotheses_with_gate) == 1
+
+
+def test_tracking_state_space_distance_gater_value_error(
+        hypothesiser_with_tracking_state_space_distance_gater):
+
+    detection = Detection(state_vector=[0, 100], timestamp=start_time, measurement_model=None)
+
+    hypothesiser_with_gate = hypothesiser_with_tracking_state_space_distance_gater
+
+    with pytest.raises(ValueError):
+        hypothesiser_with_gate.hypothesise(track, {detection}, timestamp=start_time)
+
+
+bearing_measurement_model = Cartesian2DToBearing(ndim_state=4,
+                                                 mapping=(0, 2),
+                                                 noise_covar=np.diag([1]))
+
+
+@pytest.mark.parametrize(
+    "detection",
+    [
+        # Irreversible detection
+        Detection(state_vector=[0, 100],
+                  timestamp=start_time,
+                  measurement_model=bearing_measurement_model
+                  ),
+        # Irreversible detection 2
+        Detection(state_vector=[0, 100],
+                  timestamp=start_time,
+                  measurement_model=CombinedReversibleGaussianMeasurementModel(
+                      [bearing_measurement_model])
+                  ),
+    ],
+    ids=["test_1", "test_2"]
+)
+@pytest.mark.parametrize("allow_non_reversible_detections", [True, False])
+def test_tracking_state_space_distance_gater_irreversible_detections(
+        detection, allow_non_reversible_detections,
+        hypothesiser_with_tracking_state_space_distance_gater):
+
+    hypothesiser_with_gate = hypothesiser_with_tracking_state_space_distance_gater
+    hypothesiser_with_gate.allow_non_reversible_detections = allow_non_reversible_detections
+
+    hypotheses_with_gate = hypothesiser_with_gate.hypothesise(
+        track, {detection}, timestamp=start_time)
+
+    if allow_non_reversible_detections:
+        assert len(hypotheses_with_gate) == 2
+    else:
+        assert len(hypotheses_with_gate) == 1
