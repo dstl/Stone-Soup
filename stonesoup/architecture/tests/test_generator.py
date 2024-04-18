48a35465212bba0acf553bb2fd9110f2f070544f
spike <spike@dstl.gov.uk>
2024-04-18 10:30:40 +0100

    Rework to ArchitectureGenerator logic to produce more feasible architectures

--- stonesoup/architecture/tests/test_generator.py
+++ stonesoup/architecture/tests/test_generator.py
@@ -1,7 +1,7 @@
+import numpy as np
 import pytest
 
-from stonesoup.architecture import Architecture
-from stonesoup.architecture.edge import FusionQueue, Edges
+from stonesoup.architecture.edge import FusionQueue
 from stonesoup.architecture.generator import InformationArchitectureGenerator, \
     NetworkArchitectureGenerator
 from stonesoup.sensor.sensor import Sensor
@@ -49,7 +49,7 @@ def test_info_generate_hierarchical(generator_params):
     gen = InformationArchitectureGenerator(arch_type='hierarchical',
                                            start_time=start_time,
                                            mean_degree=2,
-                                           node_ratio=[3, 1, 1],
+                                           node_ratio=[2, 2, 1],
                                            base_tracker=base_tracker,
                                            base_sensor=base_sensor,
                                            n_archs=3)
@@ -59,13 +59,13 @@ def test_info_generate_hierarchical(generator_params):
     for arch in archs:
 
         # Check correct number of nodes
-        assert len(arch.all_nodes) == sum([3, 1, 1])
+        assert len(arch.all_nodes) == sum([2, 2, 1])
 
         # Check node types
-        assert len(arch.fusion_nodes) == sum([1, 1])
-        assert len(arch.sensor_nodes) == sum([3, 1])
+        assert len(arch.fusion_nodes) == sum([2, 1])
+        assert len(arch.sensor_nodes) == sum([2, 2])
 
-        assert len(arch.edges) == sum([3, 1, 1]) - 1
+        assert len(arch.edges) == sum([2, 2, 1]) - 1
 
         for node in arch.fusion_nodes:
             # Check each fusion node has a tracker
@@ -83,13 +83,15 @@ def test_info_generate_decentralised(generator_params):
     base_sensor = generator_params['base_sensor']
     base_tracker = generator_params['base_tracker']
 
+    mean_deg = 2.5
+
     gen = InformationArchitectureGenerator(arch_type='decentralised',
                                            start_time=start_time,
-                                           mean_degree=2,
+                                           mean_degree=mean_deg,
                                            node_ratio=[3, 1, 1],
                                            base_tracker=base_tracker,
                                            base_sensor=base_sensor,
-                                           n_archs=3)
+                                           n_archs=2)
 
     archs = gen.generate()
 
@@ -102,7 +104,7 @@ def test_info_generate_decentralised(generator_params):
         assert len(arch.fusion_nodes) == sum([1, 1])
         assert len(arch.sensor_nodes) == sum([3, 1])
 
-        assert len(arch.edges) == sum([3, 1, 1])
+        assert len(arch.edges) == np.ceil(sum([3, 1, 1]) * mean_deg * 0.5)
 
         for node in arch.fusion_nodes:
             # Check each fusion node has a tracker
@@ -222,35 +224,3 @@ def test_net_generate_decentralised(generator_params):
         for node in arch.sensor_nodes:
             # Check each sensor node has a Sensor
             assert isinstance(node.sensor, Sensor)
-
-
-def test_invalid_graph(generator_params):
-    start_time = generator_params['start_time']
-    base_sensor = generator_params['base_sensor']
-    base_tracker = generator_params['base_tracker']
-
-    gen = NetworkArchitectureGenerator(arch_type='decentralised',
-                                       start_time=start_time,
-                                       mean_degree=0.5,
-                                       node_ratio=[3, 1, 1],
-                                       base_tracker=base_tracker,
-                                       base_sensor=base_sensor,
-                                       n_archs=3,
-                                       allow_invalid_graph=False)
-
-    with pytest.raises(ValueError):
-        gen.generate()
-
-    gen2 = NetworkArchitectureGenerator(arch_type='decentralised',
-                                        start_time=start_time,
-                                        mean_degree=0.5,
-                                        node_ratio=[3, 1, 1],
-                                        base_tracker=base_tracker,
-                                        base_sensor=base_sensor,
-                                        n_archs=3,
-                                        allow_invalid_graph=True)
-    archs = gen2.generate()
-
-    for arch in archs:
-        assert isinstance(arch, Architecture)
-        assert isinstance(arch.edges, Edges)
