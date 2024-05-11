8156222d44f2a03e0c18cc4e069e0fc7249e8dd1
zhenyuen <zhenyuen.dev@gmail.com>
2024-05-11 02:51:20 +0100

    feat: add Gamma NVM mixture driving process

--- stonesoup/models/transition/driver.py
+++ stonesoup/models/transition/driver.py
@@ -1,8 +1,12 @@
 import numpy as np
-
+from scipy.special import gammainc, gamma
 from stonesoup.types.array import CovarianceMatrix, StateVector
 from .base_driver import NormalSigmaMeanDriver, NormalVarianceMeanDriver
 from ...base import Property
+
+
+def incgammal(s: float, x: float) -> float: # Helper function
+    return gammainc(s, x) * gamma(s)
 
 class AlphaStableNSMDriver(NormalSigmaMeanDriver):
     alpha: float = Property(doc="Alpha parameter.")
@@ -15,32 +19,23 @@ class AlphaStableNSMDriver(NormalSigmaMeanDriver):
     def _hfunc(self, epochs: np.ndarray) -> np.ndarray:
         return np.power(epochs, -1.0 / self.alpha)
 
+    def _first_moment(self) -> np.ndarray:
+        return self.alpha / (1. - self.alpha) * np.power(self.c, 1. - 1. / self.alpha)
+    
+    def _second_moment(self) -> np.ndarray:
+        return self.alpha / (2. - self.alpha) * np.power(self.c, 1. - 2. / self.alpha)
+
     def _residual_mean(self, e_ft: np.ndarray) -> CovarianceMatrix:
-        if 1 < self.alpha < 2 or self.noise_case == 1:
+        if 1 < self.alpha < 2:
             m = e_ft.shape[0]
             r_mean = np.zeros((m, 1))
-        elif self.noise_case == 2 or self.noise_case == 3:
-            r_mean = e_ft * self.mu_W # (m, 1)
-        else:
-            raise AttributeError("invalid noise case")
-        return self.alpha / (1. - self.alpha) * np.power(self.c, 1. - 1. / self.alpha) * r_mean # (m, 1)
-
-    def _residual_cov(self, e_ft: np.ndarray) -> CovarianceMatrix:
-        if self.noise_case == 1:
-            m = e_ft.shape[0]
-            r_sigma2 = np.zeros((m, m))
-        elif self.noise_case == 2:
-            r_sigma2 =  e_ft @ e_ft.T * (self.mu_W ** 2 + self.sigma_W2)
-        elif self.noise_case == 3:
-            r_sigma2 =  e_ft @ e_ft.T * self.sigma_W2
-        else:
-            raise AttributeError("invalid noise case")
-        return self.alpha / (2. - self.alpha) * np.power(self.c, 1. - 2. / self.alpha) * r_sigma2 # (m, m)
+            return r_mean
+        return super()._residual_mean(e_ft)
     
     def _centering(self, e_ft: np.ndarray) -> StateVector:
         if 1 < self.alpha < 2:
             term = e_ft * self.mu_W # (m, 1)
-            return self.alpha / (1. - self.alpha) * np.power(self.c, 1. - 1. / self.alpha) * term # (m, 1)
+            return self._first_moment() * term # (m, 1)
         elif 0 < self.alpha < 1:
             m = e_ft.shape[0]
             return np.zeros((m, 1))
@@ -52,5 +47,20 @@ class AlphaStableNSMDriver(NormalSigmaMeanDriver):
         return np.ones_like(jsizes) # (n_jumps, n_samples)
     
 
-class GammaNSMDriver(NormalVarianceMeanDriver):
-    pass
+class GammaNVMDriver(NormalVarianceMeanDriver):
+    nu: float = Property(doc="Scale parameter")
+    beta: float = Property(doc="Shape parameter")
+
+    def _hfunc(self, epochs: np.ndarray) -> np.ndarray:
+        return 1. / (self.beta * (np.exp(epochs / self.nu) - 1.))
+
+    def _first_moment(self) -> np.ndarray:
+        truncation = self._hfunc(self.c)
+        return (self.nu / self.beta) * incgammal(1., self.beta * truncation)
+    
+    def _second_moment(self) -> np.ndarray:
+        truncation = self._hfunc(self.c)
+        return (self.nu / self.beta ** 2) * incgammal(2., self.beta * truncation)
+
+    def _thinning_probabilities(self, jsizes: np.ndarray) -> np.ndarray:
+        return (1. + self.beta * jsizes) * np.exp(-self.beta * jsizes) # (n_jumps, n_samples)
