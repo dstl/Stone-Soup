464d316b16bc5682c9670f6e11bbf46eabc6a95c
narykov <narykov@gmail.com>
2024-05-13 18:27:24 +0100

    Intermediate commit

--- docs/examples/Nonlinear_measurement_IPLF.py
+++ docs/examples/Nonlinear_measurement_IPLF.py
@@ -2,6 +2,8 @@ from abc import ABC
 from datetime import datetime
 from typing import Union
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
 
 import numpy as np
 from datetime import datetime, timedelta
@@ -9,6 +11,7 @@ from stonesoup.types.groundtruth import GroundTruthPath, GroundTruthState
 from stonesoup.models.transition.linear import CombinedLinearGaussianTransitionModel, \
                                                ConstantVelocity
 from stonesoup.models.transition.nonlinear import GaussianTransitionModel
+from stonesoup.models.transition.base import CombinedGaussianTransitionModel
 from stonesoup.models.measurement.nonlinear import NonLinearGaussianMeasurement
 from stonesoup.types.state import StateVector, StateVectors, GaussianState, State
 from stonesoup.types.hypothesis import SingleHypothesis
@@ -21,17 +24,15 @@ from stonesoup.types.array import CovarianceMatrix
 from stonesoup.base import Property
 
 
-class MyMeasurementModel(NonLinearGaussianMeasurement):
+class CustomNonlinearMeasurementModel(NonLinearGaussianMeasurement):
     """
     Cubic measurement, adapted from https://livrepository.liverpool.ac.uk/3015339/1/PL_smoothing_accepted1.pdf
     """
+    power: int = Property(default=3, doc='Raised to the power of.')
+    denominator: float = Property(default=20, doc='Denominator.')
 
     def __init__(self, *args, **kwargs):
-        """
-        Ensure that the translation offset is initiated
-        """
         super().__init__(*args, **kwargs)
-        self.n = 3
 
     @property
     def ndim_meas(self) -> int:
@@ -69,14 +70,12 @@ class MyMeasurementModel(NonLinearGaussianMeasurement):
             else:
                 noise = 0
 
-        # Cubic measurement
-        x_meas = [state.state_vector[self.mapping[0]] ** self.n / 20]
-        # y_meas = [state.state_vector[self.mapping[1]] ** self.n / 20]
+        # Cubic (or other power) measurement
+        meas = (state.state_vector[self.mapping, :] ** self.power) / self.denominator
 
-        return StateVectors([x_meas]) + noise
+        return meas + noise
 
-
-class MyTransitionModel(GaussianTransitionModel, TimeInvariantModel, ABC):
+class CustomNonlinearTransitionModel(GaussianTransitionModel, TimeInvariantModel, ABC):
     covariance_matrix: CovarianceMatrix = Property(
         doc="Transition noise covariance matrix :math:`\\mathbf{Q}`.")
 
@@ -90,14 +89,16 @@ class MyTransitionModel(GaussianTransitionModel, TimeInvariantModel, ABC):
             The number of model state dimensions.
         """
 
-        return 1
+        return 2
 
     def function(self, state: State, noise: Union[bool, np.ndarray] = False,
                  **kwargs) -> Union[StateVector, StateVectors]:
-        time_interval_sec = kwargs['time_interval'].total_seconds()
         sv1 = state.state_vector
-        sv2 = StateVector(
-            [0.9 * sv1[0] + 10 * sv1[0] /(1 + sv1[0] ** 2)])
+
+        sv2_0 = 0.9 * sv1[0] + 10 * sv1[0] / (1 + sv1[0] ** 2) + 8 * np.cos(1.2 * (sv1[1] + 1))
+        sv2_1 = sv1[1] + 1
+        sv2 = StateVector([sv2_0, sv2_1])
+
         if isinstance(noise, bool) or noise is None:
             if noise:
                 noise = self.rvs(num_samples=state.state_vector.shape[1], **kwargs)
@@ -117,27 +118,198 @@ class MyTransitionModel(GaussianTransitionModel, TimeInvariantModel, ABC):
 
         return self.covariance_matrix
 
+# class CustomNonlinearTransitionModel1D(GaussianTransitionModel, TimeInvariantModel, ABC):
+#     covariance_matrix: CovarianceMatrix = Property(
+#         doc="Transition noise covariance matrix :math:`\\mathbf{Q}`.")
+#
+#     @property
+#     def ndim_state(self):
+#         """ndim_state getter method
+#
+#         Returns
+#         -------
+#         : :class:`int`
+#             The number of model state dimensions.
+#         """
+#
+#         return 1
+#
+#     def function(self, state: State, noise: Union[bool, np.ndarray] = False,
+#                  **kwargs) -> Union[StateVector, StateVectors]:
+#         time_interval_sec = kwargs['time_interval'].total_seconds()
+#         sv1 = state.state_vector
+#         sv2 = StateVector(
+#             [0.9 * sv1[0] + 10 * sv1[0] /(1 + sv1[0] ** 2)])
+#         if isinstance(noise, bool) or noise is None:
+#             if noise:
+#                 noise = self.rvs(num_samples=state.state_vector.shape[1], **kwargs)
+#             else:
+#                 noise = 0
+#         return sv2 + noise
+#
+#     def rvs(self, num_samples: int = 1, random_state=None, **kwargs) ->\
+#             Union[StateVector, StateVectors]:
+#         r"""Model noise/sample generation function
+#
+#         Generates noise samples from the model.
+#
+#         In mathematical terms, this can be written as:
+#
+#         .. math::
+#
+#             v_t \sim \mathcal{N}(0,Q)
+#
+#         where :math:`v_t =` ``noise`` and :math:`Q` = :attr:`covar`.
+#
+#         Parameters
+#         ----------
+#         num_samples: scalar, optional
+#             The number of samples to be generated (the default is 1)
+#
+#         Returns
+#         -------
+#         noise : 2-D array of shape (:attr:`ndim`, ``num_samples``)
+#             A set of Np samples, generated from the model's noise
+#             distribution.
+#         """
+#
+#         covar = self.covar(**kwargs)
+#
+#         # If model has None-type covariance or contains None, it does not represent a Gaussian
+#         if covar is None or None in covar:
+#             raise ValueError("Cannot generate rvs from None-type covariance")
+#
+#         random_state = random_state if random_state is not None else self.random_state
+#
+#         noise = multivariate_normal.rvs(
+#             np.zeros(self.ndim), covar, num_samples, random_state=random_state)
+#
+#         noise = np.atleast_2d(noise)
+#
+#         if self.ndim > 1:
+#             noise = noise.T  # numpy.rvs method squeezes 1-dimensional matrices to integers
+#
+#         if num_samples == 1:
+#             return noise.view(StateVector)
+#         else:
+#             return noise.view(StateVectors)
+#
+#     def covar(self, **kwargs):
+#         """Returns the transition model noise covariance matrix.
+#
+#         Returns
+#         -------
+#         : :class:`stonesoup.types.state.CovarianceMatrix` of shape\
+#         (:py:attr:`~ndim_state`, :py:attr:`~ndim_state`)
+#             The process noise covariance.
+#         """
+#
+#         return self.covariance_matrix
+
+class CustomNonlinearTransitionModelVar(GaussianTransitionModel, TimeInvariantModel, ABC):
+    covariance_matrix: CovarianceMatrix = Property(
+        doc="Transition noise covariance matrix :math:`\\mathbf{Q}`.")
+
+    @property
+    def ndim_state(self):
+        """ndim_state getter method
+
+        Returns
+        -------
+        : :class:`int`
+            The number of model state dimensions.
+        """
+
+        return 1
+
+    def function(self, state: State, noise: Union[bool, np.ndarray] = False,
+                 **kwargs) -> Union[StateVector, StateVectors]:
+        sv1 = state.state_vector
+
+        sv2_0 = 0.9 * sv1[0] + 10 * sv1[0] / (1 + sv1[0] ** 2)  #  + 8 * np.cos(1.2 * (sv1[1] + 1))
+        sv2 = StateVector([sv2_0])
+
+        if isinstance(noise, bool) or noise is None:
+            if noise:
+                noise = self.rvs(num_samples=state.state_vector.shape[1], **kwargs)
+            else:
+                noise = 0
+        return sv2 + noise
+
+    def covar(self, **kwargs):
+        """Returns the transition model noise covariance matrix.
+
+        Returns
+        -------
+        : :class:`stonesoup.types.state.CovarianceMatrix` of shape\
+        (:py:attr:`~ndim_state`, :py:attr:`~ndim_state`)
+            The process noise covariance.
+        """
+
+        return self.covariance_matrix
+
+class CustomNonlinearTransitionModelStat(GaussianTransitionModel, TimeInvariantModel, ABC):
+    covariance_matrix: CovarianceMatrix = Property(
+        doc="Transition noise covariance matrix :math:`\\mathbf{Q}`.")
+
+    @property
+    def ndim_state(self):
+        """ndim_state getter method
+
+        Returns
+        -------
+        : :class:`int`
+            The number of model state dimensions.
+        """
+
+        return 1
+
+    def function(self, state: State, noise: Union[bool, np.ndarray] = False,
+                 **kwargs) -> Union[StateVector, StateVectors]:
+        sv1 = state.state_vector
+
+        sv2 = sv1 + 1
+
+        if isinstance(noise, bool) or noise is None:
+            if noise:
+                noise = self.rvs(num_samples=state.state_vector.shape[1], **kwargs)
+            else:
+                noise = 0
+        return sv2 + noise
+
+    def covar(self, **kwargs):
+        """Returns the transition model noise covariance matrix.
+
+        Returns
+        -------
+        : :class:`stonesoup.types.state.CovarianceMatrix` of shape\
+        (:py:attr:`~ndim_state`, :py:attr:`~ndim_state`)
+            The process noise covariance.
+        """
+
+        return self.covariance_matrix
+
+
 def main():
+
     start_time = datetime.now().replace(microsecond=0)
     np.random.seed(1991)
-
     # Create ground truth
-
-    q_x = 0.05
-    q_y = 0.05
-    transition_model = CombinedLinearGaussianTransitionModel([ConstantVelocity(q_x),
-                                                              ConstantVelocity(q_y)])
-
-    transition_model = MyTransitionModel(covariance_matrix=CovarianceMatrix([[1]]))
-
+    transition_model = CustomNonlinearTransitionModel(covariance_matrix=np.diag([1, 0]))
+    # # transition_model = CustomNonlinearTransitionModel1D(covariance_matrix=np.array([[1]]), seed=19915)
+    # # x = transition_model.function(State(state_vector=StateVector([1])), time_interval=timedelta(seconds=1))
+    # transition_model = CombinedGaussianTransitionModel(
+    #     CustomNonlinearTransitionModelVar(covariance_matrix=np.array(np.diag([1]))),
+    #     CustomNonlinearTransitionModelStat(covariance_matrix=np.array(np.diag([0])))
+    # )
     timesteps = [start_time]
-    truth = GroundTruthPath([GroundTruthState([0], timestamp=timesteps[0])])
+    prior_0 = GaussianState([5, 0], np.diag([4, np.finfo(float).eps]), timestamp=start_time)
+    sample = np.random.multivariate_normal(prior_0.state_vector.ravel(), prior_0.covar, 1)
+    sample[:, 0] = prior_0.state_vector[0]
+    sample[:, 1] = prior_0.state_vector[1]
+    truth = GroundTruthPath([GroundTruthState(sample, timestamp=timesteps[0])])
 
-    num_steps = 20
-    num_steps = 10
-    num_steps = 5
-    num_steps = 3
-    num_steps = 2
+    num_steps = 50
 
     for k in range(1, num_steps + 1):
         timesteps.append(start_time + timedelta(seconds=k))  # add next timestep to list of timesteps
@@ -145,22 +317,13 @@ def main():
             transition_model.function(truth[k - 1], noise=True, time_interval=timedelta(seconds=1)),
             timestamp=timesteps[k]))
 
-    print()
-    # timesteps = [start_time]
-    # truth = GroundTruthPath([GroundTruthState([0, 1, 0, 1], timestamp=timesteps[0])])
-    #
-    # num_steps = 20
-    # for k in range(1, num_steps + 1):
-    #     timesteps.append(start_time + timedelta(seconds=k))  # add next timestep to list of timesteps
-    #     truth.append(GroundTruthState(
-    #         transition_model.function(truth[k - 1], noise=True, time_interval=timedelta(seconds=1)),
-    #         timestamp=timesteps[k]))
-
     # Generate measurements
 
     from stonesoup.types.detection import Detection
-    measurement_model = MyMeasurementModel(
-        ndim_state=1,  # Number of state dimensions (position and velocity in 2D)
+    measurement_model = CustomNonlinearMeasurementModel(
+        power=3,
+        denominator=20,
+        ndim_state=2,  # Number of state dimensions (position and velocity in 2D)
         mapping=(0, ),  # Mapping measurement vector index to state index
         noise_covar=np.array([[1]])
     )
@@ -171,12 +334,14 @@ def main():
         measurements.append(Detection(measurement,
                                       timestamp=state.timestamp,
                                       measurement_model=measurement_model))
+    # measurements = measurements[1:]
     fig, ax = plt.subplots()
     tr = [state.state_vector.ravel()[0] for state in truth]
+    tr_timestamps = [state.timestamp for state in truth]
     ms = [state.state_vector.ravel()[0] for state in measurements]
-    ax.plot(tr, label='truth')
-    ax.plot(ms, label='meas')
-    # plt.legend()
+    ms_timestamps = [state.timestamp for state in measurements]
+
+
 
 
     predictor = UnscentedKalmanPredictor(transition_model=transition_model, beta=2, kappa=30)
@@ -184,15 +349,15 @@ def main():
     updater_ukf = UnscentedKalmanUpdater(beta=2, kappa=30)
     updater_iplf = IPLFKalmanUpdater(beta=2, kappa=30, max_iterations=5)
 
-    prior_0 = GaussianState([[0]], np.diag([5]), timestamp=start_time)
 
 
 
     # UKF filtering
     prior = prior_0
+    prior.state_vector[1] = -1
     track_ukf = Track([prior])
     prior = track_ukf[-1]
-    for i, measurement in enumerate(measurements[1:]):
+    for i, measurement in enumerate(measurements):
         if i == 1:
             print()
         prediction = predictor.predict(prior, timestamp=measurement.timestamp)
@@ -202,13 +367,16 @@ def main():
         prior = track_ukf[-1]
 
     tr_ukf = [state.state_vector.ravel()[0] for state in track_ukf]
-    ax.plot(tr_ukf, label='ukf')
+    ukf_timestamps = [state.timestamp for state in track_ukf]
+
+
 
     # IPLF filtering
     prior = prior_0
+    prior.state_vector[1] = -1
     track_iplf = Track([prior])
     prior = track_iplf[-1]
-    for i, measurement in enumerate(measurements[1:]):
+    for i, measurement in enumerate(measurements):
         print()
         prediction = predictor_iplf.predict(prior, timestamp=measurement.timestamp)
         hypothesis = SingleHypothesis(prediction, measurement)  # Group a prediction and measurement
@@ -217,12 +385,8 @@ def main():
         prior = track_iplf[-1]
 
     tr_iplf = [state.state_vector.ravel()[0] for state in track_iplf]
-    ax.plot(tr_iplf, label='iplf')
-    from matplotlib.ticker import MaxNLocator, FuncFormatter, MultipleLocator
-    # plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-    plt.gca().xaxis.set_major_locator(MultipleLocator(1))
+    iplf_timestamps = [state.timestamp for state in track_iplf]
 
-    plt.legend()
 
     for state in track_iplf:
         if np.any(np.linalg.eigvals(state.covar) < 0):
@@ -231,17 +395,30 @@ def main():
     covs_ukf = [state.covar for state in track_ukf]
 
     # IPLS (s)moothing
-    from stonesoup.smoother.kalman import IPLSKalmanSmoother
-    smoother = IPLSKalmanSmoother(transition_model=transition_model, n_iterations=2)
+    from stonesoup.smoother.kalman import IPLSKalmanSmoother, UnscentedKalmanSmoother
+    smoother = IPLSKalmanSmoother(transition_model=transition_model, n_iterations=0, beta=2, kappa=30)
+    # smoother_ukf = UnscentedKalmanSmoother()
     track_ipls = smoother.smooth(track_iplf)
+    tr_ipls = [state.state_vector.ravel()[0] for state in track_ipls]
+    ipls_timestamps = [state.timestamp for state in track_ipls]
 
+    ax.plot(ukf_timestamps, tr_ukf, label='ukf')
+    ax.plot(iplf_timestamps, tr_iplf, label='iplf')
+    ax.plot(ipls_timestamps, tr_ipls, label='ipls')
+    ax.plot(tr_timestamps, tr, label='truth', color='k')
+    ax.scatter(ms_timestamps, ms, label='meas', marker='x')
+
+
+
+    plt.legend()
 
     error_ukf = []
     error_iplf = []
     error_ipls = []
     from stonesoup.measures import Euclidean
     mapping = measurement_model.mapping
-    for state_true, state_ukf, state_iplf, state_ipls in zip(truth, track_ukf, track_iplf, track_ipls):
+    # mapping = (0, 1)
+    for state_true, state_ukf, state_iplf, state_ipls in zip(truth, track_ukf[1:], track_iplf[1:], track_ipls[1:]):
         dist_ukf = Euclidean(mapping)(state_true, state_ukf)
         error_ukf.append(dist_ukf)
         dist_iplf = Euclidean(mapping)(state_true, state_iplf)
@@ -250,6 +427,7 @@ def main():
         error_ipls.append(dist_ipls)
 
     fig, ax = plt.subplots()
+    # timesteps = timesteps[1:]
     ax.plot(timesteps, error_ukf, label='ukf')
     ax.plot(timesteps, error_iplf, label='iplf')
     ax.plot(timesteps, error_ipls, label='ipls')
