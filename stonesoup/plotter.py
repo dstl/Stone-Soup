38fd8276a3532fa4c00367dc12cb017d8f4469ad
James Wright <jwright2@dstl.gov.uk>
2024-05-21 09:20:16 +0100

    Add axis labels for 1d plotting in Plotterly

--- stonesoup/plotter.py
+++ stonesoup/plotter.py
@@ -985,8 +985,15 @@ class Plotterly(_Plotter):
         Generated figure to display graphs.
     """
     def __init__(self, dimension=Dimension.TWO, axis_labels=None, **kwargs):
-        if not axis_labels:
-            axis_labels = ["x", "y"]
+        if dimension != Dimension.ONE:
+            if not axis_labels:
+                axis_labels = ["x", "y"]
+        else:
+            if axis_labels:
+                if len(axis_labels) == 1:
+                    axis_labels = ["Time", axis_labels[0]]
+            else:
+                axis_labels = ["Time", "x"]
         if go is None:
             raise RuntimeError("Usage of Plotterly plotter requires installation of `plotly`")
 
