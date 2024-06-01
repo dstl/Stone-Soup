fix: particle filter covariance

stonesoup/predictor/particle.py
@@ -538,17 +538,31 @@ class MarginalisedParticlePredictor(ParticlePredictor):
         except TypeError:
             # TypeError: (timestamp or prior.timestamp) is None
             time_interval = None
+
         num_samples = len(prior)
         model = self.transition_model
         
         latents = model.sample_latents(time_interval=time_interval, num_samples=num_samples)
-        
-
         process_mean = model.mean(latents=latents, time_interval=time_interval)[:, :, 0] # (N, M)
-        assert(len(process_mean.shape) == 2)
-        process_mean = process_mean.T # (M, N)
         process_covar = model.covar(latents=latents, time_interval=time_interval)
+        process_mean = process_mean.T # (M, N)
         process_covar = process_covar.T# (M, M, N)
+
+
+        # Debug for debugging
+        # latents = model.sample_latents(time_interval=time_interval, num_samples=1)
+        # process_mean = model.mean(latents=latents, time_interval=time_interval) # (N, M)
+        # # print(process_mean.shape)
+        # # raise Exception
+        # process_covar = model.covar(latents=latents, time_interval=time_interval)[..., np.newaxis]
+        # # print(process_covar.shape)
+        # # _rng.multivariate_normal(mean.flatten(), covar, size=num_samples)
+        # process_covar = np.tile(process_covar, (1, 1, num_samples))
+        # process_mean = np.tile(process_mean, (1, num_samples))
+
+
+        # print(process_mean.shape, process_covar.shape)
+        # raise Exception
         F = model.matrix(time_interval=time_interval, **kwargs) # (M, M)
         new_state_vector = F @ prior.state_vector + process_mean
         tmp = np.einsum("jik, li-> jlk", prior.covariance, F) # ()
@@ -571,10 +585,17 @@ class MarginalisedParticlePredictor(ParticlePredictor):
 
         #     new_covariance[..., p] = covar
         #     new_state_vector[..., p] = multivariate_normal.rvs(mean.flatten(), covar)
-        return Prediction.from_state(prior,
+        # print(new_covariance[..., 0])
+        # print(new_covariance[..., 1])
+        # print(ret.covar)
+        ret = Prediction.from_state(prior,
                                      parent=prior,
                                      state_vector=new_state_vector,
                                      covariance=new_covariance,
                                      timestamp=timestamp,
                                      transition_model=self.transition_model)
+        
+        # print(ret.statecovar)
+        # print(ret.covar)
+        return ret
         