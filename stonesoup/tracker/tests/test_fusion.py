2d635499f34201afd5f4d8e9a050ff685b41d65d
Ollie Rosoman <orosoman@dstl.gov.uk>
2024-08-30 15:38:49 +0100

    remove tests for fusion tracker

--- stonesoup/tracker/tests/test_fusion.py
+++
@@ -1,9 +0,0 @@
-from ..fusion import SimpleFusionTracker
-from ..simple import MultiTargetTracker
-
-
-def test_fusion_tracker(initiator, deleter, detector, data_associator, updater):
-    base_tracker = MultiTargetTracker(
-        initiator, deleter, detector, data_associator, updater)
-    _ = SimpleFusionTracker(base_tracker, 30)
-    assert True
