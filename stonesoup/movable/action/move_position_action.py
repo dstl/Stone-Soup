7f8dc68bc4b05e8c83961eb6feca047c8dafc246
Timothy Glover2 <t.glover@lboro.ac.uk>
2025-02-12 17:43:13 +0000

    Introduce action generator and movable for sampling position changes in circle

--- stonesoup/movable/action/move_position_action.py
+++ stonesoup/movable/action/move_position_action.py
@@ -104,3 +104,105 @@ class NStepDirectionalGridActionGenerator(GridActionGenerator):
                     yield MovePositionAction(generator=self,
                                              end_time=self.end_time,
                                              target_value=target_value)
+
+
+class SamplePositionActionGenerator(ActionGenerator):
+    """Base action generator for sampling approaches to action generation. The action
+    generator requires the user to define a number of samples to generate
+    (:attr:`n_samples`) according to the defined sampling technique."""
+
+    action_space: np.ndarray = Property(
+        default=None,
+        doc="The bounds of the action space that should not be exceeded. Of shape (ndim, 2) "
+            "where ndim is the length of the action_mapping. For example, "
+            ":code:`np.array([[xmin, xmax], [ymin, ymax]])`."
+    )
+
+    action_mapping: Sequence[int] = Property(
+        default=(0, 1),
+        doc="The state dimensions that actions are applied to."
+    )
+
+    n_samples: int = Property(
+        default=10,
+        doc="Number of samples to generate. This does not include the action "
+        "to remain at the current position, meaning :attr:`n_samples` +1 "
+        "actions will be generated."
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.action_space is not None:
+            if len(self.action_space) != len(self.action_mapping):
+                raise ValueError(f"Dimensions of action_space {self.action_space.shape} "
+                                 f"are not compatible with action_mapping of length "
+                                 f"{len(self.action_mapping)}. action_space should be "
+                                 f"of shape (ndim, 2) where ndim is the length of the "
+                                 f"action_mapping.")
+
+            if (np.any(self.current_value[self.action_mapping, :] < self.action_space[:, [0]])
+                    or np.any(self.current_value[self.action_mapping, :] > self.action_space[:, [1]])):  # noqa: E501
+                raise ValueError(f"Initial platform location {self.current_value} is not within "
+                                 f"the bounds of the action space {self.action_space}.")
+
+    @property
+    def default_action(self):
+        return MovePositionAction(generator=self,
+                                  end_time=self.end_time,
+                                  target_value=self.current_value)
+
+    def __contains__(self, item):
+        return item in iter(self)
+
+    @abstractmethod
+    def __iter__(self):
+        raise NotImplementedError
+
+
+class CircleSamplePositionActionGenerator(SamplePositionActionGenerator):
+    """Action generator which samples candidate future positions uniformly within
+    a circle around the current position. Circle radius is defined by the user
+    with :attr:`maximum_travel`. This generator is only applicable to 2D position
+    actions."""
+
+    maximum_travel: float = Property(
+        default=1.0,
+        doc="Maximum possible travel distance. Specifies the radius of "
+        "sampling area."
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.action_space is not None and len(self.action_space) != 2:
+            raise ValueError(f"Action mapping {self.action_mapping} does "
+                             f"not have 2 dimensions. "
+                             f":class:`~.CircleSamplePositionActionGenerator` "
+                             f"is designed for 2D action generation only.")
+
+    def __iter__(self):
+
+        yield MovePositionAction(generator=self,
+                                 end_time=self.end_time,
+                                 target_value=self.current_value)
+
+        generated_samples = 0
+
+        while generated_samples < self.n_samples:
+
+            radius_angle_sample = np.random.uniform([0, 0], [1, 2*np.pi], 2)
+
+            value = self.maximum_travel*np.sqrt(radius_angle_sample[0]) *\
+                np.array([[np.sin(radius_angle_sample[1])], [np.cos(radius_angle_sample[1])]])
+
+            target_value = self.current_value + value
+
+            if self.action_space is None or \
+                (np.all(target_value[self.action_mapping, :] >= self.action_space[:, [0]])
+                 and np.all(target_value[self.action_mapping, :] <= self.action_space[:, [1]])):
+
+                generated_samples += 1
+                yield MovePositionAction(generator=self,
+                                         end_time=self.end_time,
+                                         target_value=target_value)
