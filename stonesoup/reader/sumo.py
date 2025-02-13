e154741a67e35eb93a11bcfaaf29b378365ace8e
Timothy Glover <t.glover@lboro.ac.uk>
2025-02-13 18:17:50 +0000

    Imporve obstacle and road network functionality. Add enumerator for vehicle types for filtering roads to import

--- stonesoup/reader/sumo.py
+++ stonesoup/reader/sumo.py
@@ -238,9 +238,18 @@ class SUMOGroundTruthReader(GroundTruthReader):
 
         traci.close()
 
-    def obstacle_gen(self, threshold=0.1):
+    def obstacle_gen(self):
         """
-        Import polygons from SUMO as Obstacle platforms
+        Import polygons from SUMO as Obstacle platforms. All polygons
+        with a type id starting with `'building'` will be imported as
+        an obstacle. Individual buildings with shared faces, or part
+        shared faces, between a neighborough building will be merged
+        into a single obstacle.
+
+        Returns
+        -------
+        : list[Obstacle],
+            List of :class:`~.Obstacle` platforms.
         """
         traci.start(self.sumoCmd)
         if self.geographic_coordinates:
@@ -279,31 +288,187 @@ class SUMOGroundTruthReader(GroundTruthReader):
 
         return obstacles
 
-    def road_network_gen(self):
+    def road_network_gen(self, vehicles_of_interest=None):
         """
-        Import SUMO road network via junction positions and edges connecting junctions
+        Import SUMO road network. Each edge in SUMO contains a minimum of one,
+        straight, line segment and typically multiple straight line segments to
+        approximate curvature. This method extracts coordinates for each line segments
+        within each edge in the provided SUMO config.
+
+        Parameters
+        ----------
+        vehicles_of_interest: Collection[str], optional
+            Collection of vehicle types from :class:`~.VehicleTypeEnum`
+            specifying roads to extract from SUMO. Default value is None
+            and all roads will be processed. If populated with valid vehicle
+            types, only edges that allow the vehicle type are processed and returned.
+
+        Returns
+        -------
+        : dict
+            Pairs of `edge_id`: `edge_coordinates`. `edge_id` is a
+            SUMO assigned ID number for each edge in the SUMO config.
+            `edge_coordinates` is an `np.ndarray` with shape (2, n_segments),
+            where the first axis corresponds to :math:`x` and :mathL`y` dimensions
+            and the second axis corresponds to the number of straigt line segments
+            in `edge_id`.
+
         """
+
+        if (vehicles_of_interest and
+            not all(type_ in [data.name for data in VehicleTypeEnum]
+                    for type_ in vehicles_of_interest)):
+            raise ValueError(f"""Invalid vehicles_of_interest value(s): {', '.join(str(field) for
+                             field in vehicles_of_interest if field not in
+                             [data.name for data in VehicleTypeEnum])}""")
+
         traci.start(self.sumoCmd)
         if self.geographic_coordinates:
             self.origin = traci.simulation.convertGeo(0, 0)   # lon, lat
 
         # Get list of 'edges' that define the road network
-        edge_IDs = traci.edge.getIDList()
+        edge_IDs = list(traci.edge.getIDList())
 
         # Initialise output dict
         road_network = {}
+        complete_list = []
 
         # Each edge has a single 'FromJunction' and 'ToJunction'. Using
         # each junction position, line segments constructing the road
         # network can be defined.
         for edge_ID in edge_IDs:
-            road_network[edge_ID] = \
-                np.array([traci.junction.getPosition(traci.edge.getFromJunction(edge_ID)),
-                          traci.junction.getPosition(traci.edge.getToJunction(edge_ID))]).T
+            if edge_ID in complete_list:
+                continue
+            # Checks to see if `edge_ID` has a complementarty edge travelling in
+            # the opposite direction. If it does these will be processed simultaneously
+            # such that only one edge is added to the road network.
+            if edge_ID[0] == '-' and edge_ID[1:] in edge_IDs:
+                alternative_edge = edge_ID[1:]
+            elif '-'+edge_ID in edge_IDs:
+                alternative_edge = '-'+edge_ID
+            else:
+                alternative_edge = None
+
+            if alternative_edge:
+
+                # Get number of lanes for both edges
+                lane_count = [traci.edge.getLaneNumber(edge_ID)]
+                lane_count.append(traci.edge.getLaneNumber(alternative_edge))
+
+                # Initialise lists
+                allowed_vehicles, shape_1, shape_2 = [], [], []
+                # Get shape data and allowed vehicles for each lane in the first edge
+                for n in range(lane_count[0]):
+                    shape_1.append(np.array(traci.lane.getShape(edge_ID+'_'+str(n))))
+                    allowed_vehicles.extend(list(traci.lane.getAllowed(edge_ID+'_'+str(n))))
+                # Remove duplicate allowed vehicle entries
+                allowed_vehicles = list(set(allowed_vehicles))
+
+                # If the desired vehicles has been defined (not None) and it is not in the
+                # list of allowed vehicles on the edge, continue to the next `edge_ID`.
+                if vehicles_of_interest and not any(type_ in allowed_vehicles for type_ in
+                                                    vehicles_of_interest):
+                    continue
+
+                # processes shape of `edge_ID` by checking for different numbers of
+                # defining points and handling this appropriately.
+                shape_1 = self._process_lane_shape(shape_1)
+
+                # Repeat the above process for the complementory edge. Shape of edge
+                # is inverted to account for opposite directionality
+                allowed_vehicles = []
+                for n in range(lane_count[1]):
+                    shape_2.append(
+                        np.flipud(np.array(traci.lane.getShape(alternative_edge+'_'+str(n)))))
+                    allowed_vehicles.extend(
+                        list(traci.lane.getAllowed(alternative_edge+'_'+str(n))))
+
+                # Check complementory edge to see if desired vehcles are permitted. If not,
+                # shape information from `edge_ID` is copied to allow the single edge to be
+                # included.
+                if vehicles_of_interest and not any(type_ in allowed_vehicles for type_ in
+                                                    vehicles_of_interest):
+                    shape_2 = shape_1
+
+                shape_2 = self._process_lane_shape(shape_2)
+
+                # Checks to see if the shape information for both edges is compatible
+                # (same number of points). If not, a simple assignment is calculated to check
+                # which points can be merged.
+                if shape_1[0].shape != shape_2[0].shape:
+                    size_diff = shape_1[0].shape[0] - shape_2[0].shape[0]
+                    slice_array = [slice(0, -np.abs(size_diff)), slice(np.abs(size_diff), None)]
+                    if size_diff > 0:
+                        first_element_assign = \
+                            np.ptp(np.sqrt(
+                                (shape_1[0][:-int(size_diff), 0]-shape_2[0][:, 0])**2 +
+                                (shape_1[0][:-int(size_diff), 1]-shape_2[0][:, 1])**2))
+                        last_element_assign = \
+                            np.ptp(np.sqrt(
+                                (shape_1[0][int(size_diff):, 0]-shape_2[0][:, 0])**2 +
+                                (shape_1[0][int(size_diff):, 1]-shape_2[0][:, 1])**2))
+                        shape_1 = \
+                            [shape_1_[slice_array[np.argmin([first_element_assign,
+                                                             last_element_assign])], :]
+                                for shape_1_ in shape_1]
+                    else:
+                        size_diff = np.abs(size_diff)
+                        first_element_assign = \
+                            np.ptp(np.sqrt(
+                                (shape_1[0][:, 0]-shape_2[0][:-int(size_diff), 0])**2 +
+                                (shape_1[0][:, 1]-shape_2[0][:-int(size_diff), 1])**2))
+                        last_element_assign = \
+                            np.ptp(np.sqrt(
+                                (shape_1[0][:, 0]-shape_2[0][int(size_diff):, 0])**2 +
+                                (shape_1[0][:, 1]-shape_2[0][int(size_diff):, 1])**2))
+                        shape_2 = \
+                            [shape_2_[slice_array[np.argmin([first_element_assign,
+                                                             last_element_assign])], :]
+                                for shape_2_ in shape_2]
+
+                # Calculate mean shape of the edges
+                mean_lane_shape = np.mean([*shape_1, *shape_2], axis=0)
+                # Store the result
+                road_network[edge_ID] = mean_lane_shape.T
+                # Store list of alternative edge to prevent duplication
+                complete_list.append(alternative_edge)
+                # edge_IDs.remove(alternative_edge)
+
+            else:
+                # Process single edge as above
+                lane_count = traci.edge.getLaneNumber(edge_ID)
+                allowed_vehicles, shape_1 = [], []
+                for n in range(lane_count):
+                    shape_1.append(np.array(traci.lane.getShape(edge_ID+'_'+str(n))))
+                    allowed_vehicles.extend(list(traci.lane.getAllowed(edge_ID+'_'+str(n))))
+                allowed_vehicles = list(set(allowed_vehicles))
+                if vehicles_of_interest and not any(type_ in allowed_vehicles for type_ in
+                                                    vehicles_of_interest):
+                    continue
+
+                shape_1 = self._process_lane_shape(shape_1)
+
+                mean_lane_shape = np.mean(shape_1, axis=0)
+
+                road_network[edge_ID] = mean_lane_shape.T
 
         traci.close(wait=False)
 
         return road_network
+
+    def _process_lane_shape(self, shape):
+
+        # Checks if an edge contains multiple lanes, and ensures that lanes have
+        # same number of segments by only considering the shortest lane.
+
+        if len(set([shape_.shape for shape_ in shape])) != 1:
+            shape_vec = np.array([shape_.shape[0] for shape_ in shape])
+            min_shape = np.min(shape_vec)
+
+            shape = [shape_[0:end_index, :] for shape_, end_index
+                     in zip(shape, shape_vec-(shape_vec-min_shape))]
+
+        return shape
 
 
 class PersonMetadataEnum(Enum):
@@ -413,3 +578,33 @@ class VehicleMetadataEnum(Enum):
     loaded_list = 0x24
     teleporting_list = 0x25
     next_links = 0x33
+
+
+class VehicleTypeEnum(Enum):
+    """
+    An Enum for vehicle types, for use with :meth:`road_network_gen`.
+    See https://sumo.dlr.de/docs/Definition_of_Vehicles%2C_Vehicle_Types%2C_and_Routes.html
+    for full details on vehicle types. This implementation only uses
+    road vehicles and pedestrians.
+    """
+
+    private = 'private'
+    emergency = 'emergency'
+    authority = 'authority'
+    army = 'army'
+    vip = 'vip'
+    pedestrian = 'pedestrian'
+    passenger = 'passenger'
+    hov = 'hov'  # High occupancy vehicle
+    taxi = 'taxi'
+    bus = 'bus'
+    coach = 'coach'
+    delivery = 'delivery'
+    truck = 'truck'
+    trailer = 'trailer'
+    motorcycle = 'motorcycle'
+    moped = 'moped'
+    bicycle = 'bicycle'
+    evehicle = 'evehicle'
+    custom1 = 'custom1'
+    custom2 = 'custom2'
