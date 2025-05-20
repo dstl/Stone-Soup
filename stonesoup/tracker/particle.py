71846602cf526990d555b05c68ae9cadf74d1ea7
James Wright <jwright2@dstl.gov.uk>
2025-05-20 13:16:10 +0100

    Add implementations of Single/MultiTargetExpectedLikelihoodParticleFilter

--- stonesoup/tracker/particle.py
+++ stonesoup/tracker/particle.py
@@ -14,7 +14,9 @@ from stonesoup.updater.particle import ParticleUpdater
 
 
 class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
+
     """An expected likelihood particle filter (ELPF) [#]_ for tracking a single target.
+
 
     Track a single object using Stone Soup components. The tracker works by
     first calling the :attr:`data_associator` with the active track, and then
@@ -33,12 +35,14 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
     track : :class:`~.Track`
         Current track being maintained. Also accessible as the sole item in
         :attr:`tracks`
+<<<<<<< HEAD
 
     References
     ----------
     .. [#] Marrs, A., Maskell, S., and Bar-Shalom, Y., “Expected likelihood for tracking in clutter
        with particle filters”, in Signal and Data Processing of Small Targets 2002, 2002,
        vol. 4728, pp. 230–239. doi:10.1117/12.478507.
+
     """
     initiator: Initiator = Property(doc="Initiator used to initialise the track.")
     deleter: Deleter = Property(doc="Deleter used to delete the track")
@@ -55,8 +59,10 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
         if self.resampler is None:
             self.resampler = SystematicResampler()
+
         self._track = None
 
     @property
@@ -76,7 +82,9 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
             multihypothesis = associations[self._track]
 
             if len(multihypothesis) == 1 and not multihypothesis[0]:
+
                 # if there is only one hypothesis, and it is the missed detection hypothesis,
+
                 # then there is nothing to update, so we just append the prediction
                 new_state = multihypothesis[0].prediction
             else:
@@ -127,7 +135,9 @@ class SingleTargetExpectedLikelihoodParticleFilter(Tracker):
 
 
 class MultiTargetExpectedLikelihoodParticleFilter(Tracker):
+
     """An expected likelihood particle filter (ELPF) [#]_ for tracking multiple targets.
+
 
     Track multiple objects using Stone Soup components. The tracker works by
     first calling the :attr:`data_associator` with the active tracks, and then
@@ -141,11 +151,13 @@ class MultiTargetExpectedLikelihoodParticleFilter(Tracker):
     Parameters
     ----------
 
+
     References
     ----------
     .. [#] Marrs, A., Maskell, S., and Bar-Shalom, Y., “Expected likelihood for tracking in clutter
        with particle filters”, in Signal and Data Processing of Small Targets 2002, 2002,
        vol. 4728, pp. 230–239. doi:10.1117/12.478507.
+
     """
     initiator: Initiator = Property(doc="Initiator used to initialise the track.")
     deleter: Deleter = Property(doc="Initiator used to initialise the track.")
