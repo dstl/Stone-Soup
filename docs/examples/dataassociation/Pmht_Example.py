9ca07b10a0c02f1e540f3e5f4b5433d399c3f4b3
James Wright <jwright2@dstl.gov.uk>
2025-05-21 14:27:06 +0100

    Fix minor spelling, grammar problems

--- docs/examples/dataassociation/Pmht_Example.py
+++ docs/examples/dataassociation/Pmht_Example.py
@@ -7,18 +7,20 @@ Probabilistic Multi-Hypothesis Tracker
 """
 
 # %%
-# The Probabilistic Multi-Hypothesis Tracker (PMHT) is an advanced tracking algorithm designed to handle multiple
-# hypotheses in a tracking scenario. Originally introduced by Streit and Luginbuhl [#]_, the PMHT is particularly
-# useful when dealing with complex scenarios. Such scenarios include the presence of multiple potential targets and
-# uncertainties in measurements.
+# The Probabilistic Multi-Hypothesis Tracker (PMHT) is an advanced tracking algorithm designed to
+# handle multiple hypotheses in a tracking scenario. Originally introduced by Streit and
+# Luginbuhl [#]_, the PMHT is particularly useful when dealing with complex scenarios.
+# Such scenarios include the presence of multiple potential targets and uncertainties in
+# measurements.
 #
-# In PMHT, the objective is to track multiple targets evolving independently over time. Each target's movement is
-# modeled probabilistically, typically using a Markov transition model. In each time step, received measurements can
-# correspond to either one of the target models or clutter. Clutter refers to measurements that do not correspond to
-# any actual target.
+# In PMHT, the objective is to track multiple targets evolving independently over time.
+# Each target's movement is modelled probabilistically, typically using a Markov transition model.
+# In each time step, received measurements can correspond to either one of the target models or
+# clutter. Clutter refers to measurements that do not correspond to any actual target.
 #
-# One of the key challenges in multi-target tracking is data association, i.e., correctly associating measurements with 
-# their respective targets. PMHT addresses this by considering multiple hypotheses for  data association.
+# One of the key challenges in multi-target tracking is data association, i.e., correctly
+# associating measurements with their respective targets. PMHT addresses this by considering
+# multiple hypotheses for  data association.
 # This enables different associations between measurements and targets.
 #
 # Let the motion of target :math:`m` from time step :math:`t-1` to :math:`t` be given by
@@ -33,36 +35,39 @@ Probabilistic Multi-Hypothesis Tracker
 #
 # .. math::
 #          p(z^t_r | x^{m}_t, k^t_r = m) = \begin{cases} 1/V & \text{if }
-#          k^t_r = 0 \\ \zeta(z^t_r | x_m) & \text{otherwise} \end{cases}
+#          k^t_r = 0 \\ \zeta(z^t_r | x_m) & \text{otherwise.} \end{cases}
 #
-# We denote by :math:`X` the joint states of all targets over time steps 1 to T, and  similarly let :math:`Z`
-# and :math:`K` denote the joint measurement and association variables. Unlike data association algorithms, such as the
-# JPDA Filter, the PMHT ignores the constraint that each target can generate at most
-# one detection. This simplification allows us to write down the joint probability as:
+# We denote by :math:`X` the joint states of all targets over time steps 1 to :math:`T`, and
+# similarly let :math:`Z` and :math:`K` denote the joint measurement and association variables.
+# Unlike data association algorithms, such as the JPDA Filter, the PMHT ignores the constraint
+# that each target can generate at most one detection.
+# This simplification allows us to write down the joint probability as:
 #
 # .. math::
-#          p(Z, X, K | \Pi) = \left(\prod_{m=1}^M \phi^m_0(x^m_0)\right)\prod_{t=1}^T\left\{ \left(\prod_{m=1}^M
-#          \phi^m_t(x^m_t | x^m_{t-1}) \right) \left(\prod_{r=1}^{n_t}\pi_t^{k^t_r}
+#          p(Z, X, K | \Pi) = \left(\prod_{m=1}^M \phi^m_0(x^m_0)\right)\prod_{t=1}^T\left\{
+#          \left(\prod_{m=1}^M \phi^m_t(x^m_t | x^m_{t-1}) \right)
+#          \left(\prod_{r=1}^{n_t}\pi_t^{k^t_r}
 #          \zeta\left(z^t_r | x^{k^t_r}_t\right) \right) \right\}
 #
-# where :math:`\pi^m_t` is the prior probability of a measurement at time step :math:`t` being assigned
-# to target :math:`m` (or clutter if :math:`m=0`), and :math:`\Pi` is the joint variable over all target hypotheses
-# and time steps. From this, the algorithm proceeds in a manner similar to the Expectation-Maximisation algorithm.
-# Given an initial estimate :math:`X` of the target states and :math:`\Pi` of the association probabilities, we choose
-# new values :math:`X` and :math:`\Pi` which maximise
+# where :math:`\pi^m_t` is the prior probability of a measurement at time step :math:`t` being
+# assigned to target :math:`m` (or clutter if :math:`m=0`), and :math:`\Pi` is the joint variable
+# over all target hypotheses and time steps.
+# From this, the algorithm proceeds in a manner similar to the Expectation-Maximisation algorithm.
+# Given an initial estimate :math:`X` of the target states and :math:`\Pi` of the association
+# probabilities, we choose new values :math:`X` and :math:`\Pi` which maximise
 #
 # .. math::
-#         Q(\Pi, X | \Pi^\prime, X^\prime) = \sum_K \left\{\log p(Z, X, K | \Pi)p(K | Z, X^\prime, \Pi^\prime)\right\}
+#         Q(\Pi, X | \Pi^\prime, X^\prime) = \sum_K \left\{\log p(Z, X, K | \Pi)p(K | Z, X^\prime,
+#         \Pi^\prime)\right\}
 #
-# This is repeated over a number of iterations. In this implementation, the prior means are used for the initial
-# estimates of the target states for the first batch of measurements. For subsequent batches, we keep an overlap 
-# of a configurable number of time steps and predict forward using the prior means.
+# This is repeated over a number of iterations. In this implementation, the prior means are used
+# for the initial estimates of the target states for the first batch of measurements.
+# For subsequent batches, we keep an overlap of a configurable number of time steps and predict
+# forward using the prior means.
 
 # %%
 # Nearly-constant velocity example
 # --------------------------------
-# In a similar fashion to the previous tutorials, we begin with some imports. These ones allow us access to
-# mathematical and timing functions.
 #
 # General imports
 # ^^^^^^^^^^^^^^^
@@ -76,8 +81,8 @@ np.random.seed(1000)
 # %%
 # Initialise ground truth
 # ^^^^^^^^^^^^^^^^^^^^^^^
-# Here are some configurable parameters associated with the ground truth. We specify a fixed number of initial
-# targets with no births and deaths.
+# Here are some configurable parameters associated with the ground truth. We specify a fixed
+# number of initial targets with no births and deaths.
 
 # The simulator requires a distribution for birth targets, so we specify a dummy one.
 from stonesoup.types.array import StateVector, CovarianceMatrix
@@ -93,7 +98,7 @@ number_of_steps = 50
 preexisting_states = [[-20, 5, 0, 10], [20, -5, 0, 10]]
 
 # %%
-# Create the transition model - default set to 2d nearly-constant velocity
+# Create the transition model
 from stonesoup.models.transition.linear import CombinedLinearGaussianTransitionModel, \
                                                ConstantVelocity
 q_x = 1.0
@@ -120,10 +125,12 @@ groundtruth_sim = MultiTargetGroundTruthSimulator(
 # Initialise the measurement models
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # The simulated ground truth will then be passed to a simple detection simulator. This has a
-# number of configurable parameters, e.g. where, at what rate or detection probability a clutter is generated.
-# This implements similar logic to the code in the previous tutorial section
-# :ref:`auto_tutorials/09_Initiators_&_Deleters:Generate Detections and Clutter`. Note that, currently, very low
-# clutter rate is used as PMHT seems to struggle with high clutter rate.
+# number of configurable parameters, e.g. where, at what rate or detection probability a clutter
+# is generated.
+# This implements similar logic to the code in tutorial 9's section
+# :ref:`auto_tutorials/09_Initiators_&_Deleters:Generate Detections and Clutter`.
+# Note that, currently, s very low clutter rate is used as PMHT seems to struggle with high clutter
+# rate.
 from stonesoup.models.measurement.linear import LinearGaussian
 measurement_model = LinearGaussian(
     ndim_state=4,  # Number of state dimensions (position and velocity in 2D)
@@ -156,8 +163,8 @@ detection_sim = SimpleDetectionSimulator(
 # -----------------------------
 # In this example a Kalman filter is used with global nearest neighbour (GNN) associator. Other
 # options are, of course, available. Note that PMHT algorithm currently assumes a fixed number of
-# tracks, with data association built in. Therefore, the usage of nitiator or deleter component are optional best
-# could be added if needed.
+# tracks, with data association built in. Therefore, the usage of an initiator or deleter component
+# are optional but could be added if needed.
 
 # %%
 # Predictor
@@ -185,16 +192,19 @@ updater = KalmanUpdater(measurement_model)
 # Initial estimate for tracks
 init_means = preexisting_states
 init_cov = np.diag([1.0, 1.0, 1.0, 1.0])
-init_priors = [GaussianState(StateVector(init_mean), init_cov, timestamp=start_time) for init_mean in init_means]
+init_priors = [GaussianState(StateVector(init_mean), init_cov, timestamp=start_time)
+               for init_mean in init_means]
 
 # %%
 # Run the Tracker
 # ---------------
-# The PMHT algorithm is a batch-based approach that processes multiple scans simultaneously. It uses a set of scans
-# from  the previous batch to initialize the new batch for improved performance. The algorithm runs a set number
-# of iterations per batch for convergence, with a maximum iteration limit. Though a convergence test might stop
-# iterations early, the current setup runs all iterations. The algorithm can maintain and update prior probabilities
-# of data association, affecting performance based on the application.
+# The PMHT algorithm is a batch-based approach that processes multiple scans simultaneously.
+# It uses a set of scans from the previous batch to initialise the new batch for improved
+# performance. The algorithm runs a set number of iterations per batch for convergence, with a
+# maximum iteration limit.
+# Though a convergence test might stop iterations early, the current setup runs all iterations.
+# The algorithm can maintain and update prior probabilities of data association, affecting
+# performance based on the application.
 
 # Number of measurement scans to run over for each batch
 batch_len = 10
@@ -249,7 +259,8 @@ plotter.fig
 # %%
 # Key points
 # ----------
-# 1. PMHT uses a probabilistic framework and Bayesian recursion to update beliefs about target states.
+# 1. PMHT uses a probabilistic framework and Bayesian recursion to update beliefs about target
+#    states.
 #    It accounts for uncertainties in target motion and measurement noise.
 # 2. An essential aspect of PMHT is the association of predicted target states with measurements.
 #    This association step, called data association, involves matching measurements to targets.
@@ -261,7 +272,7 @@ plotter.fig
 # .. [#] R. Streit and T. Luginbuhl, “Maximum likelihood method for probabilistic multihypothesis
 #        tracking”, Proceedings of SPIE, 1994.
 # .. [#] R. Streit and T. Luginbuhl”, Probabilistic Multi-Hypothesis Tracking”, Technical
-#        Report, Naval Undersea Warfare Division, 1995 problems, IEE Proc., Radar Sonar Navigation, 146:2–7
+#        Report, Naval Undersea Warfare Division, 1995 problems, IEE Proc., Radar Sonar
+#        Navigation, 146:2–7
 # .. [#] S. Davey, D. Gray and R. Streit, “Tracking, association, and classification: a combined
 #        PMHT approach”, Digital Signal Processing 12, 372–382, 2002.
-
