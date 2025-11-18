b1ada4e4f50f630b5cd4edc77a4562515fc9264a
James Wright <jwright2@dstl.gov.uk>
2025-11-18 08:08:49 +0000

    Update ModelAugmentor to use from_state method

--- stonesoup/augmentor/model.py
+++ stonesoup/augmentor/model.py
@@ -1,7 +1,7 @@
 from ..types.mixture import GaussianMixture
-from ..types.prediction import Prediction
-from ..types.state import ExpandedModelAugmentedWeightedGaussianState
-from ..types.update import Update
+from ..types.prediction import Prediction, ExpandedModelAugmentedWeightedGaussianStatePrediction
+from ..types.state import State, ExpandedModelAugmentedWeightedGaussianState
+from ..types.update import Update, ExpandedModelAugmentedWeightedGaussianStateUpdate
 
 from .base import Augmentor
 
@@ -13,33 +13,16 @@ class ModelAugmentor(Augmentor):
         """Augments the prior states and the transition models (combinatorically)."""
         new_states = []
         for state in states:
-            state_vector = state.state_vector
-            covar = state.covar
-            timestamp = state.timestamp
-            weight = state.weight
-            model_histories = state.model_histories
-            measurement_histories = state.measurement_histories
-            model_history_length = state.model_history_length
-            measurement_history_length = state.measurement_history_length
-            existence = state.existence
-
             for model in self.transition_models:
-                # Should be from_state to inherit the Prediction or Update info
-                temp_state = ExpandedModelAugmentedWeightedGaussianState(
-                    state_vector=state_vector,
-                    covar=covar,
-                    timestamp=timestamp,
-                    weight=weight,
-                    model_histories=model_histories,
-                    measurement_histories=measurement_histories,
-                    model_history_length=model_history_length,
-                    measurement_history_length=measurement_history_length,
-                    existence=existence,
-                    model=model
-                    )
                 if isinstance(state, Prediction):
-                    temp_state = Prediction.from_state(temp_state, prior=state.prior)
+                    target_type = ExpandedModelAugmentedWeightedGaussianStatePrediction
                 elif isinstance(state, Update):
-                    temp_state = Update.from_state(temp_state, hypothesis=state.hypothesis)
-                new_states.append(temp_state)
+                    target_type = ExpandedModelAugmentedWeightedGaussianStateUpdate
+                else:
+                    target_type = ExpandedModelAugmentedWeightedGaussianState
+                new_state = State.from_state(
+                    state,
+                    model=model,
+                    target_type=target_type)
+                new_states.append(new_state)
         return GaussianMixture(new_states)
