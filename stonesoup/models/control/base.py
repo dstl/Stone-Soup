a02d0996867f87cee1e2b13f04d1610131dea0ac
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2025-12-05 08:23:37 +0000

    Add prior as default input to control models

--- stonesoup/models/control/base.py
+++ stonesoup/models/control/base.py
@@ -15,3 +15,25 @@ class ControlModel(Model):
     def ndim_ctrl(self) -> int:
         """Number of control input dimensions"""
         pass
+
+    @abstractmethod
+    def function(self, control_input, prior, noise=False, **kwargs):
+        r"""Control Model function :math:`f_k(u(k),x(k),w(k))`
+
+        Parameters
+        ----------
+        control_input : :class:`State`, optional
+            :math:`\mathbf{u}_k`
+        prior : :class:`~.State`
+            The prior state, :math:`\mathbf{x}_{k-1}`
+        noise: :class:`numpy.ndarray` or bool
+            An externally generated random process noise sample (the default is
+            `False`, in which case no noise will be added
+            if 'True', the output of :meth:`~.Model.rvs` is used)
+
+        Returns
+        -------
+        : :class:`StateVector` or :class:`StateVectors`
+            The StateVector(s) with the model function evaluated.
+        """
+        raise NotImplementedError()
