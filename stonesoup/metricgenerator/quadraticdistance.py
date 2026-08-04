0cdead5a36cd7a4f3e998e1c1c773b2972ec4994
Rudyard1 <115188735+Rudyard1@users.noreply.github.com>
2026-08-04 11:22:20 +0100

    fixed leftover use of add rather than append

--- stonesoup/metricgenerator/quadraticdistance.py
+++ stonesoup/metricgenerator/quadraticdistance.py
@@ -376,7 +376,7 @@ class QuadraticDistance(MetricGenerator):
         measured_states_at_times = defaultdict(list)
         truth_states_at_times = defaultdict(list)
         for state in measured_states:
-            measured_states_at_times[state.timestamp].add(state)
+            measured_states_at_times[state.timestamp].append(state)
         for state in truth_states:
             truth_states_at_times[state.timestamp].append(state)
         for timestamp in timestamps:
