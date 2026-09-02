113665f72657c27e31a98aefe2a0a81b820ac677
Rudyard1 <115188735+Rudyard1@users.noreply.github.com>
2026-09-02 09:21:00 +0100

    Fix seed for test_mean_quadratic_error

--- stonesoup/metricgenerator/tests/test_quadraticdistance.py
+++ stonesoup/metricgenerator/tests/test_quadraticdistance.py
@@ -182,6 +182,7 @@ def test_mean_quadratic_error():
     # timestep 1
     truths = OrderedSet()
     num_init_targs = 5
+    np.random.seed(2007)
     for i in range(num_init_targs):
         x, y = np.random.uniform(-30, 30, 2)  # Range [-30, 30] for x and y
         x_vel, y_vel = (np.random.rand(2)) * 2 - 1  # Range [-1, 1] for x and y velocity
